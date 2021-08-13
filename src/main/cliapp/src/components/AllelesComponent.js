23aedd76593e4fbe388a0deaee08fd7091df2662
Olin Blodgett <nilo@nilosplace.net>
2021-08-13 16:53:52 -0400

    Tabs for Spaces

--- src/main/cliapp/src/components/AllelesComponent.js
+++ src/main/cliapp/src/components/AllelesComponent.js
@@ -6,79 +6,79 @@ import { AlleleService } from '../service/AlleleService';
 
 export const AllelesComponent = () => {
 
-	const [alleles, setAlleles] = useState(null);
-	const [multiSortMeta, setMultiSortMeta] = useState([]);
-	const [filters, setFilters] = useState({});
-	const [page, setPage] = useState(0);
-	const [first, setFirst] = useState(0);
-	const [rows, setRows] = useState(50);
-	const [totalRecords, setTotalRecords] = useState(0);
+  const [alleles, setAlleles] = useState(null);
+  const [multiSortMeta, setMultiSortMeta] = useState([]);
+  const [filters, setFilters] = useState({});
+  const [page, setPage] = useState(0);
+  const [first, setFirst] = useState(0);
+  const [rows, setRows] = useState(50);
+  const [totalRecords, setTotalRecords] = useState(0);
 
-	useEffect(() => {
-		const alleleService = new AlleleService();
-		alleleService.getAlleles(rows, page, multiSortMeta, filters).then(searchReults => {
-			setAlleles(searchReults.results);
-			setTotalRecords(searchReults.totalResults);
-		});
+  useEffect(() => {
+    const alleleService = new AlleleService();
+    alleleService.getAlleles(rows, page, multiSortMeta, filters).then(searchReults => {
+      setAlleles(searchReults.results);
+      setTotalRecords(searchReults.totalResults);
+    });
 
-	}, [rows, page, multiSortMeta, filters]);
+  }, [rows, page, multiSortMeta, filters]);
 
-	const onLazyLoad = (event) => {
-		setRows(event.rows);
-		setPage(event.page);
-		setFirst(event.first);
-	}
+  const onLazyLoad = (event) => {
+    setRows(event.rows);
+    setPage(event.page);
+    setFirst(event.first);
+  }
 
-	const onFilter = (event) => {
-		//console.log("On Filter: ");
-		//console.log(event.filters);
-		setFilters(event.filters);
-	}
+  const onFilter = (event) => {
+    //console.log("On Filter: ");
+    //console.log(event.filters);
+    setFilters(event.filters);
+  }
 
-	const onSort = (event) => {
-		//console.log("On Sort: ");
-		//console.log(event);
-		var found = false;
-		var newSort = [...multiSortMeta];
+  const onSort = (event) => {
+    //console.log("On Sort: ");
+    //console.log(event);
+    var found = false;
+    var newSort = [...multiSortMeta];
 
-		newSort.forEach((o) => {
-			if(o.field === event.multiSortMeta[0].field) {
-				o.order = event.multiSortMeta[0].order;
-				found = true;
-			}
-		});
+    newSort.forEach((o) => {
+      if(o.field === event.multiSortMeta[0].field) {
+        o.order = event.multiSortMeta[0].order;
+        found = true;
+      }
+    });
 
-		if(!found) {
-			setMultiSortMeta(newSort.concat(event.multiSortMeta));
-		} else {
-			setMultiSortMeta(newSort);
-		}
-	}
+    if(!found) {
+      setMultiSortMeta(newSort.concat(event.multiSortMeta));
+    } else {
+      setMultiSortMeta(newSort);
+    }
+  }
 
-	const symbolTemplate = (rowData) => {
-		return <div dangerouslySetInnerHTML={{__html: rowData.symbol}} />
-	}
+  const symbolTemplate = (rowData) => {
+    return <div dangerouslySetInnerHTML={{__html: rowData.symbol}} />
+  }
 
-	const paginatorLeft = <Button type="button" icon="pi pi-refresh" className="p-button-text" />;
-	const paginatorRight = <Button type="button" icon="pi pi-cloud" className="p-button-text" />;
+  const paginatorLeft = <Button type="button" icon="pi pi-refresh" className="p-button-text" />;
+  const paginatorRight = <Button type="button" icon="pi pi-cloud" className="p-button-text" />;
 
-	return (
-			<div>
-				<div className="card">
-					<DataTable value={alleles} className="p-datatable-sm"
-						paginator totalRecords={totalRecords} onPage={onLazyLoad} lazy first={first} 
-						sortMode="multiple" removableSort onSort={onSort} multiSortMeta={multiSortMeta}
-						onFilter={onFilter} filters={filters}
-						paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-						currentPageReportTemplate="Showing {first} to {last} of {totalRecords}" rows={rows} rowsPerPageOptions={[10,20,50,100,250,1000]}
-						paginatorLeft={paginatorLeft} paginatorRight={paginatorRight}>
+  return (
+      <div>
+        <div className="card">
+          <DataTable value={alleles} className="p-datatable-sm"
+            paginator totalRecords={totalRecords} onPage={onLazyLoad} lazy first={first} 
+            sortMode="multiple" removableSort onSort={onSort} multiSortMeta={multiSortMeta}
+            onFilter={onFilter} filters={filters}
+            paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords}" rows={rows} rowsPerPageOptions={[10,20,50,100,250,1000]}
+            paginatorLeft={paginatorLeft} paginatorRight={paginatorRight}>
 
-						<Column field="curie" header="Curie" sortable filter></Column>
-						<Column field="description" header="Description" sortable filter></Column>
-						<Column field="symbol" header="Symbol" body={symbolTemplate} sortable filter></Column>
-						<Column field="taxon" header="Taxon" sortable filter></Column>
-					</DataTable>
-				</div>
-			</div>
-	)
+            <Column field="curie" header="Curie" sortable filter></Column>
+            <Column field="description" header="Description" sortable filter></Column>
+            <Column field="symbol" header="Symbol" body={symbolTemplate} sortable filter></Column>
+            <Column field="taxon" header="Taxon" sortable filter></Column>
+          </DataTable>
+        </div>
+      </div>
+  )
 }
--- src/main/cliapp/src/service/GeneService.js
+++ src/main/cliapp/src/service/GeneService.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
 export class GeneService {
-	getGenes(limit, page, sorts, filters) {
-		var sortOptions = {};
+  getGenes(limit, page, sorts, filters) {
+    var sortOptions = {};
 
-		var sortArray = {};
-		sorts.forEach((o) => {
-			sortArray[o.field] = o.order;
-		});
+    var sortArray = {};
+    sorts.forEach((o) => {
+      sortArray[o.field] = o.order;
+    });
 
-		var filterArray = {};
+    var filterArray = {};
 
-		Object.keys(filters).forEach((key) => {
-			filterArray[key] = filters[key]["value"];
-		});
+    Object.keys(filters).forEach((key) => {
+      filterArray[key] = filters[key]["value"];
+    });
 
-		if(Object.keys(filterArray).length > 0) {
-			sortOptions["searchFilters"] = filterArray;
-		}
-		if(Object.keys(sortArray).length > 0) {
-			sortOptions["sortOrders"] = sortArray;
-		}
+    if(Object.keys(filterArray).length > 0) {
+      sortOptions["searchFilters"] = filterArray;
+    }
+    if(Object.keys(sortArray).length > 0) {
+      sortOptions["sortOrders"] = sortArray;
+    }
 
-		return axios.post('/api/gene/search?limit=' + limit + '&page=' + page, sortOptions).then(res => res.data);
-	}
+    return axios.post('/api/gene/search?limit=' + limit + '&page=' + page, sortOptions).then(res => res.data);
+  }
 }
