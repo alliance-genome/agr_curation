20d8c0d1a7bf46d746c77557515e917192dfe0fc
Olin Blodgett <nilo@nilosplace.net>
2021-09-02 08:02:19 -0400

    Initial Parsing

--- src/test/java/org/alliancegenome/curation_api/config/OWLTravels.java
+++ src/test/java/org/alliancegenome/curation_api/config/OWLTravels.java
@@ -1,23 +1,32 @@
 package org.alliancegenome.curation_api.config;
 
 import java.io.File;
-import java.util.*;
-import java.util.stream.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.reasoner.*;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDatatypeDefinitionAxiom;
+import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
+import org.semanticweb.owlapi.model.OWLDocumentFormat;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLObjectVisitor;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyFormat;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.rdf.rdfxml.renderer.OWLOntologyXMLNamespaceManager;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 import org.semanticweb.owlapi.search.EntitySearcher;
 
-import lombok.extern.java.Log;
-
-
-@Log
-public class OWLTravels {
+public class OWLTravels implements OWLObjectVisitor {
 
     private OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
-    private OWLDataFactory df = OWLManager.getOWLDataFactory();
+    //private OWLDataFactory df = OWLManager.getOWLDataFactory();
     private OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
     private OWLReasoner reasoner;
     private OWLOntology ontology;
@@ -30,22 +39,32 @@ public class OWLTravels {
 
 
         //IRI pizza_iri = IRI.create("https://protege.stanford.edu/ontologies/pizza/pizza.owl");
-        IRI pizza_iri = IRI.create(new File("/Users/olinblodgett/Desktop/FMS/doid.owl"));
+        IRI do_iri = IRI.create(new File("/Users/balrog/git/agr_curation/doid.owl"));
 
-        ontology = manager.loadOntologyFromOntologyDocument(pizza_iri);
-        // Create the walker
-        //OWLReasoner reasoner = OWLReasonerFactory.createReasoner(o);
+        ontology = manager.loadOntologyFromOntologyDocument(do_iri);
 
         reasoner = reasonerFactory.createReasoner(ontology);
 
         OWLClass root = manager.getOWLDataFactory().getOWLThing();
 
         System.out.println("Ontology Loaded...");
-        System.out.println("Document IRI: " + pizza_iri);
+        System.out.println("Document IRI: " + do_iri);
         System.out.println("Ontology : " + ontology.getOntologyID());
         System.out.println("Format      : " + manager.getOntologyFormat(ontology));
 
-
+        OWLDocumentFormat format = manager.getOntologyFormat(ontology);
+        
+        OWLOntologyXMLNamespaceManager nsManager = new OWLOntologyXMLNamespaceManager(ontology, format);
+        System.out.println(nsManager.getDefaultNamespace());
+        
+        for (String prefix : nsManager.getPrefixes()) {
+            System.out.println(prefix);
+        }
+        for (String ns : nsManager.getNamespaces()) {
+            System.out.println(ns);
+        }
+        
+        
         traverse(root, 0);
 
     }
@@ -54,15 +73,14 @@ public class OWLTravels {
 
         if (reasoner.isSatisfiable(parent)) {
 
-            for(int i = 0; i < depth; i++) {
-                System.out.print("   ");
-            }
-
-            System.out.println(getLabel(parent));
+            //System.out.println("--------------------------------------------------------------------------");
+            System.out.println(getLabel(parent, depth));
+            //System.out.println("--------------------------------------------------------------------------");
 
             //.map(e-> {
             //  System.out.println(e.toStringID());
             //});
+            
 
             for (OWLClass child : reasoner.getSubClasses(parent, true).getFlattened()) {
                 if (!child.equals(parent)) {
@@ -73,14 +91,20 @@ public class OWLTravels {
 
     }
 
-    public String getLabel(OWLClass node) {
+    public String getLabel(OWLClass node, int depth) {
 
         //EntitySearcher.getAnnotationObjects(node, ontology).forEach(e -> { System.out.println(e); });
         
+        node.accept(this);
+        
         List<String> ret = EntitySearcher.getAnnotationObjects(node, ontology).map(e -> {
 
-            System.out.println("Val: " + e.getValue());
-            System.out.println("Prop: " + e.getProperty());
+            for(int i = 0; i < depth; i++) {
+                System.out.print("   ");
+            }
+            //System.out.println("Val: " + e.getValue());
+            System.out.println("Prop: " + e.getProperty().getIRI());
+            //System.out.println("Sig: " + e);
             if(e.getProperty().isLabel()) {
                 return ((OWLLiteral)e.getValue()).getLiteral();
             } else {
@@ -94,7 +118,17 @@ public class OWLTravels {
         else return ret.get(0);
     }
 
+    @Override
+    public void visit(OWLDeclarationAxiom axiom) {
+        //System.out.println(axiom);
+        OWLObjectVisitor.super.visit(axiom);
+    }
 
+    @Override
+    public void visit(OWLDatatypeDefinitionAxiom axiom) {
+        //System.out.println(axiom);
+        OWLObjectVisitor.super.visit(axiom);
+    }
 
 
 }
