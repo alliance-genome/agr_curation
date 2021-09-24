bb00644af46b7bfbfac38f4c79539328aab2a081
Adam <agibson@mcw.edu>
2021-09-24 09:27:18 -0500

    SCRUM-321 removed Json Annotation from ExperimentalConditionDTO

--- src/main/java/org/alliancegenome/curation_api/model/ingest/json/dto/ExperimentalConditionDTO.java
+++ src/main/java/org/alliancegenome/curation_api/model/ingest/json/dto/ExperimentalConditionDTO.java
@@ -1,6 +1,5 @@
 package org.alliancegenome.curation_api.model.ingest.json.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.alliancegenome.curation_api.base.BaseDTO;
 
 import lombok.Data;
@@ -14,8 +13,6 @@ public class ExperimentalConditionDTO extends BaseDTO {
     private String conditionQuantity;
     private String anatomicalOntologyId;
     private String geneOntologyId;
-
-    @JsonProperty("NCBITaxonId")
     private String ncbitaxonId;
     private String chemicalOntologyId;
 
