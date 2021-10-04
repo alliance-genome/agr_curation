2b69a99554b399527526662ccf391c194ccc33cd
Andrés Becerra <abecerra@ebi.ac.uk>
2021-10-04 16:56:05 -0500

    SCRUM-248: add test for file 01_all_fields

--- src/test/java/org/alliancegenome/curation_api/bulkupload/GeneBulkUploadITCase.java
+++ src/test/java/org/alliancegenome/curation_api/bulkupload/GeneBulkUploadITCase.java
@@ -11,6 +11,7 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
 import io.restassured.config.*;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusIntegrationTest
@@ -24,9 +25,43 @@ public class GeneBulkUploadITCase {
                     .setParam("http.socket.timeout", 100000)
                     .setParam("http.connection.timeout", 100000));
     }
-    
+
     @Test
-    public void geneBulkUpload() throws IOException {
+    public void geneBulkUploadCheckFields() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/gene/01_all_fields.json"));
+
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/gene/bulk/bgifile?async=false").
+            then().
+            statusCode(200);
+
+        // check if all the fields are correctly read
+       RestAssured.given().
+            when().
+            get("/api/gene/all").
+            then().
+            statusCode(200).
+               body("totalResults", is(1)).
+               body("results", hasSize(1)).
+               body("results[0].curie", is("TEST:TestGene00001")).
+               body( "results[0].taxon", is("NCBITaxon:10090")).
+               body("results[0].name", is( "Test gene 1")).
+               body("results[0].synonyms[0].name", is("Test1")).
+               body("results[0].synonyms[1].name", is("ExampleGene1")).
+               body("results[0].crossReferences[0].curie", is("TEST:xref1b")).
+               body("results[0].crossReferences[1].curie", is("TEST:xref1a")).
+               body("results[0].symbol", is("Tg1")).
+               body("results[0].geneSynopsis", is("Test gene with all fields populated")).
+               body("results[0].geneSynopsisURL", is("http://test.org/test_synopsis_1")).
+               body("results[0].type", is("SO:0001217"));
+    }
+
+    @Test
+    public void geneBulkUploadMany() throws IOException {
         String content = Files.readString(Path.of("src/test/resources/gene/00_mod_examples.json"));
 
         // upload file
@@ -38,12 +73,12 @@ public class GeneBulkUploadITCase {
             then().
             statusCode(200);
 
-        // check if all the genes uploaded
+        // check if all the genes uploaded (605 + 1 from the test above)
         RestAssured.given().
                 when().
                 get("/api/gene/all").
                 then().
                 statusCode(200).
-                body("totalResults", is(605));
+                body("totalResults", is(606));
     }
 }
