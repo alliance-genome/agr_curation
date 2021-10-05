892449100f1fb23a1904a6b69b1a689281c54333
Adam Gibson <agibson@mcw.edu>
2021-10-05 08:44:20 -0500

    SCRUM-475 added biological entity service

---
+++ src/main/cliapp/src/service/BiologicalEntityService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export class BiologicalEntityService {
+    getBiologicalEntities(limit, page, sorts, filters) {
+        var sortOptions = {};
+
+        var sortArray = {};
+        if(sorts) {
+            sorts.forEach((o) => {
+                sortArray[o.field] = o.order;
+            });
+        }
+
+        var filterArray = {};
+        if(filters) {
+            Object.keys(filters).forEach((key) => {
+                filterArray[key] = filters[key]["value"];
+            });
+        }
+
+        if(Object.keys(filterArray).length > 0) {
+            sortOptions["searchFilters"] = filterArray;
+        }
+        if(Object.keys(sortArray).length > 0) {
+            sortOptions["sortOrders"] = sortArray;
+        }
+
+        return axios.post(`/api/biologicalentity/search?limit=${limit}&page=${page}`, sortOptions).then(res => res.data);
+    }
+}
+
+
