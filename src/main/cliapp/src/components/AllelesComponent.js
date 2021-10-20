0a96e668edabbc5556390275f9db8d46dc0606cb
Adam Gibson <agibson@mcw.edu>
2021-10-20 16:51:06 -0500

    SCRUM-568 cleaned up unused var warnings

--- src/main/cliapp/src/components/AllelesComponent.js
+++ src/main/cliapp/src/components/AllelesComponent.js
@@ -1,7 +1,6 @@
 import React, {useRef, useState} from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
 import { AlleleService } from '../service/AlleleService';
 import { useQuery } from 'react-query';
 import { Messages } from 'primereact/messages';
