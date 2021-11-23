92e9b77ce612e54d2a4d640e691e34858fcaed13
Andrés Becerra <abecerra@ebi.ac.uk>
2021-11-23 09:59:27 -0500

    SCRUM-488: add integrity rules for relations in disease annotations

--- src/test/java/org/alliancegenome/curation_api/crud/controllers/DiseaseAnnotationITCase.java
+++ src/test/java/org/alliancegenome/curation_api/crud/controllers/DiseaseAnnotationITCase.java
@@ -4,12 +4,11 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
 
-import org.alliancegenome.curation_api.model.entities.BiologicalEntity;
-import org.alliancegenome.curation_api.model.entities.CrossReference;
-import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
-import org.alliancegenome.curation_api.model.entities.Reference;
+import io.restassured.common.mapper.TypeRef;
+import org.alliancegenome.curation_api.model.entities.*;
 import org.alliancegenome.curation_api.model.entities.ontology.DOTerm;
 import org.alliancegenome.curation_api.resources.TestElasticSearchResource;
+import org.alliancegenome.curation_api.response.SearchResponse;
 import org.junit.jupiter.api.*;
 
 import java.time.LocalDateTime;
@@ -24,11 +23,22 @@ import static org.hamcrest.Matchers.hasSize;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DiseaseAnnotationITCase {
 
+    private CrossReference crossReference;
+    private DOTerm doTerm;
+    private BiologicalEntity biologicalEntity;
+    private Reference reference;
+    private DiseaseAnnotation diseaseAnnotation;
+
+    private TypeRef<SearchResponse<DiseaseAnnotation>> getSearchResponseTypeRef() {
+        return new TypeRef<SearchResponse <DiseaseAnnotation>>() {
+        };
+    }
+
     @Test
     @Order(1)
     public void createDiseaseAnnotation() throws Exception {
 
-        CrossReference crossReference = new CrossReference();
+        crossReference = new CrossReference();
         crossReference.setDisplayName("AGRreference");
         crossReference.setCurie("DOID:0001");
         crossReference.setPrefix("agr");
@@ -44,7 +54,7 @@ public class DiseaseAnnotationITCase {
             then().
             statusCode(200);
 
-        DOTerm doTerm = new DOTerm();
+        doTerm = new DOTerm();
         doTerm.setCurie("DOT:0001");
         doTerm.setCrossReferences(Arrays.asList(crossReference));
 
@@ -56,7 +66,7 @@ public class DiseaseAnnotationITCase {
             then().
             statusCode(200);
 
-        BiologicalEntity biologicalEntity = new BiologicalEntity();
+        biologicalEntity = new BiologicalEntity();
         biologicalEntity.setCurie("BO:0001");
         biologicalEntity.setTaxon("taxon:0001");
 
@@ -68,16 +78,15 @@ public class DiseaseAnnotationITCase {
             then().
             statusCode(200);
 
-        Reference reference = new Reference();
-        reference.setCurie("REF:0001");
-
-        DiseaseAnnotation diseaseAnnotation = new DiseaseAnnotation();
+        diseaseAnnotation = new DiseaseAnnotation();
         diseaseAnnotation.setDiseaseRelation(DiseaseAnnotation.DiseaseRelation.is_implicated_in);
         diseaseAnnotation.setCurie("Disease:0001");
         diseaseAnnotation.setNegated(false);
         diseaseAnnotation.setObject(doTerm);
         diseaseAnnotation.setSubject(biologicalEntity);
 //        Not ready for references yet:
+//        reference = new Reference();
+//        reference.setCurie("REF:0001");
 //        diseaseAnnotation.setReferenceList(Arrays.asList(reference));
 
         RestAssured.given().
@@ -95,10 +104,36 @@ public class DiseaseAnnotationITCase {
             post("/api/disease-annotation/find?limit=10&page=0").
             then().
             statusCode(200).
-                body("totalResults", is(1)).
-                body("results", hasSize(1)).
-                body("results[0].curie", is("Disease:0001")).
-                body("results[0].subject.curie", is("BO:0001")).
-                body("results[0].object.curie", is("DOT:0001"));
+            body("totalResults", is(1)).
+            body("results", hasSize(1)).
+            body("results[0].curie", is("Disease:0001")).
+            body("results[0].subject.curie", is("BO:0001")).
+            body("results[0].object.curie", is("DOT:0001"));
+    }
+
+    @Test
+    @Order(2)
+    public void editDiseaseAnnotation() throws Exception {
+        SearchResponse<DiseaseAnnotation> res = RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=0").
+            then().
+            statusCode(200).
+            extract().body().as(getSearchResponseTypeRef());
+
+        DiseaseAnnotation editedDiseaseAnnotation = res.getResults().get(0);
+        editedDiseaseAnnotation.setNegated(true);
+//        editedDiseaseAnnotation.setReferenceList(Arrays.asList());
+//        editedDiseaseAnnotation.setEvidenceCodes(Arrays.asList());
+//
+//        RestAssured.given().
+//            contentType("application/json").
+//            body(diseaseAnnotation).
+//            when().
+//            put("/api/disease-annotation").
+//            then().
+//            statusCode(200);
     }
 }
