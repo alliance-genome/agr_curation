37275503c9be997917a83de67fa5e471673e3d57
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2021-11-25 16:26:42 +0000

    Override view for find endpoints
    To enable lists to be checked in integration tests.

--- src/main/java/org/alliancegenome/curation_api/interfaces/rest/DiseaseAnnotationRESTInterface.java
+++ src/main/java/org/alliancegenome/curation_api/interfaces/rest/DiseaseAnnotationRESTInterface.java
@@ -1,13 +1,17 @@
 package org.alliancegenome.curation_api.interfaces.rest;
 
 
+import java.util.HashMap;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import org.alliancegenome.curation_api.base.BaseCrudRESTInterface;
 import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
 import org.alliancegenome.curation_api.response.ObjectResponse;
+import org.alliancegenome.curation_api.response.SearchResponse;
 import org.alliancegenome.curation_api.view.View;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -22,4 +26,12 @@ public interface DiseaseAnnotationRESTInterface extends BaseCrudRESTInterface<Di
     @Path("/{curie}")
     @JsonView(View.FieldsAndLists.class)
     public ObjectResponse<DiseaseAnnotation> get(@PathParam("curie") String curie);
+    
+    @POST
+    @Path("/find")
+    @JsonView(View.FieldsAndLists.class)
+    public SearchResponse<DiseaseAnnotation> find(
+            @DefaultValue("0") @QueryParam("page") Integer page,
+            @DefaultValue("10") @QueryParam("limit") Integer limit,
+            @RequestBody HashMap<String, Object> params);
 }
--- src/main/java/org/alliancegenome/curation_api/interfaces/rest/GeneRESTInterface.java
+++ src/main/java/org/alliancegenome/curation_api/interfaces/rest/GeneRESTInterface.java
@@ -1,13 +1,18 @@
 package org.alliancegenome.curation_api.interfaces.rest;
 
 
+import java.util.HashMap;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import org.alliancegenome.curation_api.base.BaseCrudRESTInterface;
 import org.alliancegenome.curation_api.model.entities.Gene;
+import org.alliancegenome.curation_api.model.entities.Molecule;
 import org.alliancegenome.curation_api.response.ObjectResponse;
+import org.alliancegenome.curation_api.response.SearchResponse;
 import org.alliancegenome.curation_api.view.View;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -22,4 +27,11 @@ public interface GeneRESTInterface extends BaseCrudRESTInterface<Gene> {
     @Override
     ObjectResponse<Gene> get(@PathParam("id") String id);
 
+    @POST
+    @Path("/find")
+    @JsonView(View.FieldsAndLists.class)
+    public SearchResponse<Gene> find(
+            @DefaultValue("0") @QueryParam("page") Integer page,
+            @DefaultValue("10") @QueryParam("limit") Integer limit,
+            @RequestBody HashMap<String, Object> params);
 }
