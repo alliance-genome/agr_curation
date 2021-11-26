7d2e382caef41a8f94d7a2f3d300e3e8f73939a7
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2021-11-26 00:06:04 +0000

    Add more disease annotation bulk upload tests

--- src/test/java/org/alliancegenome/curation_api/bulkupload/DiseaseAnnotationBulkUploadITCase.java
+++ src/test/java/org/alliancegenome/curation_api/bulkupload/DiseaseAnnotationBulkUploadITCase.java
@@ -290,7 +290,7 @@ public class DiseaseAnnotationBulkUploadITCase {
             contentType("application/json").
             body(content).
             when().
-            post("/api/disease/bulk/zfinAnnotationFile").
+            post("/api/disease/bulk/fbAnnotationFile").
             then().
             statusCode(200);
     
@@ -300,10 +300,10 @@ public class DiseaseAnnotationBulkUploadITCase {
             when().
             header("Content-Type", "application/json").
             body("{}").
-            post("/api/disease-annotation/find?limit=10&page=48").
+            post("/api/disease-annotation/find?limit=10&page=46").
             then().
             statusCode(200).
-            body("totalResults", is(485)).
+            body("totalResults", is(465)). // 40 FB annotations replaced with 1
             body("results", hasSize(5)).
             body("results[4].subject.curie", is("FB:FBgn0260936")).
             body("results[4].object.curie", is("DOID:4")).
@@ -321,10 +321,259 @@ public class DiseaseAnnotationBulkUploadITCase {
             // body("results[4].conditionRelations[0].conditions[0].conditionChemical.curie", is("CHEBI:46631")).
             body("results[4].negated", is(true)).
             // body("results[4].with", containsInAnyOrder("HGNC:1121", "HGNC:323")).
+            // body("results[4].evidenceCodes", hasSize(1)).
+            // body("results[4].evidenceCodes[0].curie", is("ECO:0000033")).
             body("results[4].referenceList", hasSize(1)).
-            body("results[4].referenceList[0].curie", is("PMID:25920554")).
-            body("results[4].evidenceCodes", hasSize(1)).
-            body("results[4].evidenceCodes[0].curie", is("ECO:0000033"));
+            body("results[4].referenceList[0].curie", is("PMID:25920554"));
+    }
+    
+    @Test
+    @Order(12)
+    public void geneDiseaseAnnotationBulkUploadSecondaryAnnotation() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/02_all_fields_gene_secondary_annotation.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/mgiAnnotationFile").
+            then().
+            statusCode(200);
+    
+        
+        // check entity count and fields correctly read
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=0").
+            then().
+            statusCode(200).
+            body("totalResults", is(406)); // 59 MGI annotations replaced with 0 as secondary annotations ignored
+    }
+    
+    @Test
+    @Order(13)
+    public void agmDiseaseAnnotationBulkUploadCheckFields() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/03_all_fields_agm_primary_annotation.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/wbAnnotationFile").
+            then().
+            statusCode(200);
+    
+        
+        // check entity count and fields correctly read
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=27").
+            then().
+            statusCode(200).
+            body("totalResults", is(276)). // 131 WB annotations replaced with 1
+            body("results", hasSize(6)).
+            body("results[5].subject.curie", is("WB:WBStrain00005113")).
+            body("results[5].object.curie", is("DOID:4")).
+            body("results[5].diseaseRelation", is("is_model_of")).
+            // body("results[5].conditionRelations", hasSize(1)).
+            // body("results[5].conditionRelations[0].conditionRelationType", is("induces")).
+            // body("results[5].conditionRelations[0].conditions", hasSize(1)).
+            // body("results[5].conditionRelations[0].conditions[0].conditionClass.curie", is("ZECO:0000101")).
+            // body("results[5].conditionRelations[0].conditions[0].conditionId.curie", is("XCO:0000131")).
+            // body("results[5].conditionRelations[0].conditions[0].conditionStatement", is("chemical treatment:4-nitroquinoline N-oxide")).
+            // body("results[5].conditionRelations[0].conditions[0].conditionQuantity", is("some amount")).
+            // body("results[5].conditionRelations[0].conditions[0].conditionAnatomy.curie", is("ZFA:0000001")).
+            // body("results[5].conditionRelations[0].conditions[0].conditionGeneOntology.curie", is("GO:0007569")).
+            // body("results[5].conditionRelations[0].conditions[0].conditionTaxon", is("NCBITaxon:1781")).
+            // body("results[5].conditionRelations[0].conditions[0].conditionChemical.curie", is("CHEBI:46631")).
+            body("results[5].negated", is(true)).
+            // body("results[5].with", containsInAnyOrder("HGNC:1121", "HGNC:323")).
+            // body("results[5].evidenceCodes", hasSize(1)).
+            // body("results[5].evidenceCodes[0].curie", is("ECO:0000033"))
+            body("results[5].referenceList", hasSize(1)).
+            body("results[5].referenceList[0].curie", is("PMID:25920554"));
+    }
+    
+    @Test
+    @Order(14)
+    public void alleleDiseaseAnnotationBulkUploadCheckFields() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/04_all_fields_allele_primary_annotation.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/zfinAnnotationFile").
+            then().
+            statusCode(200);
+    
+        
+        // check entity count and fields correctly read
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=25").
+            then().
+            statusCode(200).
+            body("totalResults", is(257)). // 20 ZFIN annotation replaced with 1
+            body("results", hasSize(7)).
+            body("results[6].subject.curie", is("ZFIN:ZDB-ALT-200608-1")).
+            body("results[6].object.curie", is("DOID:4")).
+            body("results[6].diseaseRelation", is("is_implicated_in")).
+            // body("results[6].conditionRelations", hasSize(1)).
+            // body("results[6].conditionRelations[0].conditionRelationType", is("induces")).
+            // body("results[6].conditionRelations[0].conditions", hasSize(1)).
+            // body("results[6].conditionRelations[0].conditions[0].conditionClass.curie", is("ZECO:0000101")).
+            // body("results[6].conditionRelations[0].conditions[0].conditionId.curie", is("XCO:0000131")).
+            // body("results[6].conditionRelations[0].conditions[0].conditionStatement", is("chemical treatment:4-nitroquinoline N-oxide")).
+            // body("results[6].conditionRelations[0].conditions[0].conditionQuantity", is("some amount")).
+            // body("results[6].conditionRelations[0].conditions[0].conditionAnatomy.curie", is("ZFA:0000001")).
+            // body("results[6].conditionRelations[0].conditions[0].conditionGeneOntology.curie", is("GO:0007569")).
+            // body("results[6].conditionRelations[0].conditions[0].conditionTaxon", is("NCBITaxon:1781")).
+            // body("results[6].conditionRelations[0].conditions[0].conditionChemical.curie", is("CHEBI:46631")).
+            // body("results[6].with", containsInAnyOrder("HGNC:1121", "HGNC:323")).
+            // body("results[6].evidenceCodes", hasSize(1)).
+            // body("results[6].evidenceCodes[0].curie", is("ECO:0000033"))
+            body("results[6].referenceList", hasSize(1)).
+            body("results[6].referenceList[0].curie", is("PMID:25920554")).
+            body("results[6].negated", is(true));
+    }
+    
+    @Test
+    @Order(15)
+    public void diseaseAnnotationBulkUploadNoDOid() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/05_no_doid.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/rgdAnnotationFile").
+            then().
+            statusCode(200);
+    
+        // check entity count   
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=0").
+            then().
+            statusCode(200).
+            body("totalResults", is(103)); // 154 annotations replaced with 0
+    }
+    
+    @Test
+    @Order(16)
+    public void diseaseAnnotationBulkUploadNoDataProvider() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/06_no_data_provider.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/sgdAnnotationFile").
+            then().
+            statusCode(200);
+    
+        
+        // check entity count
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=0").
+            then().
+            statusCode(200).
+            body("totalResults", is(64)); // 40 SGD annotations replaced with 1
+    }
+    
+    @Test
+    @Order(17)
+    public void diseaseAnnotationBulkUploadNoDataProviderXref() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/07_no_data_provider_cross_reference.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/sgdAnnotationFile").
+            then().
+            statusCode(200);
+    
+        
+        // check entity count
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=0").
+            then().
+            statusCode(200).
+            body("totalResults", is(64)); // Count remains the same as SGD annotation replaced
+    }
+    
+    @Test
+    @Order(18)
+    public void diseaseAnnotationBulkUploadNoDataProviderType() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/08_no_data_provider_type.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/wbAnnotationFile").
+            then().
+            statusCode(200);
+    
+        
+        // check entity count
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=0").
+            then().
+            statusCode(200).
+            body("totalResults", is(64)); // Count remains the same as WB annotation replaced
+    }
+    
+    @Test
+    @Order(19)
+    public void diseaseAnnotationBulkUploadNoDateAssigned() throws Exception {
+        String content = Files.readString(Path.of("src/test/resources/bulk/04_disease_annotation/09_no_date_assigned.json"));
+        
+        // upload file
+        RestAssured.given().
+            contentType("application/json").
+            body(content).
+            when().
+            post("/api/disease/bulk/wbAnnotationFile").
+            then().
+            statusCode(200);
+    
+        
+        // check entity count
+        RestAssured.given().
+            when().
+            header("Content-Type", "application/json").
+            body("{}").
+            post("/api/disease-annotation/find?limit=10&page=0").
+            then().
+            statusCode(200).
+            body("totalResults", is(63)); // 1 WB annotation replaced with 0
     }
     
 }
