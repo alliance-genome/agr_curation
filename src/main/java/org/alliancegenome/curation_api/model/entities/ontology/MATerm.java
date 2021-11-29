f58b101322dffcf8e500458c165eee2a2b0abcd7
Olin Blodgett <nilo@nilosplace.net>
2021-11-29 14:36:22 -0500

    Fixed inheritance

--- src/main/java/org/alliancegenome/curation_api/model/entities/ontology/MATerm.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/ontology/MATerm.java
@@ -13,6 +13,6 @@ import lombok.*;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
 @ToString(callSuper = true)
-public class MATerm extends OntologyTerm {
+public class MATerm extends AnatomicalTerm {
 
 }
