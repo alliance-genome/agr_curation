d23f4f6b1b19db2d2126f0afcffc44af31779b45
Andrés Becerra <abecerra@ebi.ac.uk>
2021-12-02 22:30:02 -0500

    SCRUM-903: CHEBITerm CRUD

---
+++ src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/CHEBITermController.java
@@ -0,0 +1,23 @@
+package org.alliancegenome.curation_api.controllers.crud.ontology;
+
+import org.alliancegenome.curation_api.base.BaseOntologyTermController;
+import org.alliancegenome.curation_api.dao.CHEBITermDAO;
+import org.alliancegenome.curation_api.interfaces.crud.ontology.CHEBITermRESTInterface;
+import org.alliancegenome.curation_api.model.entities.ontology.CHEBITerm;
+import org.alliancegenome.curation_api.services.ontology.CHEBITermService;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+@RequestScoped
+public class CHEBITermController extends BaseOntologyTermController<CHEBITermService, CHEBITerm, CHEBITermDAO> implements CHEBITermRESTInterface {
+    @Inject
+    CHEBITermService chebiTermService;
+
+    @Override
+    @PostConstruct
+    protected void init() {
+        setService(chebiTermService);
+    }
+}
