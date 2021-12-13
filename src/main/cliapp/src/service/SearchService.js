997784fea93687a287068c5f8c125e47459cc925
Adam Gibson <agibson@mcw.edu>
2021-12-13 13:22:16 -0600

    SCRUM-970 added secondary sorting fields

--- src/main/cliapp/src/service/SearchService.js
+++ src/main/cliapp/src/service/SearchService.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
+import { getSecondarySorts } from '../utils/utils';
 
 export class SearchService {
 
     search(endpoint, rows, page, sorts, filters) {
         const searchOptions = {};
-        if(!sorts){
+        if (!sorts) {
             sorts = [];
         }
+        sorts = getSecondarySorts(sorts);
+        // console.log(sorts);
         searchOptions["searchFilters"] = filters;
         searchOptions["sortOrders"] = sorts;
-        //console.log(searchOptions);
+        // console.log(searchOptions);
         return axios.post(`/api/${endpoint}/search?limit=${rows}&page=${page}`, searchOptions).then(res => res.data);
     }
 }
--- src/main/cliapp/src/utils/utils.js
+++ src/main/cliapp/src/utils/utils.js
@@ -1,36 +1,77 @@
-export function returnSorted(event, originalSort){
-    
+export function returnSorted(event, originalSort) {
+
     let found = false;
     let replace = false;
     let newSort = [...originalSort];
 
-    console.log(event);
-    console.log(newSort);
-    if(event.multiSortMeta.length > 0){
+    // console.log(event);
+    // console.log(newSort);
+    if (event.multiSortMeta.length > 0) {
         newSort.forEach((o) => {
             if (o.field === event.multiSortMeta[0].field) {
-                if(o.order === event.multiSortMeta[0].order){
+                if (o.order === event.multiSortMeta[0].order) {
                     replace = true;
                     found = true;
-                } else{
+                } else {
                     o.order = event.multiSortMeta[0].order;
                     found = true;
                 }
             }
         });
-    }else {
+    } else {
         newSort = [];
     }
 
     if (!found) {
         return newSort.concat(event.multiSortMeta);
     } else {
-        if(replace){
+        if (replace) {
             return event.multiSortMeta;
-        }else{
+        } else {
             return newSort;
         }
     }
 
 }
 
+export function getSecondarySorts(sorts) {
+    const newSorts = [];
+    sorts.forEach(sort => {
+        const secondarySorts = findSecondarySortFields(sort.field, sort.order);
+        newSorts.push(sort);
+        secondarySorts.forEach(secondarySort => {
+            if (secondarySort.field) {
+                newSorts.push(secondarySort);
+            }
+        })
+    })
+
+    return newSorts;
+}
+
+export function findSecondarySortFields(sortField, sortOrder) {
+    let newSortFields = [];
+    let newSorts = [];
+
+    switch (sortField) {
+        case 'object.name':
+            newSortFields = ['object.curie', 'object.namespace'];
+            break;
+        case 'with.symbol':
+            newSortFields = ['with.curie'];
+            break;
+        default:
+            newSortFields = [null];
+            break;
+    }
+
+    newSortFields.forEach(field => {
+        let newSort = {}
+        newSort["field"] = field;
+        newSort["order"] = sortOrder;
+        newSorts.push(newSort);
+    })
+
+    return newSorts;
+
+}
