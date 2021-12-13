c63bc3675bbfaae04d7ad2f45a2c6d412f7fe178
markquintontulloch <markquintontulloch@gmail.com>
2021-12-13 13:31:58 +0000

    Add additional fields to disease autocomplete

--- src/main/cliapp/src/components/DiseaseEditor.js
+++ src/main/cliapp/src/components/DiseaseEditor.js
@@ -33,7 +33,66 @@ export const DiseaseEditor = ({ rowProps, searchService, setDiseaseAnnotations, 
     };
     
     const diseaseItemTemplate = (item) => {
-        return <div>{item.curie} ({item.name})</div>;
+        let inputValue = rowProps.rowData.object.curie.toLowerCase();
+        let str = "";
+        let synonymsStr = "";
+        let isSynonym = false;
+        let crossReferencesStr = "";
+        let isCrossReference = false;
+        let secondaryIdentifiersStr = "";
+        let isSecondaryIdentifier = false;
+        autocompleteFields.forEach( field => {
+            if(field === "synonyms" && item["synonyms"]){
+                for(let i=0; i< item["synonyms"].length ; i++ ) {
+                    if(item["synonyms"][i]) {
+                        if (item["synonyms"][i].toLowerCase().indexOf(inputValue) >= 0) {
+                            synonymsStr +=  item["synonyms"][i] + ", ";
+                            isSynonym = true;
+                        }
+                    }
+                }
+                if(isSynonym){
+                    str += "Synonym: " + synonymsStr;
+                }
+            }else if(field === "crossReferences.curie" && item["crossReferences"]){
+                for(let i=0; i< item["crossReferences"].length ; i++ ) {
+                    if(item["crossReferences"][i].curie) {
+                        if (item["crossReferences"][i].curie.toString().toLowerCase().indexOf(inputValue) >= 0) {
+                            crossReferencesStr += item["crossReferences"][i].curie.toString() + ", ";
+                            isCrossReference = true;
+                        }
+                    }
+                }
+                if(isCrossReference){
+                    str += "CrossReferences: " + crossReferencesStr;
+                }
+            }else {
+                if (item[field]) {
+                    if(field === "secondaryIdentifiers") {
+                        for(var i=0; i< item["secondaryIdentifiers"].length ; i++ ) {
+                            if (item["secondaryIdentifiers"][i].toLowerCase().indexOf(inputValue) >= 0) {
+                                secondaryIdentifiersStr += item["secondaryIdentifiers"][i] + ", ";
+                                isSecondaryIdentifier = true;
+                            }
+                        }
+                        if(isSecondaryIdentifier){
+                            str += "SecondaryIdentifiers: " + secondaryIdentifiersStr;
+                        }
+                    }else {
+                        if (item[field].toString().toLowerCase().indexOf(inputValue) >= 0) {
+                            if (field !== "curie" && field !== "name")
+                                str += field + ": " + item[field].toString() + ", ";
+                        }
+                    }
+                }
+            }
+        });
+        str = str.length > 0 ? str.substring(0 , str.length-2) : " "; //To remove trailing comma
+        if(item.name){
+            return <div dangerouslySetInnerHTML={{__html: item.name + ' (' + item.curie + ') ' + str.toString()}}/>;
+        }else {
+            return <div>{item.curie + str.toString()}</div>;
+        }
     };
 
     return (
