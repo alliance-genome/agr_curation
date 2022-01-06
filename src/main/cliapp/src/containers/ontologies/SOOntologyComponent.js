f2b0e3f9c61edebead180470673bf05311bfa343
markquintontulloch <markquintontulloch@gmail.com>
2022-01-06 12:20:36 +0000

    Add SOTerm UI components

---
+++ src/main/cliapp/src/containers/ontologies/SOOntologyComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { OntologyTable } from './OntologyTable';
+
+export const SOOntologyComponent = () => {
+  const columns = [
+    { field: "curie", header: "Curie" },
+    { field: "name", header: "Name" },
+    { field: "namespace", header: "Name Space" },
+    { field: "definition", header: "Definition" }
+  ]
+
+  return (
+    < OntologyTable
+      endpoint={"soterm"}
+      ontologyAbbreviation={"SO"}
+      columnMap={columns}
+    />
+  )
+}
