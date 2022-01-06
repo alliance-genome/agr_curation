112a36949f1786bc6f2492fe1d1939c424e94eb6
markquintontulloch <markquintontulloch@gmail.com>
2022-01-06 12:19:54 +0000

    Add SOTerm backend components

---
+++ src/main/java/org/alliancegenome/curation_api/services/ontology/SoTermService.java
@@ -0,0 +1,22 @@
+package org.alliancegenome.curation_api.services.ontology;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+import org.alliancegenome.curation_api.base.services.BaseOntologyTermService;
+import org.alliancegenome.curation_api.dao.ontology.SoTermDAO;
+import org.alliancegenome.curation_api.model.entities.ontology.SOTerm;
+
+@RequestScoped
+public class SoTermService extends BaseOntologyTermService<SOTerm, SoTermDAO> {
+
+    @Inject SoTermDAO soTermDAO;
+
+    @Override
+    @PostConstruct
+    protected void init() {
+        setSQLDao(soTermDAO);
+    }
+    
+}
