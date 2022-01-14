deb9eb64c6ffa3bcbc908480ade4d5a4e6b9a6e0
markquintontulloch <markquintontulloch@gmail.com>
2022-01-14 00:28:16 +0000

    Add CRUD validation for alleles

--- src/main/java/org/alliancegenome/curation_api/services/AlleleService.java
+++ src/main/java/org/alliancegenome/curation_api/services/AlleleService.java
@@ -7,18 +7,29 @@ import javax.transaction.Transactional;
 
 import org.alliancegenome.curation_api.base.services.BaseCrudService;
 import org.alliancegenome.curation_api.dao.AlleleDAO;
+import org.alliancegenome.curation_api.model.entities.AffectedGenomicModel;
 import org.alliancegenome.curation_api.model.entities.Allele;
 import org.alliancegenome.curation_api.model.ingest.json.dto.AlleleDTO;
+import org.alliancegenome.curation_api.response.ObjectResponse;
+import org.alliancegenome.curation_api.services.helpers.validators.AlleleValidator;
 
 @RequestScoped
 public class AlleleService extends BaseCrudService<Allele, AlleleDAO> {
 
     @Inject AlleleDAO alleleDAO;
-
+    @Inject AlleleValidator alleleValidator;
+    
     @Override
     @PostConstruct
     protected void init() {
         setSQLDao(alleleDAO);
+    }
+    
+    @Override
+    @Transactional
+    public ObjectResponse<Allele> update(Allele uiEntity) {
+        Allele dbEntity = alleleValidator.validateAnnotation(uiEntity);
+        return new ObjectResponse<Allele>(alleleDAO.persist(dbEntity));
     }
 
     @Transactional
---
+++ src/main/java/org/alliancegenome/curation_api/services/helpers/validators/AlleleValidator.java
@@ -0,0 +1,83 @@
+package org.alliancegenome.curation_api.services.helpers.validators;
+
+import java.util.*;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+import org.alliancegenome.curation_api.dao.*;
+import org.alliancegenome.curation_api.exceptions.ApiErrorException;
+import org.alliancegenome.curation_api.model.entities.*;
+import org.alliancegenome.curation_api.response.ObjectResponse;
+import org.apache.commons.lang3.*;
+
+@RequestScoped
+public class AlleleValidator extends GenomicEntityValidator {
+    
+    @Inject
+    AlleleDAO alleleDAO;
+    
+    public Allele validateAnnotation(Allele uiEntity) {
+        response = new ObjectResponse<>(uiEntity);
+        
+        String curie = validateCurie(uiEntity);
+        if (curie == null) {
+            throw new ApiErrorException(response);
+        }
+        
+        Allele dbEntity = alleleDAO.find(curie);
+        if (dbEntity == null) {
+            addMessageResponse("Could not find allele with curie: [" + curie + "]");
+            throw new ApiErrorException(response);
+        }
+        
+
+        String errorTitle = "Could not update allele [" + curie + "]";
+        
+        String name = validateName(uiEntity);
+        if (name != null) dbEntity.setName(name);
+        
+        String taxon = validateTaxon(uiEntity);
+        if (taxon != null) dbEntity.setTaxon(taxon);
+        
+        String symbol = validateSymbol(uiEntity);
+        if (symbol != null) dbEntity.setSymbol(symbol);
+        
+        if (uiEntity.getSynonyms() != null) {
+            dbEntity.setSynonyms(uiEntity.getSynonyms());
+        }
+
+        if (uiEntity.getSecondaryIdentifiers() != null) {
+            dbEntity.setSecondaryIdentifiers(uiEntity.getSecondaryIdentifiers());
+        }
+
+        if (uiEntity.getCrossReferences() != null) {
+            dbEntity.setCrossReferences(uiEntity.getCrossReferences());
+        }
+        
+        if (uiEntity.getGenomicLocations() != null) {
+            dbEntity.setGenomicLocations(uiEntity.getGenomicLocations());
+        }
+        
+        if (uiEntity.getDescription() != null) {
+            dbEntity.setDescription(uiEntity.getDescription());
+        }
+        
+        if (response.hasErrors()) {
+            response.setErrorMessage(errorTitle);
+            throw new ApiErrorException(response);
+        }
+        
+        return dbEntity;
+    }
+    
+    private String validateSymbol(Allele uiEntity) {
+        String symbol = uiEntity.getSymbol();
+        if (StringUtils.isEmpty(symbol)) {
+            addMessageResponse("symbol", requiredMessage);
+            return null;
+        }
+        return symbol;
+    }
+
+}
