27a97263ff70272f0f99195aee0675016f99d912
Adam Gibson <agibson@mcw.edu>
2022-01-17 15:27:21 -0600

    SCRUM-1046 added logout

--- src/main/cliapp/src/components/AppTopbar.js
+++ src/main/cliapp/src/components/AppTopbar.js
@@ -1,17 +1,15 @@
 import React from 'react';
-
 export const AppTopbar = (props) => {
 
-  const button = props.authState && props.authState.isAuthenticated ?
-    <button onClick={props.logout} className="p-link layout-menu-button p-col-1" style={{'fontSize': '1.25em'}} >Logout</button> :
+  const sidebarToggle = props.authState && props.authState.isAuthenticated ?
+    <button type="button" className="p-link layout-menu-button" onClick={props.onToggleMenu}>
+      <span className="pi pi-bars" />
+    </button> :
     null;
 
   return (
-    <div className="layout-topbar clearfix p-grid">
-      <button type="button" className="p-link layout-menu-button p-col-10" onClick={props.onToggleMenu}>
-        <span className="pi pi-bars" />
-      </button>
-      {button}
+    <div className="layout-topbar clearfix">
+      {sidebarToggle}
     </div>
   );
 }
--- src/main/cliapp/src/containers/layout/SiteLayout.js
+++ src/main/cliapp/src/containers/layout/SiteLayout.js
@@ -9,6 +9,7 @@ import { useOktaAuth } from '@okta/okta-react';
 
 import { AppTopbar } from '../../components/AppTopbar';
 import { AppMenu } from '../../components/AppMenu';
+import { AppProfile } from '../../AppProfile';
 import { AppConfig } from '../../components/AppConfig';
 
 import { ApiVersionService } from '../../service/ApiVersionService';
@@ -23,61 +24,84 @@ export const SiteLayout = (props) => {
   const sidebar = useRef();
   const [apiVersion, setApiVersion] = useState({ "version": "0.0.0" });
   const { authState, oktaAuth } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
+
+
+  useEffect(() => {
+    if (!authState || !authState.isAuthenticated) {
+      setUserInfo(null);
+    } else {
+      oktaAuth.getUser().then((info) => {
+        setUserInfo(info);
+      }).catch((err) => {
+        console.error(err);
+      });
+    }
+  }, [authState, oktaAuth]);
+
   const logout = async () => await oktaAuth.signOut();
+  const appProfile = userInfo ? <AppProfile authState={authState} logout={logout} email={userInfo.email} /> :
+    null;
 
-  let menu;
+  useEffect(() => {
+    if (!authState || !authState.isAuthenticated) {
+      setSidebarActive(false);
+    }else{
+      setSidebarActive(true);
+    }
+  }, [authState])
 
-  if (authState && authState.isAuthenticated) {
-    menu = [
-      { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
-      {
-        label: 'Data Tables', icon: 'pi pi-fw pi-sitemap',
-        items: [
-          { label: 'Genes', icon: 'pi pi-fw pi-home', to: '/genes' },
-          { label: 'Alleles', icon: 'pi pi-fw pi-home', to: '/alleles' },
-          { label: 'Disease Annotations', icon: 'pi pi-fw pi-home', to: '/diseaseAnnotations' },
-          { label: 'Affected Genomic Models', icon: 'pi pi-fw pi-home', to: '/agms' },
-          { label: 'Molecules', icon: 'pi pi-fw pi-home', to: '/molecules' }
-        ]
-      },
-      {
-        label: 'Ontologies', icon: 'pi pi-fw pi-sitemap',
-        items: [
-          { label: 'Chemical Entities of Biological Interest (ChEBI)', icon: 'pi pi-fw pi-home', to: '/ontology/chebi' },
-          { label: 'Disease Ontology (DO)', icon: 'pi pi-fw pi-home', to: '/ontology/do' },
-          { label: 'Evidence & Conclusion Ontology (ECO)', icon: 'pi pi-fw pi-home', to: '/ontology/eco' },
-          { label: 'Gene Ontology (GO)', icon: 'pi pi-fw pi-home', to: '/ontology/go' },
-          { label: 'Sequence Ontology (SO)', icon: 'pi pi-fw pi-home', to: '/ontology/so' },
-          { label: 'Mouse adult gross anatomy Ontology (MA)', icon: 'pi pi-fw pi-home', to: '/ontology/ma' },
-          { label: 'The Mammalian Phenotype Ontology (MP)', icon: 'pi pi-fw pi-home', to: '/ontology/mp' },
-          { label: 'Drosophila Anatomy Ontology (DAO)', icon: 'pi pi-fw pi-home', to: '/ontology/dao' },
-          { label: 'Zebrafish Anatomy Ontology (ZFA)', icon: 'pi pi-fw pi-home', to: '/ontology/zfa' },
-          { label: 'Mouse Developmental Anatomy Ontology (EMAPA)', icon: 'pi pi-fw pi-home', to: '/ontology/emapa' },
-          { label: 'C. elegans Gross Anatomy Ontology (WBbt)', icon: 'pi pi-fw pi-home', to: '/ontology/wbbt' },
-          { label: 'Experimental condition ontology (XCO)', icon: 'pi pi-fw pi-home', to: '/ontology/xco' },
-          { label: 'Zebrafish Experimental Conditions Ontology (ZECO)', icon: 'pi pi-fw pi-home', to: '/ontology/zeco' }
-        ]
-      },
-      {
-        label: 'Controlled Vocabularies', icon: 'pi pi-fw pi-sitemap',
-        items: [
-          { label: 'Terms', icon: 'pi pi-fw pi-home', to: '/vocabterms' }
-        ]
-      },
-      {
-        label: 'Other Links', icon: 'pi pi-fw pi-sitemap',
-        items: [
-          { label: 'FMS Data Files', icon: 'pi pi-fw pi-home', to: '/fmspage' },
-          { label: 'Data Loads', icon: 'pi pi-fw pi-home', to: '/dataloads' },
-          { label: 'Swagger UI', icon: 'pi pi-fw pi-home', url: '/swagger-ui' },
-        ]
-      }
-    ]
-  } else {
-    menu = [
-      { label: 'Login', icon: 'pi pi-fw pi-home', to: '/login' }
-    ];
-  }
+  const menu = [
+    { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
+    {
+      label: 'Data Tables', icon: 'pi pi-fw pi-sitemap',
+      items: [
+        { label: 'Genes', icon: 'pi pi-fw pi-home', to: '/genes' },
+        { label: 'Alleles', icon: 'pi pi-fw pi-home', to: '/alleles' },
+        { label: 'Disease Annotations', icon: 'pi pi-fw pi-home', to: '/diseaseAnnotations' },
+        { label: 'Affected Genomic Models', icon: 'pi pi-fw pi-home', to: '/agms' },
+        { label: 'Molecules', icon: 'pi pi-fw pi-home', to: '/molecules' }
+      ]
+    },
+    {
+      label: 'Ontologies', icon: 'pi pi-fw pi-sitemap',
+      items: [
+        { label: 'Chemical Entities of Biological Interest (ChEBI)', icon: 'pi pi-fw pi-home', to: '/ontology/chebi' },
+        { label: 'Disease Ontology (DO)', icon: 'pi pi-fw pi-home', to: '/ontology/do' },
+        { label: 'Evidence & Conclusion Ontology (ECO)', icon: 'pi pi-fw pi-home', to: '/ontology/eco' },
+        { label: 'Gene Ontology (GO)', icon: 'pi pi-fw pi-home', to: '/ontology/go' },
+        { label: 'Sequence Ontology (SO)', icon: 'pi pi-fw pi-home', to: '/ontology/so' },
+        { label: 'Mouse adult gross anatomy Ontology (MA)', icon: 'pi pi-fw pi-home', to: '/ontology/ma' },
+        { label: 'The Mammalian Phenotype Ontology (MP)', icon: 'pi pi-fw pi-home', to: '/ontology/mp' },
+        { label: 'Drosophila Anatomy Ontology (DAO)', icon: 'pi pi-fw pi-home', to: '/ontology/dao' },
+        { label: 'Zebrafish Anatomy Ontology (ZFA)', icon: 'pi pi-fw pi-home', to: '/ontology/zfa' },
+        { label: 'Mouse Developmental Anatomy Ontology (EMAPA)', icon: 'pi pi-fw pi-home', to: '/ontology/emapa' },
+        { label: 'C. elegans Gross Anatomy Ontology (WBbt)', icon: 'pi pi-fw pi-home', to: '/ontology/wbbt' },
+        { label: 'Experimental condition ontology (XCO)', icon: 'pi pi-fw pi-home', to: '/ontology/xco' },
+        { label: 'Zebrafish Experimental Conditions Ontology (ZECO)', icon: 'pi pi-fw pi-home', to: '/ontology/zeco' }
+      ]
+    },
+    {
+      label: 'Controlled Vocabularies', icon: 'pi pi-fw pi-sitemap',
+      items: [
+        { label: 'Terms', icon: 'pi pi-fw pi-home', to: '/vocabterms' }
+      ]
+    },
+    {
+      label: 'Other Links', icon: 'pi pi-fw pi-sitemap',
+      items: [
+        { label: 'FMS Data Files', icon: 'pi pi-fw pi-home', to: '/fmspage' },
+        { label: 'Data Loads', icon: 'pi pi-fw pi-home', to: '/dataloads' },
+        { label: 'Swagger UI', icon: 'pi pi-fw pi-home', url: '/swagger-ui' },
+      ]
+    }
+  ]
+
+  const appMenu = authState && authState.isAuthenticated ?
+    <AppMenu model={menu} onMenuItemClick={onMenuItemClick} /> :
+    null;
+
+
   const history = useHistory();
 
   let menuClick = false;
@@ -185,13 +209,13 @@ export const SiteLayout = (props) => {
           <div style={{ cursor: 'pointer' }} onClick={() => history.push('/')}>
             <div class="card">AGR Curation: {apiVersion.version}<br /></div>
           </div>
-          <AppMenu model={menu} onMenuItemClick={onMenuItemClick} />
+          {appProfile}
+          {appMenu}
         </div>
       </CSSTransition>
 
       <AppConfig rippleEffect={ripple} onRippleEffect={onRipple} inputStyle={inputStyle} onInputStyleChange={onInputStyleChange}
         layoutMode={layoutMode} onLayoutModeChange={onLayoutModeChange} layoutColorMode={layoutColorMode} onColorModeChange={onColorModeChange} />
-
 
       <div className="layout-main">
         {children}
