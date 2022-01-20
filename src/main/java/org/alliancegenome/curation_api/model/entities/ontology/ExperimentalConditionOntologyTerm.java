4ba202d661612358c8847ae095f2238894f37dca
markquintontulloch <markquintontulloch@gmail.com>
2022-01-20 22:28:56 +0000

    Created and updated entities related to experimantal conditions
    Created ExperimentalCondition and ConditionRelation entities.
    Created ExperimentalConditionOntologyTerm entity, inherited by
    XcoTerm and ZecoTerm entities.
    Created ChemicalTerm entity - placeholder for now until we've
    decided how to deal with CHEBITerm and Molecule entities being
    referred to by the same field.
    Removed Condition entity (did not match LinkML schema, replaced by
    ExperimentalCondition entity).
    Added conditionRelations field to DiseaseRelation entity.

---
+++ src/main/java/org/alliancegenome/curation_api/model/entities/ontology/ExperimentalConditionOntologyTerm.java
@@ -0,0 +1,8 @@
+package org.alliancegenome.curation_api.model.entities.ontology;
+
+import lombok.*;
+
+@Data @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+public abstract class ExperimentalConditionOntologyTerm extends OntologyTerm {
+
+}
