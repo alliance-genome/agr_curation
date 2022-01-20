8fe91e2a7adf51dda948045fa7b5cc14eeaa4c04
Adam Gibson <agibson@mcw.edu>
2022-01-20 15:14:52 -0600

    SCRUM-1045 added BaseAuthService for authTokens

--- src/main/cliapp/src/service/DiseaseAnnotationService.js
+++ src/main/cliapp/src/service/DiseaseAnnotationService.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
+import { BaseAuthService } from './BaseAuthService';
 
-export class DiseaseAnnotationService {
+export class DiseaseAnnotationService extends BaseAuthService {
+  //eslint-disable-next-line
+  constructor(authState) {
+    super(authState);
+  }
 
-    saveDiseaseAnnotation(updatedAnnotation){
-        const { type } = updatedAnnotation;
-        let endpoint;
-        if(type in typeEndpoints){
-            endpoint = typeEndpoints[type];
-        }
-        return axios.put(`api/${endpoint}-disease-annotation`, updatedAnnotation)
+  saveDiseaseAnnotation(updatedAnnotation) {
+    const { type } = updatedAnnotation;
+    let endpoint;
+    if (type in typeEndpoints) {
+      endpoint = typeEndpoints[type];
     }
+    return axios.put(`api/${endpoint}-disease-annotation`, updatedAnnotation, this.apiAuthHeader);
+  }
 }
 
 const typeEndpoints = {
-    "AGMDiseaseAnnotation": "agm",
-    "AlleleDiseaseAnnotation": "allele",
-    "GeneDiseaseAnnotation": "gene"
+  "AGMDiseaseAnnotation": "agm",
+  "AlleleDiseaseAnnotation": "allele",
+  "GeneDiseaseAnnotation": "gene"
 }
 
 
