bcf7a5fe6f43e1c8738b6b4e34540bd6edc1d49e
markquintontulloch <markquintontulloch@gmail.com>
2022-02-23 22:34:05 +0000

    Create Note DTO

---
+++ src/main/java/org/alliancegenome/curation_api/model/ingest/dto/NoteDTO.java
@@ -0,0 +1,28 @@
+package org.alliancegenome.curation_api.model.ingest.dto;
+
+import java.util.List;
+
+import org.alliancegenome.curation_api.view.View;
+
+import com.fasterxml.jackson.annotation.*;
+
+import lombok.*;
+
+@Setter
+@Getter
+public class NoteDTO {
+    
+    @JsonView({View.FieldsOnly.class})
+    private Boolean internal = true;
+    
+    @JsonView({View.FieldsOnly.class})
+    @JsonProperty("free_text")
+    private String freeText;
+    
+    @JsonView({View.FieldsOnly.class})
+    @JsonProperty("note_type")
+    private String noteType;
+    
+    @JsonView({View.FieldsAndLists.class})
+    private List<String> references;
+}
