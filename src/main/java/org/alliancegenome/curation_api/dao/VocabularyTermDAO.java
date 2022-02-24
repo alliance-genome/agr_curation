11498b1eca89d66f69b2eb420a77df575338283c
markquintontulloch <markquintontulloch@gmail.com>
2022-02-24 17:12:37 +0000

    Add method to retrieve term from specified vocabulary

--- src/main/java/org/alliancegenome/curation_api/dao/VocabularyTermDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/VocabularyTermDAO.java
@@ -1,14 +1,43 @@
 package org.alliancegenome.curation_api.dao;
 
+import java.util.HashMap;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import org.alliancegenome.curation_api.base.dao.BaseSQLDAO;
+import org.alliancegenome.curation_api.model.entities.Vocabulary;
 import org.alliancegenome.curation_api.model.entities.VocabularyTerm;
+import org.alliancegenome.curation_api.response.SearchResponse;
 
 @ApplicationScoped
 public class VocabularyTermDAO extends BaseSQLDAO<VocabularyTerm> {
 
+    @Inject
+    VocabularyDAO vocabularyDAO;
+    @Inject
+    VocabularyTermDAO vocabularyTermDAO;
+    
     protected VocabularyTermDAO() {
         super(VocabularyTerm.class);
+    }
+    
+    public VocabularyTerm getTermInVocabulary (String termName, String vocabularyName) {
+        
+        Vocabulary vocabulary;
+        SearchResponse<Vocabulary> vocabularySearchResponse = vocabularyDAO.findByField("name", vocabularyName);
+        if (vocabularySearchResponse == null || vocabularySearchResponse.getSingleResult() == null) {
+            return null;
+        } 
+        vocabulary = vocabularySearchResponse.getSingleResult();
+        
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("name", termName);
+        params.put("vocabulary_id", vocabulary.getId());
+        SearchResponse<VocabularyTerm> vocabularyTermSearchResponse = vocabularyTermDAO.findByParams(null, params);
+        if (vocabularyTermSearchResponse == null || vocabularyTermSearchResponse.getSingleResult() == null) {
+            return null;
+        }
+        return vocabularyTermSearchResponse.getSingleResult();
     }
 }
