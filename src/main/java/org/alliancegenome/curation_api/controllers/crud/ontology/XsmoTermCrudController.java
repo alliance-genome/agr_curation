c64995d02cb0a8b0eb0334ac74f5ac47474ae440
markquintontulloch <markquintontulloch@gmail.com>
2022-03-09 10:56:53 +0000

    Create Xenopus ontology CRUD controllers

---
+++ src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/XsmoTermCrudController.java
@@ -0,0 +1,24 @@
+package org.alliancegenome.curation_api.controllers.crud.ontology;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+import org.alliancegenome.curation_api.base.controllers.BaseOntologyTermController;
+import org.alliancegenome.curation_api.dao.ontology.XsmoTermDAO;
+import org.alliancegenome.curation_api.interfaces.crud.ontology.XsmoTermCrudInterface;
+import org.alliancegenome.curation_api.model.entities.ontology.XSMOTerm;
+import org.alliancegenome.curation_api.services.ontology.XsmoTermService;
+
+@RequestScoped
+public class XsmoTermCrudController extends BaseOntologyTermController<XsmoTermService, XSMOTerm, XsmoTermDAO> implements XsmoTermCrudInterface {
+
+    @Inject XsmoTermService xsmoTermService;
+
+    @Override
+    @PostConstruct
+    public void init() {
+        setService(xsmoTermService, XSMOTerm.class);
+    }
+
+}
