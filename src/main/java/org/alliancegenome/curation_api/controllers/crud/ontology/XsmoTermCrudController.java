bfbab48698e7b01ee64b9e02e2fff7dd1e4a0096
markquintontulloch <markquintontulloch@gmail.com>
2022-03-09 15:53:19 +0000

    Limit XSMO terms to those not in CHEBI

--- src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/XsmoTermCrudController.java
+++ src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/XsmoTermCrudController.java
@@ -8,6 +8,7 @@ import org.alliancegenome.curation_api.base.controllers.BaseOntologyTermControll
 import org.alliancegenome.curation_api.dao.ontology.XsmoTermDAO;
 import org.alliancegenome.curation_api.interfaces.crud.ontology.XsmoTermCrudInterface;
 import org.alliancegenome.curation_api.model.entities.ontology.XSMOTerm;
+import org.alliancegenome.curation_api.services.helpers.GenericOntologyLoadConfig;
 import org.alliancegenome.curation_api.services.ontology.XsmoTermService;
 
 @RequestScoped
@@ -18,7 +19,9 @@ public class XsmoTermCrudController extends BaseOntologyTermController<XsmoTermS
     @Override
     @PostConstruct
     public void init() {
-        setService(xsmoTermService, XSMOTerm.class);
+        GenericOntologyLoadConfig config = new GenericOntologyLoadConfig();
+        config.setIgnoreEntitiesWithChebiXref(true);
+        setService(xsmoTermService, XSMOTerm.class, config);
     }
 
 }
