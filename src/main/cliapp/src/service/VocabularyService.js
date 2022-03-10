a21df541b339683aef8bb73a280e2808e34acb6f
Ketaki Thorat <72769853+kthorat-prog@users.noreply.github.com>
2022-03-10 11:24:07 -0600

    Create & Edit Feature for Controlled Vocab

---
+++ src/main/cliapp/src/service/VocabularyService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { BaseAuthService } from './BaseAuthService';
+
+export class VocabularyService extends BaseAuthService {
+    //eslint-disable-next-line
+    constructor(authState) {
+        super(authState);
+    }
+
+    saveTerm(updatedTerm) { //EDIT
+        return axios.put(`api/vocabularyterm`, updatedTerm, this.apiAuthHeader);
+    }
+
+    createVocabulary(vocabulary){ //new Vocab creation
+        return axios.post(`api/vocabulary`, vocabulary, this.apiAuthHeader);
+    }
+
+    createTerm(term){ //new Term creation
+        return axios.post(`api/vocabularyterm`, term, this.apiAuthHeader);
+    }
+
+    getVocabularies(){ //get all dropdown list of Vocabs
+        return axios.post(`api/vocabulary/find`, {});
+    }
+}
+
+
