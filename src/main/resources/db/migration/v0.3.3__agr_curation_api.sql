c83c3a7412165e020e1edf6cede642238e3008c9
markquintontulloch <markquintontulloch@gmail.com>
2022-03-11 13:44:23 +0000

    Add schema migration file

---
+++ src/main/resources/db/migration/v0.3.3__agr_curation_api.sql
@@ -0,0 +1,72 @@
+ALTER TABLE diseaseannotation
+  ADD COLUMN diseaserelation_id BIGINT,
+  ADD COLUMN diseasegeneticmodifierrelation_id BIGINT,
+  ADD COLUMN annotationtype_id BIGINT,
+  ADD COLUMN geneticsex_id BIGINT;
+
+ALTER TABLE conditionRelation
+  ADD COLUMN conditionrelationtype_id BIGINT;
+
+UPDATE diseaseannotation
+  SET diseaserelation_id = subquery.id
+  FROM (
+      SELECT vocabularyterm.id as id, vocabularyterm.name as name
+        FROM vocabularyterm
+        INNER JOIN vocabulary
+          ON vocabulary.id = vocabularyterm.vocabulary_id
+        WHERE vocabulary.name = 'Disease Relation Vocabulary'
+  ) AS subquery
+  WHERE diseaseannotation.diseaserelation = subquery.name;
+
+UPDATE diseaseannotation
+  SET geneticsex_id = subquery.id
+  FROM (
+    SELECT vocabularyterm.id as id, vocabularyterm.name as name
+      FROM vocabularyterm
+      INNER JOIN vocabulary
+        ON vocabulary.id = vocabularyterm.vocabulary_id
+      WHERE vocabulary.name = 'Genetic sexes'
+  ) AS subquery
+  WHERE diseaseannotation.geneticsex = subquery.name;
+
+UPDATE diseaseannotation
+  SET annotationtype_id = subquery.id
+  FROM (
+    SELECT vocabularyterm.id as id, vocabularyterm.name as name
+      FROM vocabularyterm
+        INNER JOIN vocabulary
+          ON vocabulary.id = vocabularyterm.vocabulary_id
+        WHERE vocabulary.name = 'Annotation types'
+    ) AS subquery
+    WHERE diseaseannotation.annotationtype = subquery.name;
+
+UPDATE diseaseannotation
+  SET annotationtype_id = subquery.id
+  FROM (
+    SELECT vocabularyterm.id as id, vocabularyterm.name as name
+      FROM vocabularyterm
+        INNER JOIN vocabulary
+          ON vocabulary.id = vocabularyterm.vocabulary_id
+        WHERE vocabulary.name = 'Disease genetic modifier relations'
+    ) AS subquery
+    WHERE diseaseannotation.annotationtype = subquery.name;
+
+UPDATE conditionrelation
+  SET conditionrelationtype_id = subquery.id
+  FROM (
+    SELECT vocabularyterm.id as id, vocabularyterm.name as name
+      FROM vocabularyterm
+        INNER JOIN vocabulary
+          ON vocabulary.id = vocabularyterm.vocabulary_id
+        WHERE vocabulary.name = 'Condition relation types'
+    ) AS subquery
+    WHERE conditionrelation.conditionrelationtype = subquery.name;
+
+ALTER TABLE diseaseannotation
+  DROP COLUMN diseaserelation,
+  DROP COLUMN diseasegeneticmodifierrelation,
+  DROP COLUMN annotationtype,
+  DROP COLUMN geneticsex;
+
+ALTER TABLE conditionRelation
+  DROP COLUMN conditionrelationtype;
