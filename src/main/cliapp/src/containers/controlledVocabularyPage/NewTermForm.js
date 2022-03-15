3a73f2db46c1084326c9be6c61d859fce3a08ef6
Ketaki Thorat <72769853+kthorat-prog@users.noreply.github.com>
2022-03-15 11:04:48 -0500

    Changes to updation of new added records

--- src/main/cliapp/src/containers/controlledVocabularyPage/NewTermForm.js
+++ src/main/cliapp/src/containers/controlledVocabularyPage/NewTermForm.js
@@ -1,19 +1,22 @@
 import {Dialog} from "primereact/dialog";
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import {Button} from "primereact/button";
 import {InputText} from "primereact/inputtext";
 import {Dropdown} from "primereact/dropdown";
 import {Toast} from "primereact/toast";
-import {useMutation} from "react-query";
+import {useMutation, useQueryClient} from "react-query";
+import {classNames} from "primereact/utils";
 
-export const NewTermForm = ({ newTermDialog, setNewTermDialog, newTerm, newTermDispatch, vocabularies, obsoleteTerms, vocabularyService }) => {
+export const NewTermForm = ({ newTermDialog, setNewTermDialog, newTerm, newTermDispatch, vocabularies, obsoleteTerms, vocabularyService}) => {
+    const queryClient = useQueryClient();
     const toast_success = useRef(null);
     const toast_error = useRef(null);
+    const [submitted, setSubmitted] = useState(false);
 
     const hideTermDialog = () => {
         newTermDispatch({ type: "RESET" });
         setNewTermDialog(false);
-        /*setDisableFormFields(false);*/
+        setSubmitted(false);
     };
 
     const mutation = useMutation(newTerm => {
@@ -22,20 +25,24 @@ export const NewTermForm = ({ newTermDialog, setNewTermDialog, newTerm, newTermD
 
     const handleTermSubmit = (event) => {
         event.preventDefault();
-        mutation.mutate(newTerm, {
-            onSuccess: () => {
-                toast_success.current.show({ severity: 'success', summary: 'Successful', detail: 'New Term Added' });
-                //queryClient.invalidateQueries('bulkloadtable');
-                newTermDispatch({ type: "RESET" });
-                setNewTermDialog(false);
-                /*setDisableFormFields(false);*/
-            },
-            onError: (error) => {
-                toast_error.current.show([
-                    { life: 7000, severity: 'error', summary: 'Page error: ', detail: error.message, sticky: false }
-                ]);
-            }
-        });
+        setSubmitted(true);
+        if ((newTerm.name && newTerm.name.trim()) && newTerm.vocabulary && newTerm.obsolete!==undefined)
+        {
+            mutation.mutate(newTerm, {
+                onSuccess: (data) => {
+                    queryClient.invalidateQueries('vocabterms');
+                    toast_success.current.show({severity: 'success', summary: 'Successful', detail: 'New Term Added'});
+                    setSubmitted(false);
+                    newTermDispatch({type: "RESET"});
+                    setNewTermDialog(false);
+                },
+                onError: (error) => {
+                    toast_error.current.show([
+                        {life: 7000, severity: 'error', summary: 'Page error: ', detail: error.message, sticky: false}
+                    ]);
+                }
+            });
+        }
     };
 
     const onChange = (e) => {
@@ -74,7 +81,9 @@ export const NewTermForm = ({ newTermDialog, setNewTermDialog, newTerm, newTermD
                                 //placeholder={"Name"}
                                 value={newTerm.name}
                                 onChange={onChange}
-                            />
+                                required
+                                className={classNames({ 'p-invalid': submitted && !newTerm.name })}
+                            />{submitted && !newTerm.name && <small className="p-error">Name is required.</small>}
                         </div>
                         <div className="field">
                             <label htmlFor="abbreviation">Abbreviation</label>
@@ -97,7 +106,9 @@ export const NewTermForm = ({ newTermDialog, setNewTermDialog, newTerm, newTermD
                                 name='vocabulary'
                                 optionLabel='name'
                                 onChange={onChange}
-                            />
+                                required
+                                className={classNames({ 'p-invalid': submitted && !newTerm.vocabulary })}
+                            />{submitted && !newTerm.vocabulary && <small className="p-error">Vocabulary is required.</small>}
                         </div>
                         <div className="field">
                             <label htmlFor="definition">Definition</label>
@@ -121,7 +132,9 @@ export const NewTermForm = ({ newTermDialog, setNewTermDialog, newTerm, newTermD
                                 optionLabel='text'
                                 optionValue='name'
                                 onChange={onChange}
-                            />
+                                required
+                                className={classNames({ 'p-invalid': submitted && newTerm.obsolete === undefined })}
+                            />{submitted && newTerm.obsolete === undefined && <small className="p-error">Obsolete is required.</small>}
                         </div>
                     </form>
                 </div>
