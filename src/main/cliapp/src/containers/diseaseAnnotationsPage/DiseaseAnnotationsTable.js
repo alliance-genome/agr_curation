5afa374174926c0663165df3b757ee41eadaf55e
markquintontulloch <markquintontulloch@gmail.com>
2022-03-17 20:43:55 +0000

    Add 'Mod Entity ID' column

--- src/main/cliapp/src/containers/diseaseAnnotationsPage/DiseaseAnnotationsTable.js
+++ src/main/cliapp/src/containers/diseaseAnnotationsPage/DiseaseAnnotationsTable.js
@@ -28,8 +28,8 @@ import { Tooltip } from 'primereact/tooltip';
 export const DiseaseAnnotationsTable = () => {
   // const defaultColumnNames = ["Unique Id", "Subject", "Disease Relation", "Negated", "Disease", "Reference", "With", "Evidence Code", "Genetic Sex", "Disease Qualifiers",
   //  "SGD Strain Background", "Annotation Type", "Genetic Modifier Relation", "Genetic Modifier", "Data Provider", "Secondary Data Provider", "Modified By", "Date Last Modified", "Created By", "Creation Date", "Related Notes"];
-  const defaultColumnNames = ["Unique Id", "Subject", "Disease Relation", "Negated", "Disease", "Reference", "With", "Evidence Code", "Genetic Sex", "Disease Qualifiers",
-    "SGD Strain Background", "Annotation Type", "Data Provider", "Secondary Data Provider", "Modified By", "Created By", "Related Notes"];
+  const defaultColumnNames = ["Unique ID", "MOD Entity ID", "Subject", "Disease Relation", "Negated", "Disease", "Reference", "With", "Evidence Code", "Genetic Sex", "Disease Qualifiers",
+    "SGD Strain Background", "Annotation Type", "Genetic Modifier Relation", "Genetic Modifier", "Data Provider", "Secondary Data Provider", "Modified By", "Created By", "Related Notes"];
   let initialTableState = {
     page: 0,
     first: 0,
@@ -714,6 +714,18 @@ export const DiseaseAnnotationsTable = () => {
     )
   };
 
+  const modEntityIdBodyTemplate = (rowData) => {
+    return (
+      //the 'a' at the start is a hack since css selectors can't start with a number
+      <>
+        <EllipsisTableCell otherClasses={`a${rowData.id}`}>
+          {rowData.modEntityId}
+        </EllipsisTableCell>
+        <Tooltip target={`.a${rowData.id}`} content={rowData.modEntityId} />
+      </>
+    )
+  };
+
   const filterComponentInputTextTemplate = (filterName, fields) => {
     return (<FilterComponentInputText
       isEnabled={isEnabled}
@@ -759,11 +771,19 @@ export const DiseaseAnnotationsTable = () => {
 
   const columns = [{
     field: "uniqueId",
-    header: "Unique Id",
+    header: "Unique ID",
     sortable: isEnabled,
     filter: true,
     body: uniqueIdBodyTemplate,
     filterElement: filterComponentInputTextTemplate("uniqueidFilter", ["uniqueId"])
+  },
+  {
+    field: "ModEntityId",
+    header: "MOD Entity ID",
+    sortable: isEnabled,
+    filter: true,
+    body: modEntityIdBodyTemplate,
+    filterElement: filterComponentInputTextTemplate("modentityidFilter", ["modEntityId"])
   },
   {
     field: "subject.symbol",
