c0101333cb2aa32d18213c938553081e6ac97051
Ketaki Thorat <72769853+kthorat-prog@users.noreply.github.com>
2022-03-21 13:02:39 -0500

    Layout changed to Card Component

--- src/main/cliapp/src/components/DataTableHeaderFooterTemplate.js
+++ src/main/cliapp/src/components/DataTableHeaderFooterTemplate.js
@@ -1,21 +1,22 @@
 import React from "react";
-import {Splitter, SplitterPanel} from "primereact/splitter";
+import { Card } from 'primereact/card';
+import { Splitter, SplitterPanel } from "primereact/splitter";
 import { Button } from 'primereact/button';
 
 export function DataTableHeaderFooterTemplate({title, tableState, multiselectComponent, buttons, onclickEvent, isEnabled}){
 
     return (
-        <div className="card">
-            <Splitter style={{border:'none', height:'10%'}} gutterSize="0">
-                <SplitterPanel size={50} style={{textAlign: 'left'}}>
-                    <h2>{title}</h2>
-                </SplitterPanel>
-                <SplitterPanel size={50} style={{textAlign: 'right'}}>
-                    {multiselectComponent}&nbsp;&nbsp;&nbsp;&nbsp;
-                    {buttons}&nbsp;&nbsp;
-                    <Button disabled={!isEnabled} onClick={onclickEvent}>Reset Table</Button>
-                </SplitterPanel>
-            </Splitter>
-        </div>
+            <Card>
+                <Splitter style={{border:'none', height:'10%'}} gutterSize="0">
+                    <SplitterPanel size={50} style={{textAlign: 'left'}}>
+                        <h2>{title}</h2>
+                    </SplitterPanel>
+                    <SplitterPanel size={50} style={{textAlign: 'right'}}>
+                        {multiselectComponent}&nbsp;&nbsp;&nbsp;&nbsp;
+                        {buttons}&nbsp;&nbsp;
+                        <Button disabled={!isEnabled} onClick={onclickEvent}>Reset Table</Button>
+                    </SplitterPanel>
+                </Splitter>
+            </Card>
     )
 }
