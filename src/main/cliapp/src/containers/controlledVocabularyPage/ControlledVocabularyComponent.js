c44aab550cd9ce1bc663ee1bf97dad820976430c
Ketaki Thorat <72769853+kthorat-prog@users.noreply.github.com>
2022-03-25 16:10:25 -0500

    Header backgroundcolor added to make it opaque

--- src/main/cliapp/src/containers/controlledVocabularyPage/ControlledVocabularyComponent.js
+++ src/main/cliapp/src/containers/controlledVocabularyPage/ControlledVocabularyComponent.js
@@ -381,6 +381,7 @@ export const ControlledVocabularyComponent = () => {
     useEffect(() => {
     const filteredColumns = filterColumns(columns, tableState.selectedColumnNames);
     const orderedColumns = orderColumns(filteredColumns, tableState.selectedColumnNames);
+
     setColumnMap(
       orderedColumns.map((col) => {
         return <Column
@@ -392,7 +393,7 @@ export const ControlledVocabularyComponent = () => {
           filter={col.filter}
           showFilterMenu={false}
           filterElement={col.filterElement}
-          style={{whiteSpace: 'normal'}}
+          style={{whiteSpace: 'normal',backgroundColor: 'var(--surface-0)'}}
           editor={col.editor}
           body={col.body}
         />;
