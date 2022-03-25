5f8d5d495ebdae32d97f99c92ac78176339ac0a8
Ketaki Thorat <72769853+kthorat-prog@users.noreply.github.com>
2022-03-25 16:23:09 -0500

    Header backgroundcolor added to make it opaque via primeflex Classname

--- src/main/cliapp/src/containers/controlledVocabularyPage/ControlledVocabularyComponent.js
+++ src/main/cliapp/src/containers/controlledVocabularyPage/ControlledVocabularyComponent.js
@@ -393,7 +393,8 @@ export const ControlledVocabularyComponent = () => {
           filter={col.filter}
           showFilterMenu={false}
           filterElement={col.filterElement}
-          style={{whiteSpace: 'normal',backgroundColor: 'var(--surface-0)'}}
+          style={{whiteSpace: 'normal'}}
+          //headerClassName='surface-0'
           editor={col.editor}
           body={col.body}
         />;
