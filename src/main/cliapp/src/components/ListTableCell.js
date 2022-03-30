686348779548900e59474565c115504a0388a0b0
Adam Gibson <agibson@mcw.edu>
2022-03-30 08:23:08 -0500

    SCRUM-1416 added null check to ListTableCell

--- src/main/cliapp/src/components/ListTableCell.js
+++ src/main/cliapp/src/components/ListTableCell.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const ListTableCell = ({ template, listData }) => {
   return (
     <ul className='pl-0 list-none'>
-      {listData.map((item, index) =>
+      {listData?.map((item, index) =>
         <li key={index}>
           {template(item)}
         </li>
