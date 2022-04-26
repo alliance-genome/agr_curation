1bb950d6eab4f00ea9e13e766e845564612fe51a
Ketaki Thorat <72769853+kthorat-prog@users.noreply.github.com>
2022-04-26 10:17:36 -0500

    ZIndex made highest for errormessages component

--- src/main/cliapp/src/components/ErrorMessageComponent.js
+++ src/main/cliapp/src/components/ErrorMessageComponent.js
@@ -9,7 +9,7 @@ export function ErrorMessageComponent({ errorField, errorMessages: errorMessages
                 <Message
                     severity={errorMessagesRow[errorField] ? errorMessagesRow[errorField].severity : ""}
                     text={errorMessagesRow[errorField] ? errorMessagesRow[errorField].message : ""}
-                    style={{position:"absolute", display: "inline-table"}}
+                    style={{position:"absolute", display: "inline-table", zIndex: "5"}}
                 />
             </div>
         );
