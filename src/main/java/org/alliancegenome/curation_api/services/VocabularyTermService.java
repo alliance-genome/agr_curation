d293ca38167e303bb767a843234b67dd1cb2c96a
Olin Blodgett <nilo@nilosplace.net>
2022-04-28 17:10:35 -0400

    Removed redunant method

--- src/main/java/org/alliancegenome/curation_api/services/VocabularyTermService.java
+++ src/main/java/org/alliancegenome/curation_api/services/VocabularyTermService.java
@@ -1,18 +1,15 @@
 package org.alliancegenome.curation_api.services;
 
-import org.alliancegenome.curation_api.base.services.BaseCrudService;
-import org.alliancegenome.curation_api.dao.VocabularyTermDAO;
-import org.alliancegenome.curation_api.exceptions.ApiErrorException;
-import org.alliancegenome.curation_api.model.entities.VocabularyTerm;
-import org.alliancegenome.curation_api.response.ObjectResponse;
-import org.alliancegenome.curation_api.services.helpers.validators.VocabularyTermValidator;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.validation.ConstraintViolationException;
+
+import org.alliancegenome.curation_api.base.services.BaseCrudService;
+import org.alliancegenome.curation_api.dao.VocabularyTermDAO;
+import org.alliancegenome.curation_api.model.entities.VocabularyTerm;
+import org.alliancegenome.curation_api.response.ObjectResponse;
+import org.alliancegenome.curation_api.services.helpers.validators.VocabularyTermValidator;
 
 @RequestScoped
 public class VocabularyTermService extends BaseCrudService<VocabularyTerm, VocabularyTermDAO> {
@@ -33,13 +30,6 @@ public class VocabularyTermService extends BaseCrudService<VocabularyTerm, Vocab
     public ObjectResponse<VocabularyTerm> update(VocabularyTerm uiEntity) {
         VocabularyTerm dbEntity = vocabularyTermValidator.validateVocabularyTerm(uiEntity);
         return new ObjectResponse<>(vocabularyTermDAO.persist(dbEntity));
-    }
-
-    @Override
-    @Transactional
-    public ObjectResponse<VocabularyTerm> delete(Long id) {
-        VocabularyTerm object = vocabularyTermDAO.remove(id);
-        return new ObjectResponse<>(object);
     }
 
 }
