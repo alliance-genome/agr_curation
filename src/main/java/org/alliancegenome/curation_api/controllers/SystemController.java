a70ba3a429916ccfd2c91974a52b53d2eb034104
Olin Blodgett <nilo@nilosplace.net>
2022-05-26 15:22:28 -0400

    Added code to pull counts of all entities in the database

--- src/main/java/org/alliancegenome/curation_api/controllers/SystemController.java
+++ src/main/java/org/alliancegenome/curation_api/controllers/SystemController.java
@@ -1,10 +1,13 @@
 package org.alliancegenome.curation_api.controllers;
 
+import java.util.Map;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
 import org.alliancegenome.curation_api.base.dao.SystemSQLDAO;
 import org.alliancegenome.curation_api.interfaces.SystemControllerInterface;
+import org.alliancegenome.curation_api.response.ObjectResponse;
 
 @RequestScoped
 public class SystemController implements SystemControllerInterface {
@@ -14,6 +17,11 @@ public class SystemController implements SystemControllerInterface {
     @Override
     public void reindexEverything(Integer threads, Integer indexAmount, Integer batchSize) {
         systemSQLDAO.reindexEverything(threads, indexAmount, batchSize);
+    }
+    
+    @Override
+    public ObjectResponse<Map<String, Object>> getSiteSummary() {
+        return systemSQLDAO.getSiteSummary();
     }
     
 }
