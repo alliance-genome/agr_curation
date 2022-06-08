89336f3fd498bf7f421e79516cfe5fa676fb3574
Olin Blodgett <nilo@nilosplace.net>
2022-06-08 10:12:35 -0400

    Fixed Literature links

--- src/main/java/org/alliancegenome/curation_api/base/dao/SystemSQLDAO.java
+++ src/main/java/org/alliancegenome/curation_api/base/dao/SystemSQLDAO.java
@@ -5,31 +5,36 @@ import static org.reflections.scanners.Scanners.TypesAnnotated;
 import java.util.*;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.persistence.Entity;
 
 import org.alliancegenome.curation_api.base.entity.BaseEntity;
-import org.alliancegenome.curation_api.response.ObjectResponse;
+import org.alliancegenome.curation_api.dao.LiteratureReferenceDAO;
+import org.alliancegenome.curation_api.model.document.LiteratureReference;
+import org.alliancegenome.curation_api.response.*;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.reflections.Reflections;
 
 @ApplicationScoped
 public class SystemSQLDAO extends BaseSQLDAO<BaseEntity> {
 
+	@Inject LiteratureReferenceDAO literatureReferenceDAO;
+
 	protected SystemSQLDAO() {
 		super(BaseEntity.class);
 	}
-	
+
 	public ObjectResponse<Map<String, Object>> getSiteSummary() {
-		
+
 		Reflections reflections = new Reflections("org.alliancegenome.curation_api");
 		Set<Class<?>> entityClasses = reflections.get(TypesAnnotated.with(Entity.class).asClass(reflections.getConfiguration().getClassLoaders()));
 		Set<Class<?>> indexedClasses = reflections.get(TypesAnnotated.with(Indexed.class).asClass(reflections.getConfiguration().getClassLoaders()));
-		
+
 		Set<Class<?>> allClasses = new HashSet<Class<?>>();
-		
+
 		allClasses.addAll(entityClasses);
 		allClasses.addAll(indexedClasses);
-		
+
 		Map<String, Object> map = new HashMap<>();
 
 		for(Class<?> clazz: allClasses) {
@@ -40,13 +45,19 @@ public class SystemSQLDAO extends BaseSQLDAO<BaseEntity> {
 				tempMap.put("dbCount", 0);
 			}
 			if(indexedClasses.contains(clazz)) {
-				tempMap.put("esCount", esCount(clazz));
+				if(clazz.getSimpleName().equals("Reference")) {
+					SearchResponse<LiteratureReference> res = literatureReferenceDAO.searchAllCount();
+					tempMap.put("esCount", res.getTotalResults());
+				} else {
+					tempMap.put("esCount", esCount(clazz));
+				}
 			} else {
 				tempMap.put("esCount", 0);
 			}
 			map.put(clazz.getSimpleName(), tempMap);
+
 		}
-		
+
 		return new ObjectResponse<Map<String, Object>>(map);
 	}
 
