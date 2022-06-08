ab4021402781d70b2ceccdc0595b5c31f1514764
Christian Pich <cmpich@zfin.org>
2022-06-08 17:54:13 +0200

    change JsonView for update conditionRelation

--- src/main/java/org/alliancegenome/curation_api/interfaces/crud/ConditionRelationCrudInterface.java
+++ src/main/java/org/alliancegenome/curation_api/interfaces/crud/ConditionRelationCrudInterface.java
@@ -25,7 +25,7 @@ public interface ConditionRelationCrudInterface extends BaseIdCrudInterface<Cond
 
     @PUT
     @Path("/")
-    @JsonView(View.FieldsAndLists.class)
+    @JsonView(View.ConditionRelationUpdateView.class)
     public ObjectResponse<ConditionRelation> update(ConditionRelation entity);
 
     @POST
--- src/main/java/org/alliancegenome/curation_api/model/entities/ConditionRelation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/ConditionRelation.java
@@ -44,7 +44,7 @@ public class ConditionRelation extends UniqueIdAuditedObject {
 	@IndexedEmbedded(includeDepth = 1)
 	@IndexingDependency(reindexOnUpdate = ReindexOnUpdate.SHALLOW)
 	@ManyToMany
-	@JsonView({View.FieldsAndLists.class})
+	@JsonView({View.ConditionRelationUpdateView.class})
 	private List<ExperimentalCondition> conditions;
 
 
