c645943f12eae008a6b599bf2e1e4cc859052182
Christian Pich <cmpich@zfin.org>
2022-06-15 11:30:00 +0200

    SCRUM-1641 convert list of records to delete into batches

--- src/main/java/org/alliancegenome/curation_api/dao/GeneDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/GeneDAO.java
@@ -1,5 +1,6 @@
 package org.alliancegenome.curation_api.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -15,7 +16,7 @@ public class GeneDAO extends BaseSQLDAO<Gene> {
 	protected GeneDAO() {
 		super(Gene.class);
 	}
-	
+
 	public Gene getByIdOrCurie(String id) {
 		return find(id);
 	}
@@ -25,41 +26,54 @@ public class GeneDAO extends BaseSQLDAO<Gene> {
 		jpqlQuery.setParameter("taxonId", taxonId);
 		return (List<String>) jpqlQuery.getResultList();
 	}
-	
+
 	@Transactional
 	public void deleteReferencingDiseaseAnnotations(List<String> geneCuries) {
+		getBatches(geneCuries, 10000)
+			.forEach(this::deleteReferencingDiseaseAnnotationsSmallBatches);
+	}
+
+	protected <T> List<List<T>> getBatches(List<T> collection, int batchSize) {
+		List<List<T>> batches = new ArrayList<>();
+		for (int i = 0; i < collection.size(); i += batchSize) {
+			batches.add(collection.subList(i, Math.min(i + batchSize, collection.size())));
+		}
+		return batches;
+	}
+
+	public void deleteReferencingDiseaseAnnotationsSmallBatches(List<String> geneCuries) {
 		Query jpqlQuery = entityManager.createQuery("SELECT da.id FROM DiseaseAnnotation da WHERE da.diseaseGeneticModifier.curie IN (:geneCuries)");
 		jpqlQuery.setParameter("geneCuries", geneCuries);
-		List<String> results = (List<String>)jpqlQuery.getResultList();
-		
+		List<String> results = (List<String>) jpqlQuery.getResultList();
+
 		jpqlQuery = entityManager.createQuery("SELECT gda.id FROM GeneDiseaseAnnotation gda WHERE gda.subject.curie IN (:geneCuries)");
 		jpqlQuery.setParameter("geneCuries", geneCuries);
 		results.addAll((List<String>) jpqlQuery.getResultList());
-		
+
 		jpqlQuery = entityManager.createNativeQuery("SELECT diseaseannotation_id FROM diseaseannotation_gene gda WHERE with_curie IN (:geneCuries)");
 		jpqlQuery.setParameter("geneCuries", geneCuries);
 		results.addAll((List<String>) jpqlQuery.getResultList());
-		
+
 		jpqlQuery = entityManager.createNativeQuery("DELETE FROM diseaseannotation_conditionrelation WHERE diseaseannotation_id IN (:ids)");
 		jpqlQuery.setParameter("ids", results);
 		jpqlQuery.executeUpdate();
-		
+
 		jpqlQuery = entityManager.createNativeQuery("DELETE FROM diseaseannotation_ecoterm WHERE diseaseannotation_id IN (:ids)");
 		jpqlQuery.setParameter("ids", results);
 		jpqlQuery.executeUpdate();
-		
+
 		jpqlQuery = entityManager.createNativeQuery("DELETE FROM diseaseannotation_gene WHERE diseaseannotation_id IN (:ids)");
 		jpqlQuery.setParameter("ids", results);
 		jpqlQuery.executeUpdate();
-		
+
 		jpqlQuery = entityManager.createNativeQuery("DELETE FROM diseaseannotation_note WHERE diseaseannotation_id IN (:ids)");
 		jpqlQuery.setParameter("ids", results);
 		jpqlQuery.executeUpdate();
-		
+
 		jpqlQuery = entityManager.createNativeQuery("DELETE FROM diseaseannotation_vocabularyterm WHERE diseaseannotation_id IN (:ids)");
 		jpqlQuery.setParameter("ids", results);
 		jpqlQuery.executeUpdate();
 	}
-	
+
 
 }
