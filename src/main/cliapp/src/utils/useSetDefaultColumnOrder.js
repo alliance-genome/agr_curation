a57e277bb4b20ad28db30a407ac0bcbee2d0ea33
markquintontulloch <markquintontulloch@gmail.com>
2022-08-04 12:40:40 +0100

    Add deletionEnabled param to useSetDefaultColumnOrder

--- src/main/cliapp/src/utils/useSetDefaultColumnOrder.js
+++ src/main/cliapp/src/utils/useSetDefaultColumnOrder.js
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import { setDefaultColumnOrder } from "./utils";
 
-export const useSetDefaultColumnOrder = (columns, dataTable, defaultColumnOptions, setIsFirst, isFirst) => {
+export const useSetDefaultColumnOrder = (columns, dataTable, defaultColumnOptions, setIsFirst, isFirst, deletionEnabled=false) => {
 	useEffect(() => {
 		if (isFirst) {
-			setDefaultColumnOrder(columns, dataTable, defaultColumnOptions);
+			setDefaultColumnOrder(columns, dataTable, defaultColumnOptions, deletionEnabled);
 		};
 		setIsFirst(false);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
