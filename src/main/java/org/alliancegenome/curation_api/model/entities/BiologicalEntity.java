08a1833bf465759959d04ffa23f0caba4482682c
Olin Blodgett <nilo@nilosplace.net>
2022-08-30 11:56:26 -0400

    Added type to bio entity

--- src/main/java/org/alliancegenome/curation_api/model/entities/BiologicalEntity.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/BiologicalEntity.java
@@ -10,9 +10,24 @@ import org.hibernate.envers.Audited;
 import org.hibernate.search.mapper.pojo.automaticindexing.ReindexOnUpdate;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.*;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 
 import lombok.*;
+
+
+
+@JsonTypeInfo(
+		  use = JsonTypeInfo.Id.NAME, 
+		  include = JsonTypeInfo.As.PROPERTY, 
+		  property = "type")
+		@JsonSubTypes({
+			@Type(value = AffectedGenomicModel.class, name = "AffectedGenomicModel"),
+			@Type(value = Gene.class, name = "Gene"),
+			@Type(value = Allele.class, name = "Allele")
+		})
 
 @Audited
 @Entity
