e07054992e8f5ed995b93b2dc3819e0f38c8f159
markquintontulloch <markquintontulloch@gmail.com>
2022-09-14 21:40:12 +0100

    Add bulk load integration tests for new Allele fields

--- src/test/java/org/alliancegenome/curation_api/bulkupload/AlleleBulkUploadITCase.java
+++ src/test/java/org/alliancegenome/curation_api/bulkupload/AlleleBulkUploadITCase.java
@@ -8,7 +8,12 @@ import java.nio.file.Path;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 
+import org.alliancegenome.curation_api.constants.VocabularyConstants;
+import org.alliancegenome.curation_api.model.entities.Reference;
+import org.alliancegenome.curation_api.model.entities.Vocabulary;
+import org.alliancegenome.curation_api.model.entities.VocabularyTerm;
 import org.alliancegenome.curation_api.resources.TestContainerResource;
+import org.alliancegenome.curation_api.response.ObjectResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -20,6 +25,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.config.HttpClientConfig;
 import io.restassured.config.RestAssuredConfig;
 
@@ -31,6 +37,11 @@ import io.restassured.config.RestAssuredConfig;
 @DisplayName("02 - Allele bulk upload")
 @Order(2)
 public class AlleleBulkUploadITCase {
+	
+	private String requiredInheritenceMode = "dominant";
+	private String requiredInCollection = "Million mutations project";
+	private String requiredSequencingStatus = "sequenced";
+	private String requiredReference = "PMID:25920554";
 	
 	@BeforeEach
 	public void init() {
@@ -44,6 +55,8 @@ public class AlleleBulkUploadITCase {
 	@Order(1)
 	public void alleleBulkUploadCheckFields() throws Exception {
 		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/01_all_fields_allele.json"));
+		
+		loadRequiredEntities();
 		
 		// upload file
 		RestAssured.given().
@@ -71,6 +84,12 @@ public class AlleleBulkUploadITCase {
 			body("results[0].taxon.curie", is("NCBITaxon:6239")).
 			body("results[0].internal", is(true)).
 			body("results[0].obsolete", is(true)).
+			body("results[0].inheritenceMode.name", is("dominant")).
+			body("results[0].inCollection.name", is("Million mutation project")).
+			body("results[0].sequencingStatus.name", is("sequence")).
+			body("results[0].isExtinct", is(false)).
+			body("results[0].references", hasSize(1)).
+			body("results[0].references[0].curie", is("PMID:25920550")).
 			body("results[0].createdBy.uniqueId", is("ALLELETEST:Person0001")).
 			body("results[0].updatedBy.uniqueId", is("ALLELETEST:Person0002")).
 			body("results[0].dateCreated", is(OffsetDateTime.parse("2022-03-09T22:10:12Z").atZoneSameInstant(ZoneId.systemDefault()).toOffsetDateTime().toString())).
@@ -154,7 +173,7 @@ public class AlleleBulkUploadITCase {
 			statusCode(200).
 			body("totalResults", is(1)).
 			body("results", hasSize(1)).
-			body("results[0].curie", is("ALLELETEST:Allele0004"));;
+			body("results[0].curie", is("ALLELETEST:Allele0004"));
 	}
 	
 	@Test
@@ -652,5 +671,315 @@ public class AlleleBulkUploadITCase {
 			then().
 			statusCode(200).
 			body("totalResults", is(0));
+	}
+	
+	@Test
+	@Order(23)
+	public void alleleBulkUploadNoInheritenceMode() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/23_no_inheritence_mode_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(1)).
+			body("results", hasSize(1)).
+			body("results[0].curie", is("ALLELETEST:Allele0023"));
+	}
+	
+	@Test
+	@Order(24)
+	public void alleleBulkUploadNoCollection() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/24_no_in_collection_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(1)).
+			body("results", hasSize(1)).
+			body("results[0].curie", is("ALLELETEST:Allele0024"));
+	}
+	
+	@Test
+	@Order(25)
+	public void alleleBulkUploadNoSequencingStatus() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/25_no_sequencing_status_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(1)).
+			body("results", hasSize(1)).
+			body("results[0].curie", is("ALLELETEST:Allele0025"));
+	}
+	
+	@Test
+	@Order(26)
+	public void alleleBulkUploadNoIsExtinct() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/26_no_is_extinct_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(1)).
+			body("results", hasSize(1)).
+			body("results[0].curie", is("ALLELETEST:Allele0026")).
+			body("results[0].isExtinct", is(false));
+	}
+	
+	@Test
+	@Order(27)
+	public void alleleBulkUploadNoReferences() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/27_no_references_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(1)).
+			body("results", hasSize(1)).
+			body("results[0].curie", is("ALLELETEST:Allele0027"));
+	}
+	
+	@Test
+	@Order(28)
+	public void alleleBulkUploadInvalidInheritenceMode() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/28_invalid_inheritence_mode_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(0));
+	}
+	
+	@Test
+	@Order(29)
+	public void alleleBulkUploadInvalidInCollection() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/29_invalid_in_collection_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(0));
+	}
+	
+	@Test
+	@Order(30)
+	public void alleleBulkUploadInvalidSequencingStatus() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/30_invalid_sequencing_status_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(0));
+	}
+	
+	@Test
+	@Order(31)
+	public void alleleBulkUploadInvalidReference() throws Exception {
+		String content = Files.readString(Path.of("src/test/resources/bulk/02_allele/30_invalid_reference_allele.json"));
+		
+		// upload file
+		RestAssured.given().
+			contentType("application/json").
+			body(content).
+			when().
+			post("/api/allele/bulk/alleles").
+			then().
+			statusCode(200);
+	
+		
+		// check entity count and fields correctly read
+		RestAssured.given().
+			when().
+			header("Content-Type", "application/json").
+			body("{}").
+			post("/api/allele/find?limit=10&page=0").
+			then().
+			statusCode(200).
+			body("totalResults", is(0));
+	}
+	
+	private void loadRequiredEntities() throws Exception {
+		loadReference();
+		
+		Vocabulary inheritenceModeVocabulary = createVocabulary(VocabularyConstants.ALLELE_INHERITENCE_MODE_VOCABULARY);
+		Vocabulary inCollectionVocabulary = createVocabulary(VocabularyConstants.ALLELE_COLLECTION_VOCABULARY);
+		Vocabulary sequencingStatusVocabulary = createVocabulary(VocabularyConstants.SEQUENCING_STATUS_VOCABULARY);
+		createVocabularyTerm(inheritenceModeVocabulary, requiredInheritenceMode);
+		createVocabularyTerm(inCollectionVocabulary, requiredInCollection);
+		createVocabularyTerm(sequencingStatusVocabulary, requiredSequencingStatus);
+	}
+	
+	private void loadReference() throws Exception {
+			
+		Reference reference = new Reference();
+		reference.setCurie(requiredReference);
+		reference.setObsolete(false);
+		
+		RestAssured.given().
+			contentType("application/json").
+			body(reference).
+			when().
+			put("/api/reference").
+			then().
+			statusCode(200);
+	}
+	
+	private Vocabulary createVocabulary(String name) {
+		Vocabulary vocabulary = new Vocabulary();
+		vocabulary.setName(name);
+		vocabulary.setInternal(false);
+		
+		ObjectResponse<Vocabulary> response = 
+			RestAssured.given().
+				contentType("application/json").
+				body(vocabulary).
+				when().
+				post("/api/vocabulary").
+				then().
+				statusCode(200).
+				extract().body().as(getObjectResponseTypeRefVocabulary());
+		
+		vocabulary = response.getEntity();
+		
+		return vocabulary;
+	}
+
+	private void createVocabularyTerm(Vocabulary vocabulary, String name) {
+		VocabularyTerm vocabularyTerm = new VocabularyTerm();
+		vocabularyTerm.setName(name);
+		vocabularyTerm.setVocabulary(vocabulary);
+		vocabularyTerm.setInternal(false);
+		
+		RestAssured.given().
+				contentType("application/json").
+				body(vocabularyTerm).
+				when().
+				post("/api/vocabularyterm").
+				then().
+				statusCode(200);
+	}
+	
+	private TypeRef<ObjectResponse<Vocabulary>> getObjectResponseTypeRefVocabulary() {
+		return new TypeRef<ObjectResponse <Vocabulary>>() { };
 	}
 }
