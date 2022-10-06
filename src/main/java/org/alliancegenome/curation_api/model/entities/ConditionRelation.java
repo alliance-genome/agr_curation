9a2e11f59a9316b2d3a02ea7da1cfe622430c0c7
markquintontulloch <markquintontulloch@gmail.com>
2022-10-06 22:28:40 +0100

    Update ConditionRelation conditions view

--- src/main/java/org/alliancegenome/curation_api/model/entities/ConditionRelation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/ConditionRelation.java
@@ -47,7 +47,7 @@ public class ConditionRelation extends UniqueIdAuditedObject {
 	@IndexedEmbedded(includeDepth = 1)
 	@IndexingDependency(reindexOnUpdate = ReindexOnUpdate.SHALLOW)
 	@ManyToMany
-	@JsonView({View.FieldsAndLists.class, View.DiseaseAnnotationUpdate.class})
+	@JsonView({View.FieldsAndLists.class, View.DiseaseAnnotation.class})
 	private List<ExperimentalCondition> conditions;
 
 
