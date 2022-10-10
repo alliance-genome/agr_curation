79b392b21bb0e1414d80883fe91ed69058cb8b78
Olin Blodgett <nilo@nilosplace.net>
2022-10-10 15:10:35 -0400

    Added migration for person settings

--- src/main/java/org/alliancegenome/curation_api/services/helpers/validators/GenomicEntityValidator.java
+++ src/main/java/org/alliancegenome/curation_api/services/helpers/validators/GenomicEntityValidator.java
@@ -53,4 +53,86 @@ public class GenomicEntityValidator extends CurieAuditedObjectValidator {
 		return name;
 	}
 	
+	public List<Synonym> validateSynonyms(GenomicEntity uiEntity, GenomicEntity dbEntity) {
+		String field = "synonyms";
+		
+		List<Synonym> validatedSynonyms = new ArrayList<Synonym>();
+		if (CollectionUtils.isNotEmpty(uiEntity.getSynonyms())) {	
+			for (Synonym newSynonym : uiEntity.getSynonyms()) {
+				ObjectResponse<Synonym> synonymResponse = synonymValidator.validateSynonym(newSynonym);
+				if (synonymResponse.getEntity() == null) {
+					Map<String, String> errors = synonymResponse.getErrorMessages();
+					for (String synonymField : errors.keySet()) {
+						addMessageResponse(field, synonymField + " - " + errors.get(synonymField));
+					}
+					return null;
+				}
+				validatedSynonyms.add(synonymResponse.getEntity());
+			}
+		}
+		
+		List<Long> previousIds = new ArrayList<Long>();
+		if(CollectionUtils.isNotEmpty(dbEntity.getSynonyms()))
+			previousIds = dbEntity.getSynonyms().stream().map(Synonym::getId).collect(Collectors.toList());
+		List<Long> validatedIds = new ArrayList<Long>();
+		if (CollectionUtils.isNotEmpty(validatedSynonyms))
+			validatedIds = validatedSynonyms.stream().map(Synonym::getId).collect(Collectors.toList());
+		for (Synonym validatedSynonym : validatedSynonyms) {
+			if (!previousIds.contains(validatedSynonym.getId()))
+				synonymDAO.persist(validatedSynonym);
+		}
+		
+		if (dbEntity.getSynonyms() != null) {
+			List<Long> idsToRemove = ListUtils.subtract(previousIds, validatedIds);
+			Predicate<Synonym> removeCondition = synonym -> idsToRemove.contains(synonym.getId());
+			dbEntity.getSynonyms().removeIf(removeCondition);
+		}
+		
+		if (CollectionUtils.isEmpty(validatedSynonyms))
+			return null;
+		
+		return validatedSynonyms;
+	}
+	
+	public List<CrossReference> validateCrossReferences(GenomicEntity uiEntity, GenomicEntity dbEntity) {
+		String field = "crossReferences";
+		
+		List<CrossReference> validatedXrefs = new ArrayList<CrossReference>();
+		if (CollectionUtils.isNotEmpty(uiEntity.getCrossReferences())) {	
+			for (CrossReference newXref : uiEntity.getCrossReferences()) {
+				ObjectResponse<CrossReference> xrefResponse = crossReferenceValidator.validateCrossReference(newXref);
+				if (xrefResponse.getEntity() == null) {
+					Map<String, String> errors = xrefResponse.getErrorMessages();
+					for (String xrefField : errors.keySet()) {
+						addMessageResponse(field, xrefField + " - " + errors.get(xrefField));
+					}
+					return null;
+				}
+				validatedXrefs.add(xrefResponse.getEntity());
+			}
+		}
+		
+		List<String> previousCuries = new ArrayList<String>();
+		if(CollectionUtils.isNotEmpty(dbEntity.getCrossReferences()))
+			previousCuries = dbEntity.getCrossReferences().stream().map(CrossReference::getCurie).collect(Collectors.toList());
+		List<String> validatedCuries = new ArrayList<String>();
+		if (CollectionUtils.isNotEmpty(validatedXrefs))
+			validatedCuries = validatedXrefs.stream().map(CrossReference::getCurie).collect(Collectors.toList());
+		for (CrossReference validatedXref : validatedXrefs) {
+			if (!previousCuries.contains(validatedXref.getCurie()))
+				crossReferenceDAO.persist(validatedXref);
+		}
+		
+		if (dbEntity.getCrossReferences() != null) {
+			List<String> curiesToRemove = ListUtils.subtract(previousCuries, validatedCuries);
+			Predicate<CrossReference> removeCondition = xref -> curiesToRemove.contains(xref.getCurie());
+			dbEntity.getCrossReferences().removeIf(removeCondition);
+		}
+		
+		if (CollectionUtils.isEmpty(validatedXrefs))
+			return null;
+		
+		return validatedXrefs;
+	}
+	
 }
