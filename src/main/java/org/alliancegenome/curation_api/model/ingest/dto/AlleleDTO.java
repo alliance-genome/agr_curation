02367a9310b71b0f72f6363541ac87bbeaacda47
markquintontulloch <markquintontulloch@gmail.com>
2022-10-11 23:06:10 +0100

    Remove synonyms, duplicted from GE

--- src/main/java/org/alliancegenome/curation_api/model/ingest/dto/AlleleDTO.java
+++ src/main/java/org/alliancegenome/curation_api/model/ingest/dto/AlleleDTO.java
@@ -33,8 +33,5 @@ public class AlleleDTO extends GenomicEntityDTO {
 	@JsonView({View.FieldsOnly.class})
 	@JsonProperty("is_extinct")
 	private Boolean isExtinct;
-	
-	@JsonView({View.FieldsAndLists.class})
-	private List<SynonymDTO> synonyms;
 
 }
