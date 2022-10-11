85ce53020b18a06db34e20969760220ca400c49a
Olin Blodgett <nilo@nilosplace.net>
2022-10-11 18:27:13 -0400

    Renamed keys

--- src/main/cliapp/src/service/PersonSettingsService.js
+++ src/main/cliapp/src/service/PersonSettingsService.js
@@ -1,11 +1,11 @@
 import { BaseAuthService } from './BaseAuthService';
 
 export class PersonSettingsService extends BaseAuthService {
-	getUserSettings(key) {
-		return this.api.get(`/personsettings/${key}`).then(res => res.data);
+	getUserSettings(settingsKey) {
+		return this.api.get(`/personsettings/${settingsKey}`).then(res => res.data);
 	}
 
-	saveUserSettings(key, settings) {
-		return this.api.put(`/personsettings/${key}`, settings).then(res => res.data);
+	saveUserSettings(settingsKey, settingsMap) {
+		return this.api.put(`/personsettings/${settingsKey}`, settingsMap).then(res => res.data);
 	}
 }
