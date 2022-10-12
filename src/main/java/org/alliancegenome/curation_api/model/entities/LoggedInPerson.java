930522af7f019cbd452013ac65035f06fe0f276b
Olin Blodgett <nilo@nilosplace.net>
2022-10-12 09:18:09 -0400

    Updated schema version

--- src/main/java/org/alliancegenome/curation_api/model/entities/LoggedInPerson.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/LoggedInPerson.java
@@ -24,7 +24,7 @@ import lombok.ToString;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
 @ToString(callSuper = true)
-@AGRCurationSchemaVersion(min="1.2.0", max=LinkMLSchemaConstants.LATEST_RELEASE, dependencies={Person.class})
+@AGRCurationSchemaVersion(min="1.3.2", max=LinkMLSchemaConstants.LATEST_RELEASE, dependencies={Person.class})
 public class LoggedInPerson extends Person {
 
 	@FullTextField(analyzer = "autocompleteAnalyzer", searchAnalyzer = "autocompleteSearchAnalyzer")
