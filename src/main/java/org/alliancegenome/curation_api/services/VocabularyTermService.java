5fba47f2153393394f9fbca25881049820306882
Olin Blodgett <nilo@nilosplace.net>
2022-10-26 09:38:15 -0400

    Moved method to service level

--- src/main/java/org/alliancegenome/curation_api/services/VocabularyTermService.java
+++ src/main/java/org/alliancegenome/curation_api/services/VocabularyTermService.java
@@ -25,6 +25,13 @@ public class VocabularyTermService extends BaseEntityCrudService<VocabularyTerm,
 		setSQLDao(vocabularyTermDAO);
 	}
 
+	public ObjectResponse<VocabularyTerm> getTermInVocabulary(String name, String vocabulary) {
+		VocabularyTerm term = vocabularyTermDAO.getTermInVocabulary(name, vocabulary);
+		ObjectResponse<VocabularyTerm> response = new ObjectResponse<VocabularyTerm>();
+		response.setEntity(term);
+		return response;
+	}
+	
 	@Override
 	@Transactional
 	public ObjectResponse<VocabularyTerm> create(VocabularyTerm uiEntity) {
