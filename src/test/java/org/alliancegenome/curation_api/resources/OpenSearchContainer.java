ce9f5a572acc94e2b3b4b53a9416f130822aa23a
Olin Blodgett <nilo@nilosplace.net>
2022-11-29 07:47:44 -0500

    Fix for containers?

--- src/test/java/org/alliancegenome/curation_api/resources/OpenSearchContainer.java
+++ src/test/java/org/alliancegenome/curation_api/resources/OpenSearchContainer.java
@@ -6,25 +6,24 @@ import org.testcontainers.utility.*;
 
 public class OpenSearchContainer extends GenericContainer<OpenSearchContainer> {
 
-   public OpenSearchContainer(String dockerImageName) {
-      this(DockerImageName.parse(dockerImageName));
-  }
-	
+	public OpenSearchContainer(String dockerImageName) {
+		this(DockerImageName.parse(dockerImageName));
+	}
+
 	public OpenSearchContainer(final DockerImageName dockerImageName) {
 		super(dockerImageName);
 
 		logger().info("Starting an elasticsearch container using [{}]", dockerImageName);
-		
+
 		withNetworkAliases("opensearch-" + Base58.randomString(6));
-      withEnv("discovery.type", "single-node");
-      withEnv("DISABLE_INSTALL_DEMO_CONFIG", "true");
+		withEnv("discovery.type", "single-node");
+		withEnv("DISABLE_INSTALL_DEMO_CONFIG", "true");
 		withEnv("DISABLE_SECURITY_PLUGIN", "true");
-      addExposedPort(9200);
-      
-      //String regex = ".*(\"message\":\\s?\"started\".*|] started\n$)";
-      //setWaitStrategy(new LogMessageWaitStrategy().withRegEx(regex));
-      setWaitStrategy((new HttpWaitStrategy()).forPort(9200).forStatusCodeMatching(response -> response == 200 || response == 401));
-      
-      
+		addExposedPort(9200);
+
+		// String regex = ".*(\"message\":\\s?\"started\".*|] started\n$)";
+		// setWaitStrategy(new LogMessageWaitStrategy().withRegEx(regex));
+		setWaitStrategy((new HttpWaitStrategy()).forPort(9200).forStatusCodeMatching(response -> response == 200 || response == 401));
+
 	}
 }
