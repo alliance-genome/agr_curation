92644248507e066349e72e520643d34deea52354
Olin Blodgett <nilo@nilosplace.net>
2022-11-30 10:29:30 -0500

    added for for alliance member lookup

--- src/main/java/org/alliancegenome/curation_api/model/entities/LoggedInPerson.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/LoggedInPerson.java
@@ -2,6 +2,7 @@ package org.alliancegenome.curation_api.model.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import org.alliancegenome.curation_api.constants.LinkMLSchemaConstants;
 import org.alliancegenome.curation_api.interfaces.AGRCurationSchemaVersion;
