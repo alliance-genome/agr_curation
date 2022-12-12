d51124643bcf2a0aa87d1d81088519e95cef0592
Christian Pich <cmpich@zfin.org>
2022-12-12 16:30:34 +0100

    add another convenience method

--- src/main/java/org/alliancegenome/curation_api/model/entities/Reference.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/Reference.java
@@ -1,12 +1,9 @@
 package org.alliancegenome.curation_api.model.entities;
 
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.Index;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.alliancegenome.curation_api.constants.LinkMLSchemaConstants;
 import org.alliancegenome.curation_api.interfaces.AGRCurationSchemaVersion;
 import org.alliancegenome.curation_api.view.View;
@@ -16,11 +13,13 @@ import org.hibernate.search.mapper.pojo.automaticindexing.ReindexOnUpdate;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexingDependency;
 
-import com.fasterxml.jackson.annotation.JsonView;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Audited
 @Entity
@@ -28,15 +27,44 @@ import lombok.ToString;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
 @ToString(callSuper = true)
 @Schema(name = "Reference", description = "POJO that represents the Reference")
-@AGRCurationSchemaVersion(min = "1.4.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = { InformationContentEntity.class }, partial = true)
+@AGRCurationSchemaVersion(min = "1.4.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = {InformationContentEntity.class}, partial = true)
 public class Reference extends InformationContentEntity {
 
 	@IndexedEmbedded(includeDepth = 1)
 	@IndexingDependency(reindexOnUpdate = ReindexOnUpdate.SHALLOW)
 	@ManyToMany
-	@JsonView({ View.FieldsOnly.class })
-	@JoinTable(indexes = { @Index(columnList = "Reference_curie"), @Index(columnList = "crossReferences_curie") })
+	@JsonView({View.FieldsOnly.class})
+	@JoinTable(indexes = {@Index(columnList = "Reference_curie"), @Index(columnList = "crossReferences_curie")})
 	@EqualsAndHashCode.Include
 	private List<CrossReference> crossReferences;
+
+
+	public static List<String> speciesPrefix;
+
+	static {
+		speciesPrefix.add("MGI");
+		speciesPrefix.add("ZFIN");
+		speciesPrefix.add("SGD");
+		speciesPrefix.add("FB");
+		speciesPrefix.add("WB");
+		speciesPrefix.add("XB");
+		speciesPrefix.add("OMIM");
+		speciesPrefix.add("GO");
+	}
+
+	/**
+	 * Retrieve PMID if available in the crossReference collection otherwise MOD ID
+	 */
+	public String getReferenceID() {
+		List<String> referencePrefixEnum = getCrossReferences().stream().map(CrossReference::getPrefix).collect(Collectors.toList());
+
+		Optional<CrossReference> opt = getCrossReferences().stream().filter(reference -> reference.getCurie().contains("PMID:")).findFirst();
+		// if no PUBMED ID try MOD ID
+		if (opt.isEmpty()) {
+			opt = getCrossReferences().stream().filter(reference -> speciesPrefix.contains(reference.getPrefix())).findFirst();
+		}
+		return opt.map(CrossReference::getPrefix).orElse(null);
+	}
+
 
 }
