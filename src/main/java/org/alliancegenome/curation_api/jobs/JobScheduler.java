123518ed1dae1fcf55433b85d65b3132d38e503a
Olin Blodgett <nilo@nilosplace.net>
2023-01-20 10:06:03 -0500

    Added scheduled indexer

--- src/main/java/org/alliancegenome/curation_api/jobs/JobScheduler.java
+++ src/main/java/org/alliancegenome/curation_api/jobs/JobScheduler.java
@@ -134,6 +134,13 @@ public class JobScheduler {
 			}
 		}
 	}
+	
+	@Scheduled(cron = "0 0 0 * * SUN")
+	public void runMassIndexerEverything() {
+		// Not sure what is going to happen when this time's out but should run anyway
+		// Defaults taken from the API endpoint
+		bulkLoadDAO.reindexEverything(1000, 10000, 0, 4, 7200, 1);
+	}
 
 	@Scheduled(every = "1s")
 	public void runFileJobs() {
