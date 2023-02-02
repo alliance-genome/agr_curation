c82d2ecd4a2fa7c7e2f265610aac330c27003a5c
Adam Gibson <agibson@mcw.edu>
2023-02-02 17:06:13 -0600

    SCRUM-2515 upgraded to PrimeReact 8.7

--- src/main/cliapp/src/containers/conditionRelationPage/__tests__/ConditionRelationTable.test.js
+++ src/main/cliapp/src/containers/conditionRelationPage/__tests__/ConditionRelationTable.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { waitFor } from "@testing-library/react";
+import { within, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithClient } from '../../../tools/jest/utils';
 import { ConditionRelationPage } from "../index";
 import { setLocalStorage } from "../../../tools/jest/setupTests";
@@ -41,5 +42,37 @@ describe("<ConditionRelationPage />", () => {
 			expect(relationTd).toBeInTheDocument();
 			expect(exConTd).toBeInTheDocument();
 		});
+	});
+
+	it("Has the same text in edit mode", async () => {
+
+		//jestdom doesn't support global.structuredClone so it needs to be mocked
+		global.structuredClone = jest.fn(val => {
+			return JSON.parse(JSON.stringify(val));
+		});
+
+		renderWithClient(<ConditionRelationPage />);
+
+		const user = userEvent.setup();
+
+		let cell;
+		let editButton;
+
+		await waitFor(() => {
+			cell = screen.getAllByRole('cell');
+			editButton = within(cell[0]).getByRole('button');
+		});
+
+		await user.click(editButton);
+
+		let handleInput = screen.getByRole('textbox', { name: /handle/i });
+		let referenceAutocomplete = screen.getByRole('combobox', { name: /singleReference/i });
+		let relationDropdown = screen.getByRole('button', { name: /has_condition/i });
+		let experimentalConditionsAutocomplete = screen.getByRole('combobox', { name: /conditions/i });
+		
+		expect(handleInput.value).toEqual('Standard');
+		expect(referenceAutocomplete.value).toEqual("PMID:28806732 (DOI:10.1371/journal.pgen.1006959|PMCID:PMC5570503|ZFIN:ZDB-PUB-170815-1|AGRKB:101000000675992)");
+		expect(relationDropdown.previousSibling.firstChild.nodeValue).toEqual("has_condition");
+		expect(experimentalConditionsAutocomplete.parentElement.previousSibling.firstChild.firstChild.nodeValue).toEqual("standard conditions");
 	});
 });
