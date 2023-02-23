c8a63642f5054e966a7ec1ff828d95199c9af26d
Andres Becerra <abecerra@ebi.ac.uk>
2023-02-23 19:05:01 +0000

    SCRUM-2282: remove console.logs

--- src/main/cliapp/src/__mocks__temp/SystemService.js
+++ src/main/cliapp/src/__mocks__temp/SystemService.js
@@ -3,7 +3,6 @@ import { BaseAuthService } from './BaseAuthService';
 export class SystemService extends BaseAuthService {
 
 	getSiteSummary() {
-        console.log("in mock siteSummary");
 	}
 
 }
