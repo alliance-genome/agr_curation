b9c3ede1fccf0bb469705d3b018efdbffab33ae4
markquintontulloch <markquintontulloch@gmail.com>
2023-03-21 13:22:00 +0000

    Migration to population data providers for biological entities

---
+++ src/main/resources/db/migration/v0.18.0.2__populate_missing_data_providers.sql
@@ -0,0 +1,49 @@
+ALTER TABLE dataprovider
+	ADD COLUMN tmp_curie varchar(255);
+	
+INSERT INTO dataprovider (id, tmp_curie)
+	SELECT nextval('hibernate_sequence'), curie FROM biologicalentity WHERE dataprovider_id IS NULL;
+	
+UPDATE biologicalentity 
+	SET dataprovider_id = id 
+	FROM dataprovider 
+	WHERE biologicalentity.dataprovider_id IS NULL AND biologicalentity.curie = dataprovider.tmp_curie;
+	
+UPDATE dataprovider
+	SET sourceorganization_id = organization.id
+	FROM organization
+	WHERE dataprovider.curie LIKE 'FB:%' AND organization.abbreviation = 'FB';
+	
+UPDATE dataprovider
+	SET sourceorganization_id = organization.id
+	FROM organization
+	WHERE dataprovider.curie LIKE 'WB:%' AND organization.abbreviation = 'WB';
+	
+UPDATE dataprovider
+	SET sourceorganization_id = organization.id
+	FROM organization
+	WHERE dataprovider.curie LIKE 'MGI:%' AND organization.abbreviation = 'MGI';
+	
+UPDATE dataprovider
+	SET sourceorganization_id = organization.id
+	FROM organization
+	WHERE dataprovider.curie LIKE 'RGD:%' AND organization.abbreviation = 'RGD';
+	
+UPDATE dataprovider
+	SET sourceorganization_id = organization.id
+	FROM organization
+	WHERE dataprovider.curie LIKE 'SGD:%' AND organization.abbreviation = 'SGD';
+	
+UPDATE dataprovider
+	SET sourceorganization_id = organization.id
+	FROM organization
+	WHERE dataprovider.curie LIKE 'ZFIN:%' AND organization.abbreviation = 'ZFIN';
+	
+UPDATE dataprovider
+	SET sourceorganization_id = organization.id
+	FROM organization
+	WHERE dataprovider.curie LIKE 'HGNC:%' AND organization.abbreviation = 'OMIM';
+	
+ALTER TABLE dataprovider
+	DROP COLUMN tmp_curie;
+	
