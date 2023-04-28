b91e94fbb9f0c89a6abb87751f5b6d6c34463d9e
Christian Pich <cmpich@zfin.org>
2023-04-28 16:38:21 +0200

    SCRUM-2450 load PATO into database (#995)
    * SCRUM-2450 load PATO into database
    
    * Updated pom
    
    * remove java source constraint
    
    * modify PatoTermCrudController to use correct IRI prefix
    
    * remove setting prefix
    
    ---------
    
    Co-authored-by: Olin Blodgett <nilo@nilosplace.net>

---
+++ src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/PatoTermCrudController.java
@@ -0,0 +1,27 @@
+package org.alliancegenome.curation_api.controllers.crud.ontology;
+
+import org.alliancegenome.curation_api.controllers.base.BaseOntologyTermController;
+import org.alliancegenome.curation_api.dao.ontology.PatoTermDAO;
+import org.alliancegenome.curation_api.interfaces.crud.ontology.PatoTermCrudInterface;
+import org.alliancegenome.curation_api.model.entities.ontology.PATOTerm;
+import org.alliancegenome.curation_api.services.helpers.GenericOntologyLoadConfig;
+import org.alliancegenome.curation_api.services.ontology.PatoTermService;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+@RequestScoped
+public class PatoTermCrudController extends BaseOntologyTermController<PatoTermService, PATOTerm, PatoTermDAO> implements PatoTermCrudInterface {
+
+	@Inject
+	PatoTermService patoTermService;
+
+	@Override
+	@PostConstruct
+	public void init() {
+		GenericOntologyLoadConfig config = new GenericOntologyLoadConfig();
+		setService(patoTermService, PATOTerm.class, config);
+	}
+
+}
