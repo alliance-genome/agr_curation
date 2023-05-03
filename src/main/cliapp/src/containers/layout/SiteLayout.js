96257cc37fe97ec2b38ce5e35a05a1e7d1872153
Christian Pich <cmpich@zfin.org>
2023-05-03 15:51:16 +0200

    SCRUM-24450 fix endpoint call for PATO (#1007)

--- src/main/cliapp/src/containers/layout/SiteLayout.js
+++ src/main/cliapp/src/containers/layout/SiteLayout.js
@@ -258,7 +258,7 @@ export const SiteLayout = (props) => {
 												{ label: 'Alliance Tags for Papers Ontology (ATP)', icon: 'pi pi-fw pi-home', to: '/ontology/atp' },
 												{ label: 'Ontology for Biomedical Investigations (OBI)', icon: 'pi pi-fw pi-home', to: '/ontology/obi' },
 												{ label: 'NCBI Organismal Classification (NCBITaxon)', icon: 'pi pi-fw pi-home', to: '/ontology/ncbitaxon' },
-												{ label: 'Phenotype and Trait Ontology (PATO)', icon: 'pi pi-fw pi-home', to: '/ontology/patotaxon' },
+												{ label: 'Phenotype and Trait Ontology (PATO)', icon: 'pi pi-fw pi-home', to: '/ontology/pato' },
 												{ label: 'C. elegans Phenotype Ontology (WBPhenotype)', icon: 'pi pi-fw pi-home', to: '/ontology/wbpheno' },
 												{ label: 'Human Phenotype Ontology (HP)', icon: 'pi pi-fw pi-home', to: '/ontology/hp' }
 										]
