6aa5a9ebfedd3e01fa506c19447717c25d9bd70d
Adam Gibson <agibson@mcw.edu>
2023-05-11 07:37:24 -0500

    SCRUM-2877 added basic component tests to most data tables

---
+++ src/main/cliapp/src/containers/vocabularyTermSetPage/mockData/mockData.js
@@ -0,0 +1,130 @@
+export const data = {
+	"internal": false,
+	"obsolete": false,
+	"id": 20284841,
+	"name": "AGM disease relations",
+	"vocabularyTermSetVocabulary": {
+			"dateCreated": "2022-01-26T09:39:10.441792Z",
+			"dateUpdated": "2022-01-26T09:39:10.441794Z",
+			"internal": false,
+			"obsolete": false,
+			"id": 6363601,
+			"name": "Disease Relation Vocabulary"
+	},
+	"memberTerms": [
+			{
+					"updatedBy": {
+							"dateCreated": "2022-05-03T18:33:16.445309Z",
+							"dateUpdated": "2022-05-03T18:33:16.445322Z",
+							"internal": false,
+							"obsolete": false,
+							"id": 7685810,
+							"uniqueId": "Chris|Grove|chris@wormbase.org",
+							"firstName": "Chris",
+							"lastName": "Grove",
+							"allianceMember": {
+									"internal": false,
+									"obsolete": false,
+									"id": 20308683,
+									"uniqueId": "WB",
+									"abbreviation": "WB",
+									"fullName": "WormBase",
+									"homepageResourceDescriptorPage": {
+											"internal": false,
+											"obsolete": false,
+											"dbDateCreated": "2023-02-20T09:26:39.609757Z",
+											"dbDateUpdated": "2023-05-04T22:00:09.611705Z",
+											"id": 41750788,
+											"name": "homepage",
+											"urlTemplate": "https://www.wormbase.org/"
+									}
+							},
+							"oktaId": "00u347gob7EQ6WsMz5d7",
+							"oktaEmail": "chris@wormbase.org",
+							"apiToken": "9df20f1f-f1dc-4d8d-8ba5-3daedc69cb9f"
+					},
+					"dateUpdated": "2022-08-19T01:04:13.175841Z",
+					"internal": false,
+					"obsolete": false,
+					"dbDateCreated": "2022-08-19T01:03:04.411367Z",
+					"dbDateUpdated": "2022-08-19T01:04:13.175984Z",
+					"id": 10964458,
+					"name": "is_ameliorated_model_of",
+					"definition": "A relation between an AGM and a disease for which the AGM is an ameliorated model of the disease",
+					"vocabulary": {
+							"dateCreated": "2022-01-26T09:39:10.441792Z",
+							"dateUpdated": "2022-01-26T09:39:10.441794Z",
+							"internal": false,
+							"obsolete": false,
+							"id": 6363601,
+							"name": "Disease Relation Vocabulary"
+					}
+			},
+			{
+					"updatedBy": {
+							"dateCreated": "2022-05-03T18:33:16.445309Z",
+							"dateUpdated": "2022-05-03T18:33:16.445322Z",
+							"internal": false,
+							"obsolete": false,
+							"id": 7685810,
+							"uniqueId": "Chris|Grove|chris@wormbase.org",
+							"firstName": "Chris",
+							"lastName": "Grove",
+							"allianceMember": {
+									"internal": false,
+									"obsolete": false,
+									"id": 20308683,
+									"uniqueId": "WB",
+									"abbreviation": "WB",
+									"fullName": "WormBase",
+									"homepageResourceDescriptorPage": {
+											"internal": false,
+											"obsolete": false,
+											"dbDateCreated": "2023-02-20T09:26:39.609757Z",
+											"dbDateUpdated": "2023-05-04T22:00:09.611705Z",
+											"id": 41750788,
+											"name": "homepage",
+											"urlTemplate": "https://www.wormbase.org/"
+									}
+							},
+							"oktaId": "00u347gob7EQ6WsMz5d7",
+							"oktaEmail": "chris@wormbase.org",
+							"apiToken": "9df20f1f-f1dc-4d8d-8ba5-3daedc69cb9f"
+					},
+					"dateUpdated": "2022-08-19T01:04:57.413042Z",
+					"internal": false,
+					"obsolete": false,
+					"dbDateCreated": "2022-08-19T01:04:57.420084Z",
+					"dbDateUpdated": "2022-08-19T01:04:57.420085Z",
+					"id": 10964461,
+					"name": "is_exacerbated_model_of",
+					"definition": "A relation between an AGM and a disease for which the AGM is an exacerbated model of the disease",
+					"vocabulary": {
+							"dateCreated": "2022-01-26T09:39:10.441792Z",
+							"dateUpdated": "2022-01-26T09:39:10.441794Z",
+							"internal": false,
+							"obsolete": false,
+							"id": 6363601,
+							"name": "Disease Relation Vocabulary"
+					}
+			},
+			{
+					"dateCreated": "2022-01-26T09:40:54.020724Z",
+					"dateUpdated": "2022-01-26T09:40:54.020726Z",
+					"internal": false,
+					"obsolete": false,
+					"id": 6363603,
+					"name": "is_model_of",
+					"definition": "is_model_of",
+					"vocabulary": {
+							"dateCreated": "2022-01-26T09:39:10.441792Z",
+							"dateUpdated": "2022-01-26T09:39:10.441794Z",
+							"internal": false,
+							"obsolete": false,
+							"id": 6363601,
+							"name": "Disease Relation Vocabulary"
+					}
+			}
+	],
+	"vocabularyTermSetDescription": "Disease relation vocabulary terms that are valid for AGM disease annotations"
+}
