7504fe0283998269d1451b4a65c7502a5ae0e0cb
Olin Blodgett <nilo@nilosplace.net>
2023-05-25 12:59:56 -0400

    Updated the table

--- src/main/resources/db/migration/v0.19.0.6__add_cleanup_param.sql
+++ src/main/resources/db/migration/v0.19.0.6__add_cleanup_param.sql
@@ -2,3 +2,6 @@ ALTER TABLE public.bulkloadfile_aud
    ADD bulkloadcleanup character varying(255);
 ALTER TABLE public.bulkloadfile
    ADD bulkloadcleanup character varying(255);
+
+UPDATE public.bulkloadfile_aud SET bulkloadcleanup = 'YES';
+UPDATE public.bulkloadfile SET bulkloadcleanup = 'YES';
