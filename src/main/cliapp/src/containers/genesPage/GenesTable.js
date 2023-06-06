106319408b13939e494199d68eb477314194d85c
Andres Becerra <abecerra@ebi.ac.uk>
2023-06-06 15:27:52 +0100

    SCRUM-2917: left aligning the column contents

--- src/main/cliapp/src/containers/genesPage/GenesTable.js
+++ src/main/cliapp/src/containers/genesPage/GenesTable.js
@@ -111,7 +111,7 @@ export const GenesTable = () => {
 					<Button className="p-button-text" 
 						onClick={(event) => { handleSymbolOpen(event, rowData, false) }} >
 							<EllipsisTableCell otherClasses={`b${rowData.curie.replace(':', '')}`}>
-								<div className='overflow-hidden text-overflow-ellipsis' dangerouslySetInnerHTML={{ __html: rowData.geneSymbol.formatText }} />								
+								<div style={{ textAlign: 'left' }} className='overflow-hidden text-overflow-ellipsis' dangerouslySetInnerHTML={{ __html: rowData.geneSymbol.formatText }} />
 							</EllipsisTableCell>
 							<Tooltip target={`.b${rowData.curie.replace(':', '')}`}>
 								<div dangerouslySetInnerHTML={{__html: rowData.geneSymbol.formatText}}/>
@@ -229,7 +229,7 @@ export const GenesTable = () => {
 			header: "Synonyms",
 			body: synonymsTemplate,
 			sortable: isEnabled,
-			filterConfig: FILTER_CONFIGS.geneSynonymsFilterConfig,
+			filterConfig: FILTER_CONFIGS.geneSynonymsFilterConfig
 		},
 		{
 			field: "geneSecondaryIds.secondaryId",
