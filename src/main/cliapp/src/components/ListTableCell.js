7c9ac5728b5a2f0405c4b553a67309076192e324
Andres Becerra <abecerra@ebi.ac.uk>
2023-06-12 15:16:50 +0100

    SCRUM-2917: align synonyms left

--- src/main/cliapp/src/components/ListTableCell.js
+++ src/main/cliapp/src/components/ListTableCell.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 export const ListTableCell = ({ template, listData, showBullets, style}) => {
 
-	const leftAlign = { textAlign: 'left' };
-	let alignment = style ?? leftAlign;
+	// const leftAlign = { textAlign: 'left' };
+	// let alignment = style ?? leftAlign;
 	let suppress = 'list-none'
 	if(showBullets)
 		suppress = ''
 	return (
 		<ul className={`pl-0 ${suppress}`}>
 			{listData?.map((item, index) =>
-				<li key={index} style={alignment}>
+				<li key={index}> 
+				{/* style={alignment}> */}
 					{template(item)}
 				</li>
 			)}
