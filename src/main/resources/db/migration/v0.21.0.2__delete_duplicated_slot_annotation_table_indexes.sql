028f766793e25f4ec8a41d36c32247cd8047aa4f
markquintontulloch <markquintontulloch@gmail.com>
2023-06-16 21:47:48 +0100

    Actually save changes

--- src/main/resources/db/migration/v0.21.0.2__delete_duplicated_slot_annotation_table_indexes.sql
+++ src/main/resources/db/migration/v0.21.0.2__delete_duplicated_slot_annotation_table_indexes.sql
@@ -1,31 +1,4 @@
-CREATE TABLE alleledatabasestatusslotannotation (
-	id bigint PRIMARY KEY,
-	singleallele_curie varchar(255),
-	databasestatus_id bigint
-);
-
-ALTER TABLE alleledatabasestatusslotannotation ADD CONSTRAINT alleledatabasestatus_singleallele_curie_fk FOREIGN KEY (singleallele_curie) REFERENCES allele (curie);
-ALTER TABLE alleledatabasestatusslotannotation ADD CONSTRAINT alleledatabasestatus_databasestatus_id_fk FOREIGN KEY (databasestatus_id) REFERENCES vocabularyterm (id);
-ALTER TABLE alleledatabasestatusslotannotation ADD CONSTRAINT alleledatabasestatus_id_fk FOREIGN KEY (id) REFERENCES slotannotation (id);
-
-CREATE INDEX alleledatabasestatus_singleallele_curie_index ON alleledatabasestatusslotannotation USING btree (singleallele_curie);
-CREATE INDEX alleledatabasestatus_databasestatus_id_index ON alleledatabasestatusslotannotation USING btree (databasestatus_id);
-
-CREATE TABLE alleledatabasestatusslotannotation_aud (
-    id bigint NOT NULL,
-    rev integer NOT NULL,
-	singleallele_curie varchar(255),
-	databasestatus_id bigint
-);
-
-ALTER TABLE alleledatabasestatusslotannotation_aud ADD PRIMARY KEY (id, rev);
-
-ALTER TABLE alleledatabasestatusslotannotation_aud ADD CONSTRAINT alleledatabasestatus_aud_id_rev_fk FOREIGN KEY (id, rev) REFERENCES slotannotation_aud (id, rev);
-
-INSERT INTO vocabulary (id, name) VALUES (nextval('hibernate_sequence'), 'Allele database status vocabulary');
-	
-INSERT INTO vocabularyterm (id, name, vocabulary_id) SELECT nextval('hibernate_sequence'), 'approved', id FROM vocabulary WHERE name = 'Allele database status vocabulary';
-INSERT INTO vocabularyterm (id, name, vocabulary_id) SELECT nextval('hibernate_sequence'), 'deleted', id FROM vocabulary WHERE name = 'Allele database status vocabulary';
-INSERT INTO vocabularyterm (id, name, vocabulary_id) SELECT nextval('hibernate_sequence'), 'reserved', id FROM vocabulary WHERE name = 'Allele database status vocabulary';
-INSERT INTO vocabularyterm (id, name, vocabulary_id) SELECT nextval('hibernate_sequence'), 'autoloaded', id FROM vocabulary WHERE name = 'Allele database status vocabulary';
-INSERT INTO vocabularyterm (id, name, vocabulary_id) SELECT nextval('hibernate_sequence'), 'in_progress', id FROM vocabulary WHERE name = 'Allele database status vocabulary';
+DROP INDEX genefullname_singleallele_curie_index;
+DROP INDEX genesymbol_singleallele_curie_index;
+DROP INDEX genesynonym_singleallele_curie_index;
+DROP INDEX genesystematicname_singleallele_curie_index;
