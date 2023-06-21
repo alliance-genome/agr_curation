87fc6e705609857e122697b7a6fea3ce6af439f9
markquintontulloch <markquintontulloch@gmail.com>
2023-06-21 13:22:54 +0100

    Load APO ontology

---
+++ src/main/java/org/alliancegenome/curation_api/interfaces/crud/ontology/ApoTermCrudInterface.java
@@ -0,0 +1,18 @@
+package org.alliancegenome.curation_api.interfaces.crud.ontology;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.alliancegenome.curation_api.interfaces.base.BaseOntologyTermCrudInterface;
+import org.alliancegenome.curation_api.model.entities.ontology.APOTerm;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/apoterm")
+@Tag(name = "CRUD - Ontology")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface ApoTermCrudInterface extends BaseOntologyTermCrudInterface<APOTerm> {
+
+}
