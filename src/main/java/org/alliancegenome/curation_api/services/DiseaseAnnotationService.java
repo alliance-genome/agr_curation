2156128991e1ba786ade4e61bcd8b7690ae19caa
markquintontulloch <markquintontulloch@gmail.com>
2023-07-04 11:25:27 +0100

    Create temporary endpoint for DA dataProvider reset

--- src/main/java/org/alliancegenome/curation_api/services/DiseaseAnnotationService.java
+++ src/main/java/org/alliancegenome/curation_api/services/DiseaseAnnotationService.java
@@ -8,13 +8,18 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 
+import org.alliancegenome.curation_api.dao.CrossReferenceDAO;
+import org.alliancegenome.curation_api.dao.DataProviderDAO;
 import org.alliancegenome.curation_api.dao.DiseaseAnnotationDAO;
 import org.alliancegenome.curation_api.exceptions.ApiErrorException;
+import org.alliancegenome.curation_api.model.entities.DataProvider;
 import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
 import org.alliancegenome.curation_api.model.entities.Note;
+import org.alliancegenome.curation_api.model.input.Pagination;
 import org.alliancegenome.curation_api.response.ObjectResponse;
 import org.alliancegenome.curation_api.response.SearchResponse;
 import org.alliancegenome.curation_api.services.base.BaseEntityCrudService;
+import org.alliancegenome.curation_api.util.ProcessDisplayHelper;
 import org.apache.commons.collections.CollectionUtils;
 
 import lombok.extern.jbosslog.JBossLog;
@@ -31,6 +36,10 @@ public class DiseaseAnnotationService extends BaseEntityCrudService<DiseaseAnnot
 	LoggedInPersonService loggedInPersonService;
 	@Inject
 	PersonService personService;
+	@Inject
+	DataProviderDAO dataProviderDAO;
+	@Inject
+	CrossReferenceDAO crossReferenceDAO;
 
 	@Override
 	@PostConstruct
@@ -100,6 +109,52 @@ public class DiseaseAnnotationService extends BaseEntityCrudService<DiseaseAnnot
 		}
 
 		return null;
+	}
+	
+	public void resetDataProviders() {
+		// TODO remove once SCRUM-3037 resolved
+		ProcessDisplayHelper pdh = new ProcessDisplayHelper();
+		int page = 0;
+		int limit = 500;
+		Pagination pagination = new Pagination(page, limit);
+
+		Boolean allSynced = false;
+		while (!allSynced) {
+			pagination.setPage(page);
+			SearchResponse<String> response = diseaseAnnotationDAO.findAllIds(pagination);
+			if (page == 0)
+				pdh.startProcess("DiseaseAnnotation data provider reset", response.getTotalResults());
+			for (String daId : response.getResults()) {
+				resetDataProvider(Long.parseLong(daId));
+				pdh.progressProcess();
+			}
+			page = page + 1;
+			int nrSynced = limit * page;
+			if (nrSynced > response.getTotalResults().intValue()) {
+				nrSynced = response.getTotalResults().intValue();
+				allSynced = true;
+			}
+		}
+		pdh.finishProcess();
+	}
+
+	private void resetDataProvider(Long id) {
+		// TODO remove once SCRUM-3037 resolved
+		DiseaseAnnotation annotation = diseaseAnnotationDAO.find(id);
+		if (annotation == null)
+			return;
+		
+		DataProvider newDataProvider = new DataProvider();
+		DataProvider oldDataProvider = annotation.getDataProvider();
+		
+		newDataProvider.setSourceOrganization(oldDataProvider.getSourceOrganization());	
+		annotation.setDataProvider(newDataProvider);
+		diseaseAnnotationDAO.merge(annotation);
+		
+		Long xrefId = oldDataProvider.getCrossReference() == null ? null : oldDataProvider.getCrossReference().getId();
+		dataProviderDAO.remove(oldDataProvider.getId());
+		if (xrefId != null)
+			crossReferenceDAO.remove(xrefId);
 	}
 
 }
