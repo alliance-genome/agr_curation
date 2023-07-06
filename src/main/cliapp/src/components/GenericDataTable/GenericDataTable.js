2ea8f3bb85152efe547a416cf0655acf3b162270
Adam Gibson <agibson@mcw.edu>
2023-07-06 10:57:06 -0500

    SCRUM-3030 disabled row dup for all rows while in row edit

--- src/main/cliapp/src/components/GenericDataTable/GenericDataTable.js
+++ src/main/cliapp/src/components/GenericDataTable/GenericDataTable.js
@@ -275,7 +275,7 @@ export const GenericDataTable = (props) => {
 					{duplicationEnabled &&
 						<Column field="duplicate" 
 						editor={(props) => <DuplicationAction props={props} handleDuplication={handleDuplication} disabled={true}/>} 
-						body={(props) => <DuplicationAction props={props} handleDuplication={handleDuplication} disabled={false}/>} 
+						body={(props) => <DuplicationAction props={props} handleDuplication={handleDuplication} disabled={!isEnabled}/>} 
 						showFilterMenu={false} style={{maxWidth: '4rem', minWidth: '4rem', display: props.isEditable ? 'visible' : 'none' }} headerStyle={{ width: '4rem', position: 'sticky' }} bodyStyle={{textAlign: 'center'}}
 						frozen headerClassName='surface-0'/>
 					}
