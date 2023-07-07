4d6c40cce0e5f2b7071b3292deec19cf58f4348b
Adam Gibson <agibson@mcw.edu>
2023-07-07 16:27:10 -0500

    SCRUM-3009 display allele data on page

--- src/main/cliapp/src/containers/allelesDetailPage/AllelesDetailComponent.js
+++ src/main/cliapp/src/containers/allelesDetailPage/AllelesDetailComponent.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
+import ReactJson from 'react-json-view'
+import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { AlleleService } from '../../service/AlleleService';
 
-export const AllelesDetailComponent = ({ curie }) => {
-	const [allele, setAllele] = useState({});
+export const AllelesDetailComponent = () => {
+	const [allele, setAllele] = useState();
+	const { curie } = useParams();
+	const { layoutColorMode } = localStorage.getItem("themeSettings");
 	//call service
 	const alleleService = new AlleleService();
 	//retrieve data
 	useQuery([curie],
 		() => alleleService.getAllele(curie), 
 		{
-			onSuccess: (data) => {
-				setAllele(data.result);
+			onSuccess: (result) => {
+				// const alleleString = JSON.stringify(result.data.entity);
+				setAllele(result?.data?.entity);
 			},
 			onError: (error) => {
 				console.warn(error);
@@ -21,6 +26,11 @@ export const AllelesDetailComponent = ({ curie }) => {
 		}
 	);
 	//display output data
-	return(<>{allele}</>)
+	return(
+		<>
+			<h1>Allele Detail Page</h1>
+			<ReactJson src={allele} theme={layoutColorMode === "light" ? "rjv-default" : "google"} />
+		</>
+	)
 	
 };
