8adb58da04ac6f4fd92624c75cedf186aae07acd
markquintontulloch <markquintontulloch@gmail.com>
2023-07-11 22:25:58 +0100

    Revert import changes

--- src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/BspoTermCrudController.java
+++ src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/BspoTermCrudController.java
@@ -1,15 +1,15 @@
 package org.alliancegenome.curation_api.controllers.crud.ontology;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
 import org.alliancegenome.curation_api.controllers.base.BaseOntologyTermController;
 import org.alliancegenome.curation_api.dao.ontology.BspoTermDAO;
 import org.alliancegenome.curation_api.interfaces.crud.ontology.BspoTermCrudInterface;
 import org.alliancegenome.curation_api.model.entities.ontology.BSPOTerm;
 import org.alliancegenome.curation_api.services.helpers.GenericOntologyLoadConfig;
 import org.alliancegenome.curation_api.services.ontology.BspoTermService;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
 
 @RequestScoped
 public class BspoTermCrudController extends BaseOntologyTermController<BspoTermService, BSPOTerm, BspoTermDAO> implements BspoTermCrudInterface {
