5a8e68b16b3eb41b0d4f9ce201d8958e7c398ab9
Manuel Luypaert <luypaert@ebi.ac.uk>
2023-08-08 14:56:31 +0100

    Replaced OMIM UI DataProvider

--- src/main/cliapp/src/service/DataLoadService.js
+++ src/main/cliapp/src/service/DataLoadService.js
@@ -91,7 +91,7 @@ export class DataLoadService extends BaseAuthService {
 
 		getDataProviders() {
 				return [
-					"FB", "MGI", "OMIM", "RGD", "SGD", "WB", "XB", "ZFIN"
+					"FB", "MGI", "HUMAN", "RGD", "SGD", "WB", "XB", "ZFIN"
 				];
 		}
 
