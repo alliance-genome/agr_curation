435316e66a00cca9cf04a64b93a7cb521daf3f59
markquintontulloch <markquintontulloch@gmail.com>
2023-08-08 13:15:29 +0100

    UI update for vocabularies

--- src/main/cliapp/src/containers/vocabularyPage/VocabulariesTable.js
+++ src/main/cliapp/src/containers/vocabularyPage/VocabulariesTable.js
@@ -1,11 +1,17 @@
 import React, { useRef, useState } from 'react'
+import { useMutation } from 'react-query';
 import { Toast } from 'primereact/toast';
 import { BooleanTemplate } from '../../components/BooleanTemplate';
+import { VocabularyService } from "../../service/VocabularyService";
+import { useControlledVocabularyService } from "../../service/useControlledVocabularyService";
 import { GenericDataTable } from '../../components/GenericDataTable/GenericDataTable';
 import { EllipsisTableCell } from '../../components/EllipsisTableCell';
+import { ErrorMessageComponent } from "../../components/ErrorMessageComponent";
 import { Tooltip } from 'primereact/tooltip';
 import { getDefaultTableState } from '../../service/TableStateService';
 import { FILTER_CONFIGS } from '../../constants/FilterFields';
+import { InputTextEditor } from "../../components/InputTextEditor";
+import { TrueFalseDropdown } from "../../components/TrueFalseDropDownSelector";
 
 export const VocabulariesTable = () => {
 
@@ -14,44 +20,89 @@ export const VocabulariesTable = () => {
 
 	const toast_topleft = useRef(null);
 	const toast_topright = useRef(null);
+	const errorMessagesRef = useRef();
+	errorMessagesRef.current = errorMessages;
 
-	const nameBodyTemplate = (rowData) => {
+	const obsoleteTerms = useControlledVocabularyService('generic_boolean_terms');
+
+	let vocabularyService = new VocabularyService();
+
+	const mutation = useMutation(updatedVocabulary => {
+		if (!vocabularyService) {
+			vocabularyService = new VocabularyService();
+		}
+		return vocabularyService.saveVocabulary(updatedVocabulary);
+	});
+	
+	const stringBodyTemplate = (rowData, field) => {
 		return (
 			<>
-				<EllipsisTableCell otherClasses={`a${rowData.id}`}>{rowData.name}</EllipsisTableCell>
-				<Tooltip target={`.a${rowData.id}`} content={rowData.name} style={{ width: '450px', maxWidth: '450px' }} />
+				<EllipsisTableCell otherClasses={`${field}${rowData.id}`}>{rowData[field]}</EllipsisTableCell>
+				<Tooltip target={`.${field}${rowData.id}`} content={rowData[field]} style={{ width: '450px', maxWidth: '450px' }} />
 			</>
 		)
 	}
 
-	const descriptionBodyTemplate = (rowData) => {
+	const stringEditor = (props, field) => {
 		return (
 			<>
-				<EllipsisTableCell otherClasses={`b${rowData.id}`}>{rowData.vocabularyDescription}</EllipsisTableCell>
-				<Tooltip target={`.b${rowData.id}`} content={rowData.vocabularyDescription} style={{ width: '450px', maxWidth: '450px' }} />
+				<InputTextEditor
+					rowProps={props}
+					fieldName={field}
+				/>
+				<ErrorMessageComponent errorMessages={errorMessagesRef.current[props.rowIndex]} errorField={field}/>
 			</>
-		)
-	}
+		);
+	};
 
+	const obsoleteEditorTemplate = (props) => {
+		return (
+			<>
+				<TrueFalseDropdown
+					options={obsoleteTerms}
+					editorChange={onObsoleteEditorValueChange}
+					props={props}
+					field={"obsolete"}
+				/>
+				<ErrorMessageComponent errorMessages={errorMessagesRef.current[props.rowIndex]} errorField={"obsolete"} />
+			</>
+		);
+	};
+	
+	const onObsoleteEditorValueChange = (props, event) => {
+		let updatedTerms = [...props.props.value];
+		if (event.value || event.value === '') {
+			updatedTerms[props.rowIndex].obsolete = JSON.parse(event.value.name);
+		}
+	};
 
 	const columns = [
 		{ 
 			field: "name", 
 			header: "Name", 
-			body: nameBodyTemplate,
-			filterConfig: FILTER_CONFIGS.nameFilterConfig
+			body: (rowData) => stringBodyTemplate(rowData, "name"),
+			filterConfig: FILTER_CONFIGS.nameFilterConfig,
+			editor: (props) => stringEditor(props, "name")
 		},
 		{ 
 			field: "vocabularyDescription", 
 			header: "Description", 
-			body: descriptionBodyTemplate,
-			filterConfig: FILTER_CONFIGS.vocabularyDescriptionFilterConfig
+			body: (rowData) => stringBodyTemplate(rowData, "vocabularyDescription"),
+			filterConfig: FILTER_CONFIGS.vocabularyDescriptionFilterConfig,
+			editor: (props) => stringEditor(props, "vocabularyDescription")
 		},
 		{ 
 			field: "obsolete", 
 			header: "Obsolete", 
 			body: (rowData) => <BooleanTemplate value={rowData.obsolete}/>,
-			filterConfig: FILTER_CONFIGS.obsoleteFilterConfig
+			filterConfig: FILTER_CONFIGS.obsoleteFilterConfig,
+			editor: (props) => obsoleteEditorTemplate(props)
+		},
+		{
+			field: "vocabularyLabel",
+			header: "Label",
+			filterConfig: FILTER_CONFIGS.vocabularyLabelFilterConfig,
+			body: (rowData) => stringBodyTemplate(rowData, "vocabularyLabel")
 		}
 	]
 
@@ -77,12 +128,15 @@ export const VocabulariesTable = () => {
 					columns={columns}	 
 					defaultColumnNames={defaultColumnNames}
 					initialTableState={initialTableState}
-					isEditable={false}
+					isEditable={true}
+					mutation={mutation}
 					isEnabled={isEnabled}
 					setIsEnabled={setIsEnabled}
 					toasts={{toast_topleft, toast_topright }}
 					errorObject = {{errorMessages, setErrorMessages}}
 					widthsObject={widthsObject}
+					deletionEnabled={true}
+					deletionMethod={vocabularyService.deleteVocabulary}
 				/>
 			</div>
 	)
