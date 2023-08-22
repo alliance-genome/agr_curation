986847830e27d29edbdb4910d1f0deb1571967ff
Christian Pich <cmpich@zfin.org>
2023-08-22 05:38:42 +0200

    SCRUM-2784 make short citation show up next to the entry box when selected

--- src/main/cliapp/src/components/FieldData/SingleReferenceAdditionalFieldData.js
+++ src/main/cliapp/src/components/FieldData/SingleReferenceAdditionalFieldData.js
@@ -3,16 +3,15 @@ import React from 'react';
 export function SingleReferenceAdditionalFieldData({ fieldData }) {
 	let ret = [];
 	if(!fieldData || fieldData.curie === '') return ret;
-
 	if (fieldData['cross_references'] && fieldData['cross_references'].length > 0 ){
-		createCrossReferenceDivs(ret, fieldData['cross_references'], 'curie');
+		createCrossReferenceDivs(ret, fieldData['cross_references'], 'curie', fieldData['short_citation']);
 	} else if(fieldData['crossReferences'] && fieldData['crossReferences'].length > 0 ){//if the crossReference is coming from rowData
-		createCrossReferenceDivs(ret, fieldData['crossReferences'], 'displayName');
+		createCrossReferenceDivs(ret, fieldData['crossReferences'], 'displayName', fieldData['short_citation']);
 	}
 	return ret;
 };
 
-const createCrossReferenceDivs = (resultList, crossReferences, field) => {
+const createCrossReferenceDivs = (resultList, crossReferences, field, shortCitation) => {
 	let modList = ["PMID", "FB", "MGI", "RGD", "SGD", "WB", "ZFIN", "XB"];
 	for(let i = 0; i < crossReferences.length; i++){
 		let prefix = crossReferences[i][field]?.split(":")[0];
@@ -20,4 +19,5 @@ const createCrossReferenceDivs = (resultList, crossReferences, field) => {
 			resultList.push(<div key={i} className="p-info">{crossReferences[i][field]}</div>);
 		}
 	}
+	resultList.push(<div key={shortCitation} className="p-info">{shortCitation}</div>);
 }
