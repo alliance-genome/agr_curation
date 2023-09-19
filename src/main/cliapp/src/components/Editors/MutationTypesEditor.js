77e1758beebf6440306e90f51ae1402c17a9ddd0
Adam Gibson <agibson@mcw.edu>
2023-09-19 10:33:06 -0500

    SCRUM-3128 add MutationTypesEditor

---
+++ src/main/cliapp/src/components/Editors/MutationTypesEditor.js
@@ -0,0 +1,37 @@
+import { AutocompleteMultiEditor } from "../Autocomplete/AutocompleteMultiEditor";
+import { SearchService } from '../../service/SearchService';
+import { autocompleteSearch, buildAutocompleteFilter } from '../../utils/utils';
+import { SubjectAutocompleteTemplate } from "../Autocomplete/SubjectAutocompleteTemplate";
+import { DialogErrorMessageComponent } from "../Error/DialogErrorMessageComponent";
+
+const mutationTypeSearch = (event, setFiltered, setInputValue) => {
+	const searchService = new SearchService();
+	const autocompleteFields = ["name", "curie"];
+	const endpoint = "soterm";
+	const filterName = "mutationTypeFilter";
+	const filter = buildAutocompleteFilter(event, autocompleteFields);
+
+	setInputValue(event.query);
+	autocompleteSearch(searchService, endpoint, filterName, filter, setFiltered);
+};
+export const MutationTypesEditor = ({ props, errorMessages, onChange }) => {
+	console.log("rowData", props.rowData);
+	return (
+		<>
+			<AutocompleteMultiEditor
+				search={mutationTypeSearch}
+				initialValue={props?.rowData?.mutationTypes}
+				rowProps={props}
+				fieldName='mutationTypes'
+				subField='curie'
+				valueDisplay={(item, setAutocompleteHoverItem, op, query) =>
+					<SubjectAutocompleteTemplate item={item} setAutocompleteHoverItem={setAutocompleteHoverItem} op={op} query={query} />}
+				onValueChangeHandler={onChange}
+			/>
+			<DialogErrorMessageComponent
+				errorMessages={errorMessages[props?.rowIndex]}
+				errorField={"mutationTypes"}
+			/>
+		</>
+	);
+};
