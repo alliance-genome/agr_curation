5b1c2c30709cb07afd53a310599e7be36173e0f8
Olin Blodgett <nilo@nilosplace.net>
2023-09-21 18:50:56 -0400

    Fix NPE

--- src/main/java/org/alliancegenome/curation_api/services/helpers/GenericOntologyLoadHelper.java
+++ src/main/java/org/alliancegenome/curation_api/services/helpers/GenericOntologyLoadHelper.java
@@ -195,9 +195,12 @@ public class GenericOntologyLoadHelper<T extends OntologyTerm> implements OWLObj
 					}
 				}
 			}
-			
-			currentTerm.setChildCount(currentTerm.getIsaChildren().size());
-			currentTerm.setDescendantCount(currentTerm.getIsaDescendants().size());
+			if(currentTerm.getIsaChildren() != null) {
+				currentTerm.setChildCount(currentTerm.getIsaChildren().size());
+			}
+			if(currentTerm.getIsaDescendants() != null) {
+				currentTerm.setDescendantCount(currentTerm.getIsaDescendants().size());
+			}
 		}
 
 		return currentTerm;
