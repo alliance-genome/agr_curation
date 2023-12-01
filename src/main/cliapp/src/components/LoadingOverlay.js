5dd6179c4daa66b6c9a66fa4ba4dd39ef39bd901
Adam Gibson <agibson@mcw.edu>
2023-12-01 13:03:48 -0600

    SCRUM-3536 add text to LoadingOverlay

--- src/main/cliapp/src/components/LoadingOverlay.js
+++ src/main/cliapp/src/components/LoadingOverlay.js
@@ -2,6 +2,9 @@ export const LoadingOverlay = ({ isLoading }) => {
   if (!isLoading) return null;
   return (
     <div className="top-0 left-0 w-full h-full opacity-90 fixed p-5 surface-overlay z-2 origin-top">
+      <div className='flex align-items-center justify-content-center h-screen'>
+        <h1>Saving in progress...</h1>
+      </div>
     </div>
   );
 };
