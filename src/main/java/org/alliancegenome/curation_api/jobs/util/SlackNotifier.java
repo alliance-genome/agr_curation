d1d5a29317cf02b412f0491909be33f98e024de1
VarunReddy1111 <varunreddy.go@gmail.com>
2023-12-07 15:22:48 -0600

    added few more attributes

--- src/main/java/org/alliancegenome/curation_api/jobs/util/SlackNotifier.java
+++ src/main/java/org/alliancegenome/curation_api/jobs/util/SlackNotifier.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.alliancegenome.curation_api.enums.BackendBulkLoadType;
+import org.alliancegenome.curation_api.enums.JobStatus;
 import org.alliancegenome.curation_api.model.entities.bulkloads.BulkLoad;
 import org.alliancegenome.curation_api.model.entities.bulkloads.BulkLoadFile;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -71,37 +72,43 @@ public class SlackNotifier {
 
 	public void slackalert(BulkLoad bulkLoad) {
 		
-		List<Field> fields = new ArrayList<>();
-		fields.add(new Field("Load Type", String.valueOf(bulkLoad.getBackendBulkLoadType()), true));
-		if(bulkLoad.getBackendBulkLoadType() == BackendBulkLoadType.ONTOLOGY) {
-			fields.add(new Field("Ontology Type", String.valueOf(bulkLoad.getOntologyType()), true));
+		if(bulkLoad.getBulkloadStatus() == JobStatus.FAILED) {
+		
+			List<Field> fields = new ArrayList<>();
+			fields.add(new Field("Load Type", String.valueOf(bulkLoad.getBackendBulkLoadType()), true));
+			if(bulkLoad.getBackendBulkLoadType() == BackendBulkLoadType.ONTOLOGY) {
+				fields.add(new Field("Ontology Type", String.valueOf(bulkLoad.getOntologyType()), true));
+			}
+			slackalert(
+					
+					bulkLoad.getGroup().getName(), 
+					bulkLoad.getName(),
+					bulkLoad.getErrorMessage(),
+					fields
+					);
 		}
-		slackalert(
-				
-				bulkLoad.getGroup().getName(), 
-				bulkLoad.getName(),
-				bulkLoad.getErrorMessage(),
-				fields
-				);
 	}
 
 	public void slackalert(BulkLoadFile bulkLoadFile) {
 		
-		List<Field> fields = new ArrayList<>();
-		fields.add(new Field("Load Type", String.valueOf(bulkLoadFile.getBulkLoad().getBackendBulkLoadType()), true));
-		if(bulkLoadFile.getBulkLoad().getBackendBulkLoadType() == BackendBulkLoadType.ONTOLOGY) {
-			fields.add(new Field("Ontology Type", String.valueOf(bulkLoadFile.getBulkLoad().getOntologyType()), true));
-		}
-		fields.add(new Field("MD5Sum", bulkLoadFile.getMd5Sum(), true));
-		fields.add(new Field("File Size", String.valueOf(bulkLoadFile.getFileSize()), true));
-		fields.add(new Field("LinkML Version", bulkLoadFile.getLinkMLSchemaVersion(), true));
-		fields.add(new Field("Alliance Member Release Version", bulkLoadFile.getAllianceMemberReleaseVersion(), false));
 		
-		slackalert(
-				bulkLoadFile.getBulkLoad().getGroup().getName(),
-				bulkLoadFile.getBulkLoad().getName(),
-				bulkLoadFile.getErrorMessage(),
-				fields
-				);
+		if(bulkLoadFile.getBulkloadStatus() == JobStatus.FAILED) {
+			List<Field> fields = new ArrayList<>();
+			fields.add(new Field("Load Type", String.valueOf(bulkLoadFile.getBulkLoad().getBackendBulkLoadType()), true));
+			if(bulkLoadFile.getBulkLoad().getBackendBulkLoadType() == BackendBulkLoadType.ONTOLOGY) {
+				fields.add(new Field("Ontology Type", String.valueOf(bulkLoadFile.getBulkLoad().getOntologyType()), true));
+			}
+			fields.add(new Field("MD5Sum", bulkLoadFile.getMd5Sum(), true));
+			fields.add(new Field("File Size", String.valueOf(bulkLoadFile.getFileSize()), true));
+			fields.add(new Field("LinkML Version", bulkLoadFile.getLinkMLSchemaVersion(), true));
+			fields.add(new Field("Alliance Member Release Version", bulkLoadFile.getAllianceMemberReleaseVersion(), false));
+			
+			slackalert(
+					bulkLoadFile.getBulkLoad().getGroup().getName(),
+					bulkLoadFile.getBulkLoad().getName(),
+					bulkLoadFile.getErrorMessage(),
+					fields
+					);
+		}
 	}
 }
