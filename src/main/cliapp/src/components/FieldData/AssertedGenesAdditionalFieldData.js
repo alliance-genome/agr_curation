d5359bc6e12d270304626c33b0cfe9abb2d6d519
markquintontulloch <markquintontulloch@gmail.com>
2024-01-04 01:45:35 +0000

    Fix typo

--- src/main/cliapp/src/components/FieldData/AssertedGenesAdditionalFieldData.js
+++ src/main/cliapp/src/components/FieldData/AssertedGenesAdditionalFieldData.js
@@ -5,7 +5,7 @@ export function AssertedGenesAdditionalFieldData({ fieldData }) {
 	let ret = [];
 	if (fieldData && fieldData.length >0) {
 		for(let i=0; i< fieldData.length; i++){
-			if (getIdentifty(fieldData[i]) !== '' && fieldData[i]["type"] === "Gene")
+			if (getIdentifier(fieldData[i]) !== '' && fieldData[i]["type"] === "Gene")
 				ret.push(<div key={i} className="p-info" dangerouslySetInnerHTML={{__html: fieldData[i]["geneSymbol"]["displayText"]}}></div>);
 		}
 	}
