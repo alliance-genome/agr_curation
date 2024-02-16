253f4c605bd47978d883e8f5bbf2a16470aba9b6
Olin Blodgett <nilo@nilosplace.net>
2024-02-16 14:17:11 -0500

    Fix for failing ontologies

--- src/main/java/org/alliancegenome/curation_api/jobs/executors/GeneDiseaseAnnotationExecutor.java
+++ src/main/java/org/alliancegenome/curation_api/jobs/executors/GeneDiseaseAnnotationExecutor.java
@@ -79,7 +79,7 @@ public class GeneDiseaseAnnotationExecutor extends LoadFileExecutor {
 		return new LoadHistoryResponce(history);
 	}
 	
-	public void runLoad(BulkLoadFileHistory history, BackendBulkDataProvider dataProvider, List<GeneDiseaseAnnotationDTO> annotations, List<Long> idsAdded) {
+	private void runLoad(BulkLoadFileHistory history, BackendBulkDataProvider dataProvider, List<GeneDiseaseAnnotationDTO> annotations, List<Long> idsAdded) {
 
 		ProcessDisplayHelper ph = new ProcessDisplayHelper(2000);
 		ph.addDisplayHandler(loadProcessDisplayService);
---
+++ src/main/resources/db/migration/v0.29.0.5__synonym_name_length.sql
@@ -0,0 +1,1 @@
+ALTER TABLE synonym ALTER COLUMN name TYPE text;
