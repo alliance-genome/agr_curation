9182f29c85b7d5783515c294854adcafdb09a67f
Christian Pich <cmpich@zfin.org>
2024-02-27 13:05:22 +0100

    SCRUM-3593 override search endpoint to use different JSON View to rem… (#1441)
    * SCRUM-3593 override search endpoint to use different JSON View to remove unused objects
    
    * Update DiseaseAnnotationCrudInterface.java
    
    ---------
    
    Co-authored-by: Olin Blodgett <nilo@nilosplace.net>

--- src/main/java/org/alliancegenome/curation_api/interfaces/crud/DiseaseAnnotationCrudInterface.java
+++ src/main/java/org/alliancegenome/curation_api/interfaces/crud/DiseaseAnnotationCrudInterface.java
@@ -1,19 +1,20 @@
 package org.alliancegenome.curation_api.interfaces.crud;
 
+import jakarta.ws.rs.*;
 import org.alliancegenome.curation_api.interfaces.base.BaseIdCrudInterface;
 import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
+import org.alliancegenome.curation_api.model.entities.VocabularyTerm;
 import org.alliancegenome.curation_api.response.ObjectResponse;
+import org.alliancegenome.curation_api.response.SearchResponse;
 import org.alliancegenome.curation_api.view.View;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.HashMap;
 
 @Path("/disease-annotation")
 @Tag(name = "CRUD - Disease Annotations")
@@ -25,5 +26,12 @@ public interface DiseaseAnnotationCrudInterface extends BaseIdCrudInterface<Dise
 	@Path("/findBy/{modEntityId|modInternalId|uniqueId}")
 	@JsonView(View.FieldsAndLists.class)
 	public ObjectResponse<DiseaseAnnotation> get(@PathParam("identifier") String identifier);
+
+	@Override
+	@POST
+	@Path("/search")
+	@JsonView(View.DiseaseAnnotation.class)
+	@Tag(name = "Elastic Search Disease Annotations")
+	public SearchResponse<DiseaseAnnotation> search(@DefaultValue("0") @QueryParam("page") Integer page, @DefaultValue("10") @QueryParam("limit") Integer limit, @RequestBody HashMap<String, Object> params);
 
 }
