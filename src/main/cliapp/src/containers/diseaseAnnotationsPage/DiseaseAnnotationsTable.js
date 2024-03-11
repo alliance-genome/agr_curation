52725d046bb9fffc502bc30cd9485ac38deba112
Adam Gibson <agibson@mcw.edu>
2024-03-11 11:21:43 -0500

    SCRUM-3794 update filter dropdown and header name

--- src/main/cliapp/src/containers/diseaseAnnotationsPage/DiseaseAnnotationsTable.js
+++ src/main/cliapp/src/containers/diseaseAnnotationsPage/DiseaseAnnotationsTable.js
@@ -923,7 +923,7 @@ export const DiseaseAnnotationsTable = () => {
 	},
 	{
 		field: "negated",
-		header: "Negated",
+		header: "Not",
 		body: (rowData) => <NotTemplate value={rowData.negated}/>,
 		sortable: true,
 		filterConfig: FILTER_CONFIGS.negatedFilterConfig,
