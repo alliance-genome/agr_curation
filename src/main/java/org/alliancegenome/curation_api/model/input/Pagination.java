219c21efc6a402614ea9a43d437d71acf22b45df
Olin Blodgett <nilo@nilosplace.net>
2024-03-19 10:28:36 -0400

    Added inital sample code for graphql

--- src/main/java/org/alliancegenome/curation_api/model/input/Pagination.java
+++ src/main/java/org/alliancegenome/curation_api/model/input/Pagination.java
@@ -1,16 +1,13 @@
 package org.alliancegenome.curation_api.model.input;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Pagination {
 
 	private Integer page = 0; // This has to be 0 when quering the database and 1 for querying ES
 	private Integer limit = 20;
-
-	public Pagination(Integer page, Integer limit) {
-		this.page = page;
-		this.limit = limit;
-	}
 
 }
