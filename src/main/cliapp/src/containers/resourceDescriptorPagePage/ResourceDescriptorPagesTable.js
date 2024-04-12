a2de44e7b0e4b65d688e997970aa5bb22b2b91b0
Adam Gibson <agibson@mcw.edu>
2024-04-12 10:42:58 -0500

    SCRUM-3803 update ResourceDescriptorPagesTable

--- src/main/cliapp/src/containers/resourceDescriptorPagePage/ResourceDescriptorPagesTable.js
+++ src/main/cliapp/src/containers/resourceDescriptorPagePage/ResourceDescriptorPagesTable.js
@@ -5,10 +5,18 @@ import { Tooltip } from 'primereact/tooltip';
 import { Toast } from 'primereact/toast';
 import { getDefaultTableState } from '../../service/TableStateService';
 import { FILTER_CONFIGS } from '../../constants/FilterFields';
+import { useGetTableData } from '../../service/useGetTableData';
+import { useGetUserSettings } from '../../service/useGetUserSettings';
+import { SearchService } from '../../service/SearchService';
 
 export const ResourceDescriptorPagesTable = () => {
 	const [isInEditMode, setIsInEditMode] = useState(false);
 	const [errorMessages, setErrorMessages] = useState({});
+	const [totalRecords, setTotalRecords] = useState(0);
+
+	const [resourceDescriptorPages, setResourceDescriptorPages] = useState();
+
+	const searchService = new SearchService();
 
 	const toast_topleft = useRef(null);
 	const toast_topright = useRef(null);
@@ -53,57 +61,71 @@ export const ResourceDescriptorPagesTable = () => {
 		{ 
 			field: "resourceDescriptor.prefix", 
 			header: "Resource Descriptor", 
+			sortable: true,
 			body: resourceDescriptorBodyTemplate,
 			filterConfig: FILTER_CONFIGS.resourceDescriptorFilterConfig
 		},
 		{ 
 			field: "name", 
 			header: "Name", 
+			sortable: true,
 			body: nameBodyTemplate,
 			filterConfig: FILTER_CONFIGS.nameFilterConfig
 		},
 		{ 
 			field: "urlTemplate", 
 			header: "URL Template", 
+			sortable: true,
 			body: urlTemplateBodyTemplate,
 			filterConfig: FILTER_CONFIGS.urlTemplateFilterConfig
 		},
 		{ 
 			field: "pageDescription", 
 			header: "Page Description", 
+			sortable: true,
 			body: pageDescriptionBodyTemplate,
 			filterConfig: FILTER_CONFIGS.pageDescriptionFilterConfig
 		}
 	]
 
-	const defaultColumnNames = columns.map((col) => {
-		return col.header;
+	const DEFAULT_COLUMN_WIDTH = 20;
+	const SEARCH_ENDPOINT = "resourcedescriptorpage";
+
+	const initialTableState = getDefaultTableState("ResourceDescriptorPages", columns, DEFAULT_COLUMN_WIDTH);
+
+	const { settings: tableState, mutate: setTableState } = useGetUserSettings(initialTableState.tableSettingsKeyName, initialTableState);
+
+	const { isLoading, isFetching } = useGetTableData({
+		tableState,
+		endpoint: SEARCH_ENDPOINT,
+		setIsInEditMode,
+		setEntities: setResourceDescriptorPages,
+		setTotalRecords,
+		toast_topleft,
+		searchService
 	});
-
-	const widthsObject = {};
-
-	columns.forEach((col) => {
-		widthsObject[col.field] = 20;
-	});
-
-	const initialTableState = getDefaultTableState("ResourceDescriptorPages", defaultColumnNames, undefined, widthsObject);
 
 	return (
 			<div className="card">
 				<Toast ref={toast_topleft} position="top-left" />
 				<Toast ref={toast_topright} position="top-right" />
 				<GenericDataTable 
-					endpoint="resourcedescriptorpage" 
+					endpoint={SEARCH_ENDPOINT} 
+					entities={resourceDescriptorPages}
+					setEntities={setResourceDescriptorPages}
+					totalRecords={totalRecords}
+					setTotalRecords={setTotalRecords}
+					tableState={tableState}
+					setTableState={setTableState}
 					tableName="Resource Descriptor Pages" 
 					columns={columns}	 
-					defaultColumnNames={defaultColumnNames}
-					initialTableState={initialTableState}
 					isEditable={false}
 					isInEditMode={isInEditMode}
 					setIsInEditMode={setIsInEditMode}
 					toasts={{toast_topleft, toast_topright }}
 					errorObject = {{errorMessages, setErrorMessages}}
-					widthsObject={widthsObject}
+					defaultColumnWidth={DEFAULT_COLUMN_WIDTH}
+					fetching={isFetching || isLoading}
 				/>
 			</div>
 	)
