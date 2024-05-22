4de00344a0170aa6b683cac3390bdf44cef16e84
VarunReddy1111 <varunreddy.go@gmail.com>
2024-05-22 13:19:37 -0500

    removed Jbosslog

--- src/main/java/org/alliancegenome/curation_api/jobs/executors/ParalogyExecutor.java
+++ src/main/java/org/alliancegenome/curation_api/jobs/executors/ParalogyExecutor.java
@@ -1,11 +1,10 @@
-package org.alliancegenome.curation_api.jobs.executors.associations;
+package org.alliancegenome.curation_api.jobs.executors;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.extern.jbosslog.JBossLog;
-import org.alliancegenome.curation_api.jobs.executors.LoadFileExecutor;
+
 import org.alliancegenome.curation_api.model.entities.bulkloads.BulkLoadFile;
 
-@JBossLog
 @ApplicationScoped
 public class ParalogyExecutor extends LoadFileExecutor {
 	public void execLoad(BulkLoadFile bulkLoadFile) {
