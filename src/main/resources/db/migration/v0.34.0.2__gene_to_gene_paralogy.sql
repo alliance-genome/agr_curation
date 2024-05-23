9d8454bd2bd2afef88f6b51b74df6e652419d49e
VarunReddy1111 <varunreddy.go@gmail.com>
2024-05-23 09:09:35 -0500

    Changes in migration file

--- src/main/resources/db/migration/v0.34.0.2__gene_to_gene_paralogy.sql
+++ src/main/resources/db/migration/v0.34.0.2__gene_to_gene_paralogy.sql
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-CREATE SEQUENCE public.genetogeneparalogy_seq         START WITH 200000000 INCREMENT BY 50 NO MINVALUE NO MAXVALUE CACHE 1;
-=======
->>>>>>> fc1316352 (Added migration file and created GeneToGeneParalogy class)
+CREATE SEQUENCE public.genetogeneparalogy_seq         START WITH 1 INCREMENT BY 50 NO MINVALUE NO MAXVALUE CACHE 1;
 CREATE TABLE genetogeneparalogy (
  	id bigint CONSTRAINT genetogeneparalogy_pkey PRIMARY KEY,
  	subjectgene_id bigint,
