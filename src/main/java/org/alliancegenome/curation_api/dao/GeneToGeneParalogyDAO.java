704a44b0fafdab47e503e4914c5d92b67fc81f8b
VarunReddy1111 <varunreddy.go@gmail.com>
2024-05-23 16:52:04 -0500

    Indentation

--- src/main/java/org/alliancegenome/curation_api/dao/GeneToGeneParalogyDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/GeneToGeneParalogyDAO.java
@@ -1,6 +1,5 @@
 package org.alliancegenome.curation_api.dao;
 
-
 import org.alliancegenome.curation_api.dao.base.BaseSQLDAO;
 import org.alliancegenome.curation_api.model.entities.GeneToGeneParalogy;
 
@@ -9,8 +8,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class GeneToGeneParalogyDAO extends BaseSQLDAO<GeneToGeneParalogy> {
 
-    protected GeneToGeneParalogyDAO() {
-        super(GeneToGeneParalogy.class);
-    }
-    
+	protected GeneToGeneParalogyDAO() {
+		super(GeneToGeneParalogy.class);
+	}
+
 }
--- src/main/java/org/alliancegenome/curation_api/services/GeneToGeneParalogyService.java
+++ src/main/java/org/alliancegenome/curation_api/services/GeneToGeneParalogyService.java
@@ -14,8 +14,7 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
 @RequestScoped
-public class GeneToGeneParalogyService extends BaseEntityCrudService<GeneToGeneParalogy, GeneToGeneParalogyDAO>
-		implements BaseUpsertServiceInterface<GeneToGeneParalogy, ParalogyFmsDTO> {
+public class GeneToGeneParalogyService extends BaseEntityCrudService<GeneToGeneParalogy, GeneToGeneParalogyDAO> implements BaseUpsertServiceInterface<GeneToGeneParalogy, ParalogyFmsDTO> {
 
 	@Inject
 	GeneToGeneParalogyDAO geneToGeneParalogyDAO;
