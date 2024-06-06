ed140b33b80821515cc8017cc098c9b5eb227c8e
markquintontulloch <markquintontulloch@gmail.com>
2024-06-06 14:12:28 +0100

    Fix orthology cleanup

--- src/main/java/org/alliancegenome/curation_api/jobs/executors/OrthologyExecutor.java
+++ src/main/java/org/alliancegenome/curation_api/jobs/executors/OrthologyExecutor.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
 
+import org.alliancegenome.curation_api.dao.orthology.GeneToGeneOrthologyGeneratedDAO;
+import org.alliancegenome.curation_api.enums.BackendBulkDataProvider;
 import org.alliancegenome.curation_api.exceptions.ObjectUpdateException;
 import org.alliancegenome.curation_api.exceptions.ObjectUpdateException.ObjectUpdateExceptionData;
 import org.alliancegenome.curation_api.interfaces.AGRCurationSchemaVersion;
@@ -21,7 +23,6 @@ import org.alliancegenome.curation_api.services.orthology.GeneToGeneOrthologyGen
 import org.alliancegenome.curation_api.util.ProcessDisplayHelper;
 import org.apache.commons.collections.ListUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -33,6 +34,7 @@ import lombok.extern.jbosslog.JBossLog;
 public class OrthologyExecutor extends LoadFileExecutor {
 
 	@Inject GeneToGeneOrthologyGeneratedService generatedOrthologyService;
+	@Inject GeneToGeneOrthologyGeneratedDAO generatedOrthologyDAO;
 
 	public void execLoad(BulkLoadFile bulkLoadFile) {
 		try {
@@ -47,17 +49,17 @@ public class OrthologyExecutor extends LoadFileExecutor {
 				bulkLoadFile.setAllianceMemberReleaseVersion(orthologyData.getMetaData().getRelease());
 			}
 
-			List<Pair<String, String>> orthoPairsLoaded = new ArrayList<>();
-			String dataProviderAbbreviation = fms.getFmsDataSubType();
-			List<Object[]> orthoPairsBefore = generatedOrthologyService.getAllOrthologyPairsBySubjectGeneDataProvider(dataProviderAbbreviation);
-			log.debug("runLoad: Before: total " + orthoPairsBefore.size());
+			List<Long> orthoPairsLoaded = new ArrayList<>();
+			BackendBulkDataProvider dataProvider = BackendBulkDataProvider.valueOf(fms.getFmsDataSubType());
+			List<Long> orthoPairIdsBefore = generatedOrthologyService.getAllOrthologyPairIdsBySubjectGeneDataProvider(dataProvider);
+			log.debug("runLoad: Before: total " + orthoPairIdsBefore.size());
 
 			bulkLoadFileDAO.merge(bulkLoadFile);
 
 			BulkLoadFileHistory history = new BulkLoadFileHistory(orthologyData.getData().size());
 			createHistory(history, bulkLoadFile);
 			runLoad(history, fms.getFmsDataSubType(), orthologyData, orthoPairsLoaded);
-			runCleanup(history, fms.getFmsDataSubType(), orthoPairsBefore, orthoPairsLoaded);
+			runCleanup(history, fms.getFmsDataSubType(), orthoPairIdsBefore, orthoPairsLoaded);
 			history.finishLoad();
 			finalSaveHistory(history);
 		} catch (Exception e) {
@@ -66,26 +68,22 @@ public class OrthologyExecutor extends LoadFileExecutor {
 		}
 	}
 
-	private void runCleanup(BulkLoadFileHistory history, String dataProvider, List<Object[]> orthoPairsBefore, List<Pair<String, String>> orthoPairsAfter) {
-		Log.debug("runLoad: After: " + dataProvider + " " + orthoPairsAfter.size());
+	private void runCleanup(BulkLoadFileHistory history, String dataProvider, List<Long> orthoPairIdsBefore, List<Long> orthoPairIdsAfter) {
+		Log.debug("runLoad: After: " + dataProvider + " " + orthoPairIdsAfter.size());
 
-		List<Pair<String, String>> transformedPairsBefore = new ArrayList<>();
-		for (Object[] pair : orthoPairsBefore) {
-			transformedPairsBefore.add(Pair.of((String) pair[0], (String) pair[1]));
-		}
-		List<Pair<String, String>> distinctAfter = orthoPairsAfter.stream().distinct().collect(Collectors.toList());
+		List<Long> distinctAfter = orthoPairIdsAfter.stream().distinct().collect(Collectors.toList());
 		Log.debug("runLoad: Distinct: " + dataProvider + " " + distinctAfter.size());
 
-		List<Pair<String, String>> pairsToRemove = ListUtils.subtract(transformedPairsBefore, distinctAfter);
-		Log.debug("runLoad: Remove: " + dataProvider + " " + pairsToRemove.size());
+		List<Long> idsToRemove = ListUtils.subtract(orthoPairIdsBefore, distinctAfter);
+		Log.debug("runLoad: Remove: " + dataProvider + " " + idsToRemove.size());
 
-		history.setTotalDeleteRecords((long) pairsToRemove.size());
+		history.setTotalDeleteRecords((long) idsToRemove.size());
 
 		ProcessDisplayHelper ph = new ProcessDisplayHelper();
-		ph.startProcess("Deletion/deprecation of orthology pairs " + dataProvider, pairsToRemove.size());
-		for (Pair<String, String> pairToRemove : pairsToRemove) {
+		ph.startProcess("Deletion/deprecation of orthology pairs " + dataProvider, idsToRemove.size());
+		for (Long idToRemove : idsToRemove) {
 			try {
-				generatedOrthologyService.removeNonUpdated(pairToRemove);
+				generatedOrthologyDAO.remove(idToRemove);
 				history.incrementDeleted();
 			} catch (Exception e) {
 				history.incrementDeleteFailed();
@@ -96,7 +94,7 @@ public class OrthologyExecutor extends LoadFileExecutor {
 		ph.finishProcess();
 	}
 
-	private void runLoad(BulkLoadFileHistory history, String dataProvider, OrthologyIngestFmsDTO orthologyData, List<Pair<String, String>> orthoPairsAdded) {
+	private void runLoad(BulkLoadFileHistory history, String dataProvider, OrthologyIngestFmsDTO orthologyData, List<Long> orthoPairIdsAdded) {
 		ProcessDisplayHelper ph = new ProcessDisplayHelper();
 		ph.addDisplayHandler(loadProcessDisplayService);
 		ph.startProcess(dataProvider + " Orthology DTO Update", orthologyData.getData().size());
@@ -105,8 +103,8 @@ public class OrthologyExecutor extends LoadFileExecutor {
 			try {
 				GeneToGeneOrthologyGenerated orthoPair = generatedOrthologyService.upsert(orthoPairDTO);
 				history.incrementCompleted();
-				if (orthoPairsAdded != null) {
-					orthoPairsAdded.add(Pair.of(orthoPair.getSubjectGene().getCurie(), orthoPair.getObjectGene().getCurie()));
+				if (orthoPairIdsAdded != null) {
+					orthoPairIdsAdded.add(orthoPair.getId());
 				}
 			} catch (ObjectUpdateException e) {
 				history.incrementFailed();
@@ -124,10 +122,10 @@ public class OrthologyExecutor extends LoadFileExecutor {
 
 	// Gets called from the API directly
 	public APIResponse runLoad(String dataProvider, OrthologyIngestFmsDTO dto) {
-		List<Pair<String, String>> orthoPairsAdded = new ArrayList<>();
+		List<Long> orthoPairIdsAdded = new ArrayList<>();
 
 		BulkLoadFileHistory history = new BulkLoadFileHistory(dto.getData().size());
-		runLoad(history, dataProvider, dto, orthoPairsAdded);
+		runLoad(history, dataProvider, dto, orthoPairIdsAdded);
 		history.finishLoad();
 
 		return new LoadHistoryResponce(history);
