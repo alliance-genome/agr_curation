1760fc4c77035940ab2ef032b5645b12ab06d8fb
Andres Becerra <abecerra@ebi.ac.uk>
2024-06-19 15:44:28 +0100

    SCRUM-3708: correct removal

--- src/main/java/org/alliancegenome/curation_api/interfaces/crud/GeneExpressionAnnotationCrudInterface.java
+++ src/main/java/org/alliancegenome/curation_api/interfaces/crud/GeneExpressionAnnotationCrudInterface.java
@@ -7,12 +7,10 @@ import org.alliancegenome.curation_api.interfaces.base.BaseIdCrudInterface;
 import org.alliancegenome.curation_api.model.entities.GeneExpressionAnnotation;
 import org.alliancegenome.curation_api.model.ingest.dto.fms.GeneExpressionFmsDTO;
 import org.alliancegenome.curation_api.response.APIResponse;
-import org.alliancegenome.curation_api.response.SearchResponse;
+import org.alliancegenome.curation_api.response.ObjectResponse;
 import org.alliancegenome.curation_api.view.View;
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-import java.util.HashMap;
 import java.util.List;
 
 @Path("/gene-expression-annotation")
@@ -21,17 +19,13 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 public interface GeneExpressionAnnotationCrudInterface extends BaseIdCrudInterface<GeneExpressionAnnotation> {
 
-	@Override
-	@POST
-	@Path("/search")
+	@GET
+	@Path("/findBy/{identifier}")
 	@JsonView(View.FieldsAndLists.class)
-	@Tag(name = "Elastic Search Gene Expression Annotations")
-	SearchResponse<GeneExpressionAnnotation> search(@DefaultValue("0") @QueryParam("page") Integer page, @DefaultValue("10") @QueryParam("limit") Integer limit, @RequestBody HashMap<String, Object> params);
+	ObjectResponse<GeneExpressionAnnotation> getByIdentifier(@PathParam("identifier") String identifier);
 
 	@POST
 	@Path("/bulk/{dataProvider}/annotationFile")
 	@JsonView(View.FieldsAndLists.class)
 	APIResponse updateExpressionAnnotations(@PathParam("dataProvider") String dataProvider, List<GeneExpressionFmsDTO> annotationData);
-
-
 }
