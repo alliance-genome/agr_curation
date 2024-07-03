dcd8a00b2f5559f1eb8378f0b187148b3e38d947
markquintontulloch <markquintontulloch@gmail.com>
2024-07-03 16:51:26 +0100

    3rd attempt at checkstyle fixes

--- src/main/java/org/alliancegenome/curation_api/constants/Gff3Constants.java
+++ src/main/java/org/alliancegenome/curation_api/constants/Gff3Constants.java
@@ -2,11 +2,11 @@ package org.alliancegenome.curation_api.constants;
 
 import java.util.List;
 
-public final class Gff3Constants {
+public class Gff3Constants {
 	private Gff3Constants() {
 		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
 	}
-	public static final List<String> TRANSCRIPT_TYPES = List.of(
+	public static List<String> TRANSCRIPT_TYPES = List.of(
 		"mRNA", "ncRNA", "piRNA", "lincRNA", "miRNA", "pre_miRNA", "snoRNA", "lncRNA",
 		"tRNA", "snRNA", "rRNA", "antisense_RNA", "C_gene_segment", "V_gene_segment",
 		"pseudogene_attribute", "snoRNA_gene", "pseudogenic_transcript", "lnc_RNA"
