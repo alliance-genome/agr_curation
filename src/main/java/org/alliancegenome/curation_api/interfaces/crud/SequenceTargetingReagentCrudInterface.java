af9a2bf00b9b3d73971a087f8a818efcb616c18f
Adam Gibson <agibson@mcw.edu>
2024-07-11 09:01:21 -0500

    SCRUM-4187 update SQTR endpoints to exclude gene associations

--- src/main/java/org/alliancegenome/curation_api/interfaces/crud/SequenceTargetingReagentCrudInterface.java
+++ src/main/java/org/alliancegenome/curation_api/interfaces/crud/SequenceTargetingReagentCrudInterface.java
@@ -1,19 +1,26 @@
 package org.alliancegenome.curation_api.interfaces.crud;
 
+import java.util.HashMap;
+
 import org.alliancegenome.curation_api.interfaces.base.BaseIdCrudInterface;
+import org.alliancegenome.curation_api.model.entities.Allele;
 import org.alliancegenome.curation_api.model.entities.SequenceTargetingReagent;
 import org.alliancegenome.curation_api.model.ingest.dto.fms.SequenceTargetingReagentIngestFmsDTO;
 import org.alliancegenome.curation_api.response.APIResponse;
+import org.alliancegenome.curation_api.response.SearchResponse;
 import org.alliancegenome.curation_api.view.View;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/sqtr")
@@ -26,4 +33,18 @@ public interface SequenceTargetingReagentCrudInterface extends BaseIdCrudInterfa
 	@Path("/bulk/{dataProvider}/sqtrfile")
 	@JsonView(View.FieldsAndLists.class)
 	APIResponse updateSequenceTargetingReagent(@PathParam("dataProvider") String dataProvider, SequenceTargetingReagentIngestFmsDTO sqtrData);
+
+	@Override
+	@POST
+	@Path("/find")
+	@Tag(name = "Relational Database Browsing Endpoints")
+	@JsonView(View.SequenceTargetingReagentView.class)
+	SearchResponse<SequenceTargetingReagent> find(@DefaultValue("0") @QueryParam("page") Integer page, @DefaultValue("10") @QueryParam("limit") Integer limit, @RequestBody HashMap<String, Object> params);
+
+	@Override
+	@POST
+	@Path("/search")
+	@Tag(name = "Elastic Search Browsing Endpoints")
+	@JsonView({ View.SequenceTargetingReagentView.class })
+	SearchResponse<SequenceTargetingReagent> search(@DefaultValue("0") @QueryParam("page") Integer page, @DefaultValue("10") @QueryParam("limit") Integer limit, @RequestBody HashMap<String, Object> params);
 }
