65c0a806f199ab95cb230c624467d5ff57a8a709
Adam Gibson <agibson@mcw.edu>
2024-08-12 11:18:11 -0500

    SCRUM-4300 add cellularComponentRibbonTerm and cellularComponentOther to AnatomicalSite

--- src/main/java/org/alliancegenome/curation_api/services/ontology/GoTermService.java
+++ src/main/java/org/alliancegenome/curation_api/services/ontology/GoTermService.java
@@ -1,5 +1,6 @@
 package org.alliancegenome.curation_api.services.ontology;
 
+
 import org.alliancegenome.curation_api.dao.ontology.GoTermDAO;
 import org.alliancegenome.curation_api.model.entities.ontology.GOTerm;
 import org.alliancegenome.curation_api.services.base.BaseOntologyTermService;
