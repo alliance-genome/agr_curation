15f2ec575e8de5013efe6c9b0d13c0d56f70c048
VarunReddy1111 <varunreddy.go@gmail.com>
2024-09-17 08:49:21 -0400

    Checkstyle fix

--- src/main/java/org/alliancegenome/curation_api/dao/base/BaseSQLDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/base/BaseSQLDAO.java
@@ -480,7 +480,7 @@ public class BaseSQLDAO<E extends AuditedObject> extends BaseEntityDAO<E> {
 										BooleanPredicateClausesStep<?> clause = p.bool();
 										if (useKeywordFields != null && useKeywordFields) {
 											clause.should(p.match().field(field + "_keyword").matching(searchFilters.get(filterName).get(field).get("queryString").toString()).boost(boost + 500));
-										} else { 
+										} else {
 											clause.should(p.match().field(field).matching(searchFilters.get(filterName).get(field).get("queryString").toString()).boost(boost));
 										}
 										q.should(clause);
