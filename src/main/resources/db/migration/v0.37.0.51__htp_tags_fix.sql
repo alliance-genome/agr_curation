64ed6d81ef24762559e0af098470d9a27836a7b5
VarunReddy1111 <varunreddy.go@gmail.com>
2024-09-25 16:30:03 -0500

    minor changes

--- src/main/resources/db/migration/v0.37.0.51__htp_tags_fix.sql
+++ src/main/resources/db/migration/v0.37.0.51__htp_tags_fix.sql
@@ -5,7 +5,6 @@ DELETE FROM vocabularyterm WHERE name = 'strain study';
 
 DELETE FROM vocabularyterm_synonyms WHERE vocabularyterm_id IN (SELECT id FROM vocabularyterm WHERE name = 'anatomical structure');
 DELETE FROM vocabularyterm_synonyms WHERE vocabularyterm_id IN (SELECT id FROM vocabularyterm WHERE name = 'chemical stimulus');
-DELETE FROM vocabularyterm_synonyms WHERE vocabularyterm_id IN (SELECT id FROM vocabularyterm WHERE name = 'strain study');
 
 INSERT INTO vocabularyterm_synonyms (vocabularyterm_id, synonyms) SELECT id, 'tissue type study' FROM vocabularyterm WHERE name = 'anatomical structure';
 INSERT INTO vocabularyterm_synonyms (vocabularyterm_id, synonyms) SELECT id, 'tissue specific' FROM vocabularyterm WHERE name = 'anatomical structure';
