9745d026ba6f195d97c7a137d9353e1f3d109340
Adam Gibson <agibson@mcw.edu>
2024-09-27 09:09:24 -0500

    SCRUM-3767 update VocabulariesTable templates

--- src/main/cliapp/src/containers/vocabularyPage/VocabulariesTable.js
+++ src/main/cliapp/src/containers/vocabularyPage/VocabulariesTable.js
@@ -2,14 +2,11 @@ import React, { useRef, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
-import { BooleanTemplate } from '../../components/Templates/BooleanTemplate';
 import { VocabularyService } from '../../service/VocabularyService';
 import { useControlledVocabularyService } from '../../service/useControlledVocabularyService';
 import { GenericDataTable } from '../../components/GenericDataTable/GenericDataTable';
 import { NewVocabularyForm } from '../../containers/controlledVocabularyPage/NewVocabularyForm';
-import { EllipsisTableCell } from '../../components/EllipsisTableCell';
 import { ErrorMessageComponent } from '../../components/Error/ErrorMessageComponent';
-import { Tooltip } from 'primereact/tooltip';
 import { getDefaultTableState } from '../../service/TableStateService';
 import { FILTER_CONFIGS } from '../../constants/FilterFields';
 import { InputTextEditor } from '../../components/InputTextEditor';
@@ -18,6 +15,10 @@ import { useGetTableData } from '../../service/useGetTableData';
 import { useGetUserSettings } from '../../service/useGetUserSettings';
 import { SearchService } from '../../service/SearchService';
 import { setNewEntity } from '../../utils/utils';
+
+import { StringTemplate } from '../../components/Templates/StringTemplate';
+import { BooleanTemplate } from '../../components/Templates/BooleanTemplate';
+
 
 export const VocabulariesTable = () => {
 	const [isInEditMode, setIsInEditMode] = useState(false);
@@ -44,19 +45,6 @@ export const VocabulariesTable = () => {
 		}
 		return vocabularyService.saveVocabulary(updatedVocabulary);
 	});
-
-	const stringBodyTemplate = (rowData, field) => {
-		return (
-			<>
-				<EllipsisTableCell otherClasses={`${field}${rowData.id}`}>{rowData[field]}</EllipsisTableCell>
-				<Tooltip
-					target={`.${field}${rowData.id}`}
-					content={rowData[field]}
-					style={{ width: '450px', maxWidth: '450px' }}
-				/>
-			</>
-		);
-	};
 
 	const stringEditor = (props, field) => {
 		return (
@@ -93,7 +81,7 @@ export const VocabulariesTable = () => {
 			field: 'name',
 			header: 'Name',
 			sortable: true,
-			body: (rowData) => stringBodyTemplate(rowData, 'name'),
+			body: (rowData) => <StringTemplate string={rowData.name}/>,
 			filterConfig: FILTER_CONFIGS.nameFilterConfig,
 			editor: (props) => stringEditor(props, 'name'),
 		},
@@ -101,7 +89,7 @@ export const VocabulariesTable = () => {
 			field: 'vocabularyDescription',
 			header: 'Description',
 			sortable: true,
-			body: (rowData) => stringBodyTemplate(rowData, 'vocabularyDescription'),
+			body: (rowData) => <StringTemplate string={rowData.vocabularyDescription}/>,
 			filterConfig: FILTER_CONFIGS.vocabularyDescriptionFilterConfig,
 			editor: (props) => stringEditor(props, 'vocabularyDescription'),
 		},
@@ -117,8 +105,8 @@ export const VocabulariesTable = () => {
 			field: 'vocabularyLabel',
 			header: 'Label',
 			sortable: true,
+			body: (rowData) => <StringTemplate string={rowData.vocabularyLabel}/>,
 			filterConfig: FILTER_CONFIGS.vocabularyLabelFilterConfig,
-			body: (rowData) => stringBodyTemplate(rowData, 'vocabularyLabel'),
 		},
 	];
 
