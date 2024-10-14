42368f73cd15475b6b9fddf0257aa04bfe54a13c
VarunReddy1111 <varunreddy.go@gmail.com>
2024-10-14 16:47:52 -0500

    Adding new indexes for htpdatasample

--- src/main/java/org/alliancegenome/curation_api/model/entities/BioSampleGenomicInformation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/BioSampleGenomicInformation.java
@@ -29,6 +29,11 @@ import lombok.ToString;
 @Schema(name = "BioSampleGenomicInformation", description = "POJO that represents the BioSampleGenomicInformation")
 @AGRCurationSchemaVersion(min = "2.7.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = { AuditedObject.class })
 @Indexed
+@Table(indexes = {
+	@Index(name = "biosamplegenomicinfo_bioSampleAllele", columnList = "bioSampleAllele_id"),
+	@Index(name = "biosamplegenomicinfo_bioSampleAgm", columnList = "bioSampleAgm_id"),
+	@Index(name = "biosamplegenomicinfo_bioSampleAgmType", columnList = "bioSampleAgmType_id")
+})
 public class BioSampleGenomicInformation extends AuditedObject {
 
 	@IndexedEmbedded(includePaths = {
