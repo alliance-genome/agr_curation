e0b316bd8080d969ed189143187a7d98fe2aec74
Olin Blodgett <nilo@nilosplace.net>
2024-10-15 13:55:24 -0400

    Added transactional

--- src/main/java/org/alliancegenome/curation_api/dao/loads/BulkLoadFileExceptionDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/loads/BulkLoadFileExceptionDAO.java
@@ -6,6 +6,7 @@ import org.alliancegenome.curation_api.model.entities.bulkloads.BulkLoadFileExce
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 
 @ApplicationScoped
 public class BulkLoadFileExceptionDAO extends BaseSQLDAO<BulkLoadFileException> {
@@ -13,6 +14,7 @@ public class BulkLoadFileExceptionDAO extends BaseSQLDAO<BulkLoadFileException> 
 		super(BulkLoadFileException.class);
 	}
 
+	@Transactional
 	public void cleanUpTwoWeekOldExceptions() {
 		Log.info("Deleting Old Bulk Exceptions: \"DELETE FROM BulkLoadFileException WHERE dbdatecreated < NOW() - INTERVAL '14 days'\"");
 		Query jpqlQuery = entityManager.createNativeQuery("DELETE FROM BulkLoadFileException WHERE dbdatecreated < NOW() - INTERVAL '14 days'");
