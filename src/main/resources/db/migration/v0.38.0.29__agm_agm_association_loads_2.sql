0f218ad837134b465bf3f1b0b5ca4e679fb0ac0c
Christian Pich <cmpich@zfin.org>
2025-01-23 14:26:46 +0100

    SCRUM-4667 removing individual bulkload tables entries

--- src/main/resources/db/migration/v0.38.0.29__agm_agm_association_loads_2.sql
+++ src/main/resources/db/migration/v0.38.0.29__agm_agm_association_loads_2.sql
@@ -6,3 +6,28 @@ ALTER TABLE agmagmassociation
 alter table agmagmassociation
     DROP agmassociationobject_id
 ;
+
+delete
+from bulkscheduledload
+where id in (SELECT id
+             FROM bulkload
+             WHERE backendbulkloadtype = 'AGM_AGM_ASSOCIATION');
+delete
+from bulkload
+WHERE group_id in (select id from bulkloadgroup where name = 'Direct (LinkML) AGM AGM Association Loads');
+delete
+from bulkloadgroup
+where name = 'Direct (LinkML) AGM AGM Association Loads';
+
+delete
+from bulkmanualload
+where id in (SELECT id
+             FROM bulkload
+             WHERE backendbulkloadtype = 'AGM_AGM_ASSOCIATION');
+
+SELECT id, 'ZFIN'
+FROM bulkload
+WHERE name = 'ZFIN AGM AGM Association Load';
+
+select *
+from bulkmanualload
