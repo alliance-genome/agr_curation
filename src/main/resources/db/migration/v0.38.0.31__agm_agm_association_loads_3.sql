d5c72476318aaa8012d1a284ce0fe9bb5c63e042
Christian Pich <cmpich@zfin.org>
2025-02-03 18:01:45 +0100

    fix typo

--- src/main/resources/db/migration/v0.38.0.31__agm_agm_association_loads_3.sql
+++ src/main/resources/db/migration/v0.38.0.31__agm_agm_association_loads_3.sql
@@ -1,8 +1,8 @@
 --alter TABLE agmagmassociation ADD COLUMN agmAgmAssociationObject_id bigint;
 --alter TABLE agmagmassociation drop agmAssociationObject_id;
 
-CREATE INDEX agmagmassociation_agmassociationsubject_index ON public.agmagmassociation USING btree (agmassociationsubject_id);
-CREATE INDEX agmagmassociation_agmagmassocicationobject_index ON public.agmagmassociation USING btree (agmAgmAssociationObject_id);
+CREATE INDEX AgmAgmAssociation_agmAssociationSubject_index ON public.agmagmassociation USING btree (agmassociationsubject_id);
+CREATE INDEX AgmAgmAssociation_AgmAgmAssociationObject_index ON public.agmagmassociation USING btree (agmAgmAssociationObject_id);
 
 drop index agmagmassociation_agmassocobject_in;
 drop index agmagmassociation_agmassocsubject_in;
