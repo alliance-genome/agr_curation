bd2a4aca68642546353413c8d26c3dadff4103c4
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2025-03-06 11:11:55 +0000

    Another lazy loading fix

--- src/main/java/org/alliancegenome/curation_api/services/validation/dto/base/BaseDTOValidator.java
+++ src/main/java/org/alliancegenome/curation_api/services/validation/dto/base/BaseDTOValidator.java
@@ -73,6 +73,7 @@ public class BaseDTOValidator {
 				MITerm miTerm = miTermService.findByCurie(curie);
 				if (miTerm != null) {
 					miTerm.getSecondaryIdentifiers().size();
+					miTerm.getSynonyms().size();
 					miTermCache.put(curie, miTerm);
 					return miTerm;
 				}
