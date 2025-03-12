02144547a9e1a3915032f9a4079f928c9ba48123
Adam Gibson <agibson@mcw.edu>
2025-03-12 09:08:16 -0500

    make pretty

--- src/main/cliapp/src/oktaAuthConfig.js
+++ src/main/cliapp/src/oktaAuthConfig.js
@@ -21,7 +21,7 @@ const oktaSignInConfig = {
 	clientId: clientId,
 	redirectUri: window.location.origin + '/login/callback',
 	scopes: ['openid', 'profile', 'email'],
-	useClassicEngine:true,
+	useClassicEngine: true,
 	authParams: {
 		pkce: true,
 	},
