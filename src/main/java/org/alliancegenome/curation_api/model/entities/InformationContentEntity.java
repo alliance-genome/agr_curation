7be94d8dab699ad09edb35ce32ac2bb43eca6af4
Adam Gibson <agibson@mcw.edu>
2025-03-12 14:35:42 -0500

    SCRUM-4944 checkstyle

--- src/main/java/org/alliancegenome/curation_api/model/entities/InformationContentEntity.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/InformationContentEntity.java
@@ -25,9 +25,9 @@ import lombok.ToString;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @ToString(callSuper = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({ 
-	@JsonSubTypes.Type(value = Reference.class, name = "Reference"), 
-	@JsonSubTypes.Type(value = Reference.class, name = "LiteratureReference") 
+@JsonSubTypes({
+	@JsonSubTypes.Type(value = Reference.class, name = "Reference"),
+	@JsonSubTypes.Type(value = Reference.class, name = "LiteratureReference")
 })
 @AGRCurationSchemaVersion(min = "1.4.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = { CurieObject.class })
 @Table(
