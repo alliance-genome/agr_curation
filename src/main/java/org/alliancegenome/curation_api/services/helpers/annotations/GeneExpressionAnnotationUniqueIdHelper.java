62e22244fd9815cba2b73d89e6efea5adce0a007
Andres Becerra <abecerra@ebi.ac.uk>
2025-03-28 14:42:50 +0000

    SCRUM-4932: consolidate annotations during loads from FMS

--- src/main/java/org/alliancegenome/curation_api/services/helpers/annotations/GeneExpressionAnnotationUniqueIdHelper.java
+++ src/main/java/org/alliancegenome/curation_api/services/helpers/annotations/GeneExpressionAnnotationUniqueIdHelper.java
@@ -1,37 +1,75 @@
 package org.alliancegenome.curation_api.services.helpers.annotations;
 
 import jakarta.enterprise.context.RequestScoped;
+import org.alliancegenome.curation_api.model.ingest.dto.fms.ConsolidatedGeneExpressionFmsDTO;
 import org.alliancegenome.curation_api.model.ingest.dto.fms.GeneExpressionFmsDTO;
 import org.alliancegenome.curation_api.services.helpers.UniqueIdGeneratorHelper;
+import org.apache.commons.lang3.ObjectUtils;
 
 @RequestScoped
 public class GeneExpressionAnnotationUniqueIdHelper {
 
 	//	UniqueID =
 	//	assayId | evidenceCurie | geneId | stageTermId | stageName | whereExpressedStatement | anatomicalStructureTermId | cellularComponentTermId
-	public String generateUniqueId(GeneExpressionFmsDTO geneExpressionFmsDTO, String referenceCurie) {
+	public String generateUniqueId(ConsolidatedGeneExpressionFmsDTO consolidatedGeneExpressionFmsDTO, String referenceCurie) {
 		UniqueIdGeneratorHelper uniqueIdGeneratorHelper = new UniqueIdGeneratorHelper();
-		uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getAssay());
-		uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getGeneId());
+		uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getAssay());
+		uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getGeneId());
 		uniqueIdGeneratorHelper.add(referenceCurie);
-		if (geneExpressionFmsDTO.getWhenExpressed() != null) {
-			uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhenExpressed().getStageTermId());
-			uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhenExpressed().getStageName());
+		if (consolidatedGeneExpressionFmsDTO.getWhenExpressed() != null) {
+			uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getWhenExpressed().getStageTermId());
+			uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getWhenExpressed().getStageName());
 		}
-		if (geneExpressionFmsDTO.getWhereExpressed() != null) {
-			uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhereExpressed().getWhereExpressedStatement());
-			uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhereExpressed().getAnatomicalStructureTermId());
-			uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhereExpressed().getCellularComponentTermId());
+		if (consolidatedGeneExpressionFmsDTO.getWhereExpressed() != null) {
+			uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getWhereExpressed().getWhereExpressedStatement());
+			uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getWhereExpressed().getAnatomicalStructureTermId());
+			uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getWhereExpressed().getCellularComponentTermId());
+		}
+		return uniqueIdGeneratorHelper.getUniqueId();
+	}
+
+	// used to consolidate annotation DTOs before validating
+	public String generateHash(GeneExpressionFmsDTO geneExpressionFmsDTO) {
+		UniqueIdGeneratorHelper uniqueIdGeneratorHelper = new UniqueIdGeneratorHelper();
+		if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getAssay())) {
+			uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getAssay());
+		}
+		if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getGeneId())) {
+			uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getGeneId());
+		}
+		if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getEvidence())) {
+			if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getEvidence().getPublicationId())) {
+				uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getEvidence().getPublicationId());
+			}
+		}
+		if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getWhenExpressed())) {
+			if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getWhenExpressed().getStageTermId())) {
+				uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhenExpressed().getStageTermId());
+			}
+			if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getWhenExpressed().getStageName())) {
+				uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhenExpressed().getStageName());
+			}
+		}
+		if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getWhereExpressed())) {
+			if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getWhereExpressed().getWhereExpressedStatement())) {
+				uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhereExpressed().getWhereExpressedStatement());
+			}
+			if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getWhereExpressed().getAnatomicalStructureTermId())) {
+				uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhereExpressed().getAnatomicalStructureTermId());
+			}
+			if (ObjectUtils.isNotEmpty(geneExpressionFmsDTO.getWhereExpressed().getCellularComponentTermId())) {
+				uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getWhereExpressed().getCellularComponentTermId());
+			}
 		}
 		return uniqueIdGeneratorHelper.getUniqueId();
 	}
 
 	//	UniqueID =	geneId | evidenceReferenceCurie | assayId
-	public String generateExperimentId(GeneExpressionFmsDTO geneExpressionFmsDTO, String referenceCurie) {
+	public String generateExperimentId(ConsolidatedGeneExpressionFmsDTO consolidatedGeneExpressionFmsDTO, String referenceCurie) {
 		UniqueIdGeneratorHelper uniqueIdGeneratorHelper = new UniqueIdGeneratorHelper();
-		uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getGeneId());
+		uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getGeneId());
 		uniqueIdGeneratorHelper.add(referenceCurie);
-		uniqueIdGeneratorHelper.add(geneExpressionFmsDTO.getAssay());
+		uniqueIdGeneratorHelper.add(consolidatedGeneExpressionFmsDTO.getAssay());
 		return uniqueIdGeneratorHelper.getUniqueId();
 	}
 }
