6a016eb423270af195bedf676fb30800e5580b40
Andres Becerra <abecerra@ebi.ac.uk>
2025-04-29 15:08:07 +0100

    SCRUM-5039: delete gene expression crossrefs for mgi wb and zfin

--- src/main/resources/db/migration/v0.39.0.4__del_expression_crossrefs.sql
+++ src/main/resources/db/migration/v0.39.0.4__del_expression_crossrefs.sql
@@ -2,42 +2,40 @@
 DELETE FROM geneexpressionannotation_crossreference geac WHERE  geac.crossreferences_id IN (
 SELECT gea_cr.crossreferences_id FROM geneexpressionannotation_crossreference gea_cr, geneexpressionannotation gea
 WHERE
- gea.dataprovider_id = 20308680 AND
+ gea.dataprovider_id = (SELECT id FROM organization WHERE abbreviation = 'MGI') AND
  gea.id = gea_cr.geneexpressionannotation_id
 );
 
 DELETE FROM geneexpressionexperiment_crossreference geec WHERE  geec.crossreferences_id IN (
 SELECT gee_cr.crossreferences_id FROM geneexpressionexperiment_crossreference gee_cr, geneexpressionexperiment gee
 WHERE
- gee.dataprovider_id = 20308680 AND
+ gee.dataprovider_id = (SELECT id FROM organization WHERE abbreviation = 'MGI') AND
  gee.id = gee_cr.geneexpressionexperiment_id);
 
 -- WB
 DELETE FROM geneexpressionannotation_crossreference geac WHERE  geac.crossreferences_id IN (
 SELECT gea_cr.crossreferences_id FROM geneexpressionannotation_crossreference gea_cr, geneexpressionannotation gea
 WHERE
- gea.dataprovider_id = 20308683 AND
+ gea.dataprovider_id = (SELECT id FROM organization WHERE abbreviation = 'WB') AND
  gea.id = gea_cr.geneexpressionannotation_id
 );
 
 DELETE FROM geneexpressionexperiment_crossreference geec WHERE  geec.crossreferences_id IN (
 SELECT gee_cr.crossreferences_id FROM geneexpressionexperiment_crossreference gee_cr, geneexpressionexperiment gee
 WHERE
- gee.dataprovider_id = 20308683 AND
+ gee.dataprovider_id = (SELECT id FROM organization WHERE abbreviation = 'WB') AND
  gee.id = gee_cr.geneexpressionexperiment_id);
 
 -- ZFIN
 DELETE FROM geneexpressionannotation_crossreference geac WHERE  geac.crossreferences_id IN (
 SELECT gea_cr.crossreferences_id FROM geneexpressionannotation_crossreference gea_cr, geneexpressionannotation gea
 WHERE
- gea.dataprovider_id = 20308685 AND
+ gea.dataprovider_id = (SELECT id FROM organization WHERE abbreviation = 'ZFIN') AND
  gea.id = gea_cr.geneexpressionannotation_id
 );
 
 DELETE FROM geneexpressionexperiment_crossreference geec WHERE  geec.crossreferences_id IN (
 SELECT gee_cr.crossreferences_id FROM geneexpressionexperiment_crossreference gee_cr, geneexpressionexperiment gee
 WHERE
- gee.dataprovider_id = 20308685 AND
+ gee.dataprovider_id = (SELECT id FROM organization WHERE abbreviation = 'ZFIN') AND
  gee.id = gee_cr.geneexpressionexperiment_id);
-
-
