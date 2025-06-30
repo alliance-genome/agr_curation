3354d148fbbebe72612c4d13cfac6cad38008ede
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2025-06-30 11:29:54 +0100

    More constants

--- src/main/java/org/alliancegenome/curation_api/dao/AffectedGenomicModelDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/AffectedGenomicModelDAO.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.alliancegenome.curation_api.constants.EntityFieldConstants;
 import org.alliancegenome.curation_api.dao.base.BaseSQLDAO;
 import org.alliancegenome.curation_api.model.entities.AffectedGenomicModel;
 import org.apache.commons.collections.CollectionUtils;
@@ -24,21 +25,21 @@ public class AffectedGenomicModelDAO extends BaseSQLDAO<AffectedGenomicModel> {
 	
 	public Boolean hasReferencingDiseaseAnnotations(Long agmId) {
 		Map<String, Object> params = new HashMap<>();
-		params.put("diseaseAnnotationSubject.id", agmId);
+		params.put(EntityFieldConstants.DA_SUBJECT + ".id", agmId);
 		List<Long> results = agmDiseaseAnnotationDAO.findIdsByParams(params);
 		if (CollectionUtils.isNotEmpty(results)) {
 			return true;
 		}
 		
 		Map<String, Object> dgmParams = new HashMap<>();
-		dgmParams.put("diseaseGeneticModifierAgms.id", agmId);
+		dgmParams.put(EntityFieldConstants.DA_MODIFIER_AGMS + ".id", agmId);
 		results = diseaseAnnotationDAO.findIdsByParams(dgmParams);
 		return CollectionUtils.isNotEmpty(results);
 	}
 	
 	public Boolean hasReferencingPhenotypeAnnotations(Long agmId) {
 		Map<String, Object> params = new HashMap<>();
-		params.put("phenotypeAnnotationSubject.id", agmId);
+		params.put(EntityFieldConstants.PA_SUBJECT + ".id", agmId);
 		List<Long> results = agmPhenotypeAnnotationDAO.findIdsByParams(params);
 		return CollectionUtils.isNotEmpty(results);
 	}
