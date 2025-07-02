fb451c6a800763af76038ec4ace8b791f15adf0d
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2025-07-02 17:04:59 +0100

    Fix table name

--- src/main/resources/db/migration/v0.41.0.1__remove_dataprovider_xref_from_pa.sql
+++ src/main/resources/db/migration/v0.41.0.1__remove_dataprovider_xref_from_pa.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tmp_dpxref_id AS SELECT DISTINCT id FROM crossreference WHERE referencedcurie IN ('WB','FB','SGD','RGD','MGI','ZFIN','XB','XBXL','XBXT','OMIM');
 
 UPDATE phenotypeannotation SET dataprovidercrossreference_id = NULL;
-UPDATE htpdatasetexpressionannotation SET dataprovidercrossreference_id = NULL;
+UPDATE htpexpressiondatasetannotation SET dataprovidercrossreference_id = NULL;
 
 UPDATE biologicalentity SET dataprovidercrossreference_id = NULL WHERE dataprovidercrossreference_id IN (SELECT id FROM tmp_dpxref_id);
 
