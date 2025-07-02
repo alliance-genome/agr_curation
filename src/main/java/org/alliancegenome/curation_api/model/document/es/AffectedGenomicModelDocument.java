4da1bccc2f66cc0fabdc1b1146ca9ed282cbbf06
Christian Pich <cmpich@zfin.org>
2025-07-02 22:19:06 +0200

    SCRUM-4073 case insensitive sort of phenotypes (#2122)

--- src/main/java/org/alliancegenome/curation_api/model/document/es/AffectedGenomicModelDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/AffectedGenomicModelDocument.java
@@ -36,7 +36,6 @@ public class AffectedGenomicModelDocument extends ESDocument {
 	private List<ConditionRelation> conditionRelations;
 	private List<ConditionRelation> conditionModifiers;
 
-
 	public void addDiseaseTerm(DOTerm diseaseAnnotationObject) {
 		if (diseaseTerms == null) {
 			diseaseTerms = new ArrayList<>();
@@ -45,7 +44,7 @@ public class AffectedGenomicModelDocument extends ESDocument {
 		diseaseTerms = diseaseTerms.stream()
 				.distinct()
 				.collect(Collectors.toList());
-		Collections.sort(diseaseTerms, Comparator.comparing(o -> o.getName() == null ? "" : o.getName()));
+		Collections.sort(diseaseTerms, Comparator.comparing(o -> o.getName() == null ? "" : o.getName().toLowerCase()));
 	}
 
 	private List<String> modifierRelationshipTypes = List.of("ameliorated_by", "exacerbated_by");
@@ -74,5 +73,16 @@ public class AffectedGenomicModelDocument extends ESDocument {
 
 	public boolean isHasPhenotypeAnnotations() {
 		return CollectionUtils.isNotEmpty(associatedPhenotype);
+	}
+
+	public void addAssociatedPhenotype(List<String> phenotypeList) {
+		if (associatedPhenotype == null) {
+			associatedPhenotype = new ArrayList<>();
+		}
+		associatedPhenotype.addAll(phenotypeList);
+		associatedPhenotype = associatedPhenotype.stream()
+				.distinct()
+				.collect(Collectors.toList());
+		Collections.sort(associatedPhenotype, Comparator.comparing(String::toLowerCase));
 	}
 }
