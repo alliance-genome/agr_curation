01a406b9a444bbaf6b8324ef4cb70c7506c95b79
Adam Gibson <agibson@mcw.edu>
2025-07-15 11:11:34 -0500

    SCRUM-4951 add allele summary doc

---
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/AlleleSummaryDocument.java
@@ -0,0 +1,28 @@
+package org.alliancegenome.curation_api.model.document.es;
+
+import java.util.List;
+import java.util.Map;
+
+import org.alliancegenome.curation_api.model.entities.Allele;
+import org.alliancegenome.curation_api.view.View;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import lombok.Data;
+
+@Data
+@JsonView(View.AlleleSummaryDocument.class)
+public class AlleleSummaryDocument extends ESDocument {
+
+	{
+		category = "allele_summary";
+	}
+	
+	private Allele allele;
+	private String species;
+	private String symbol;
+	private String alterationType;
+	private List<String> synonyms;
+	private String description;
+	private List<Map<String, String>> additionalInformation;
+}
