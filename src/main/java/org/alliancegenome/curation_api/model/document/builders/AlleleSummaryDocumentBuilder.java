c3cce4012ea23428cea34bf005bd777cd7f80e18
Adam Gibson <agibson@mcw.edu>
2025-07-24 15:12:44 -0500

    SCRUM-4952 add constructs to allele summary

--- src/main/java/org/alliancegenome/curation_api/model/document/builders/AlleleSummaryDocumentBuilder.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/builders/AlleleSummaryDocumentBuilder.java
@@ -2,19 +2,25 @@ package org.alliancegenome.curation_api.model.document.builders;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.alliancegenome.curation_api.model.document.es.AlleleSummaryDocument;
 import org.alliancegenome.curation_api.model.entities.Allele;
+import org.alliancegenome.curation_api.model.entities.Construct;
 import org.alliancegenome.curation_api.model.entities.CrossReference;
 import org.alliancegenome.curation_api.model.entities.Gene;
+import org.alliancegenome.curation_api.model.entities.GenomicEntity;
 import org.alliancegenome.curation_api.model.entities.ResourceDescriptorPage;
+import org.alliancegenome.curation_api.model.entities.associations.AlleleConstructAssociation;
 import org.alliancegenome.curation_api.model.entities.associations.AlleleGeneAssociation;
+import org.alliancegenome.curation_api.model.entities.associations.ConstructGenomicEntityAssociation;
 import org.alliancegenome.curation_api.model.entities.slotAnnotations.AlleleSynonymSlotAnnotation;
 import org.alliancegenome.curation_api.services.ResourceDescriptorPageService;
 import org.apache.commons.collections.CollectionUtils;
 
+import io.quarkus.logging.Log;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -48,6 +54,8 @@ public class AlleleSummaryDocumentBuilder {
 		doc.setAdditionalInformation(buildAdditionalInformation(allele, resourceDescriptorPageService));
 
 		doc.setAlleleOfGene(buildAlleleOfGene(allele));
+
+		doc.setConstructs(buildConstructs(allele, resourceDescriptorPageService));
 
 		return doc;
 	}
@@ -111,6 +119,66 @@ public class AlleleSummaryDocumentBuilder {
 		}
 
 		return alleleOfGene;
+	}
+
+	private List<Map<String, Object>> buildConstructs(Allele allele, ResourceDescriptorPageService resourceDescriptorPageService) {
+		List<Map<String, Object>> constructs = new ArrayList<>();
+
+		if (CollectionUtils.isNotEmpty(allele.getAlleleConstructAssociations())) {
+			for (AlleleConstructAssociation association : allele.getAlleleConstructAssociations()) {
+				Construct construct = association.getAlleleConstructAssociationObject();
+
+
+				if (construct != null) {
+					Map<String, Object> constructMap = new HashMap<>();
+
+					if (construct.getPrimaryExternalId() != null) {
+						constructMap.put("id", construct.getPrimaryExternalId());
+					}
+
+					String displayText = null;
+					if (construct.getConstructSymbol() != null && construct.getConstructSymbol().getDisplayText() != null) {
+						displayText = construct.getConstructSymbol().getDisplayText();
+					} else if (construct.getConstructFullName() != null && construct.getConstructFullName().getDisplayText() != null) {
+						displayText = construct.getConstructFullName().getDisplayText();
+					} else if (construct.getPrimaryExternalId() != null) {
+						displayText = construct.getPrimaryExternalId();
+					}
+
+					if (displayText != null) {
+						constructMap.put("name", displayText);
+					}
+
+					constructMap.put("type", "construct");
+
+					Map<String, Object> crossReferenceMap = new HashMap<>();
+					if (construct.getDataProviderCrossReference() != null || construct.getPrimaryExternalId() != null) {
+						Map<String, String> primary = new HashMap<>();
+
+						String referencedCurie = construct.getPrimaryExternalId();
+						if (construct.getDataProviderCrossReference() != null && construct.getDataProviderCrossReference().getReferencedCurie() != null) {
+							referencedCurie = construct.getDataProviderCrossReference().getReferencedCurie();
+						}
+
+						if (referencedCurie != null) {
+							primary.put("name", referencedCurie);
+
+							String constructUrl = buildUrlFromResourceDescriptorPage(referencedCurie, "construct", resourceDescriptorPageService);
+							if (constructUrl != null) {
+								primary.put("crossRefCompleteUrl", constructUrl);
+							}
+
+							crossReferenceMap.put("primary", primary);
+						}
+					}
+					constructMap.put("crossReferenceMap", crossReferenceMap);
+
+					constructs.add(constructMap);
+				}
+			}
+		}
+
+		return constructs;
 	}
 
 	private String buildUrlFromResourceDescriptorPage(String referencedCurie, String pageName, ResourceDescriptorPageService resourceDescriptorPageService) {
