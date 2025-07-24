306ed224ab6e34a8a9bfa1fe32dbf3c2e7d621e1
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2025-07-24 15:22:00 +0100

    Split migration

--- src/main/resources/db/migration/v0.41.0.7__add_deprecation_term_2.sql
+++ src/main/resources/db/migration/v0.41.0.7__add_deprecation_term_2.sql
@@ -1,10 +1,7 @@
-INSERT INTO vocabularyterm (id, name, vocabulary_id)
-	SELECT nextval('vocabularyterm_seq'), 'deprecation_reason', id FROM vocabulary WHERE vocabularylabel = 'note_type';
-	
 INSERT INTO vocabularytermset_vocabularyterm (vocabularytermsets_id, memberterms_id) 
 	WITH
   	t1 AS (
-    	SELECT id FROM vocabularytermset WHERE vocabularylabel = 'gene_note_type'
+    	SELECT id FROM vocabularytermset WHERE vocabularylabel = 'allele_note_type'
   	),
   	t2 AS (
     	SELECT id FROM vocabularyterm WHERE name = 'deprecation_reason' AND vocabulary_id = (
