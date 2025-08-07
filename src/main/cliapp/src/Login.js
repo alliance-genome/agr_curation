81d9d503166bf69a1c47ac4ef75fa9c97b3da012
Adam Gibson <agibson@mcw.edu>
2025-08-07 09:59:43 -0500

    update cookie expiration to 12 hours

--- src/main/cliapp/src/Login.js
+++ src/main/cliapp/src/Login.js
@@ -9,8 +9,8 @@ export const Login = ({ children }) => {
 	const [, setCookie] = useCookies(['okta-token-cookie']);
 
 	const onSuccess = (tokens) => {
-		//set cookie max age to 36 hours
-		setCookie('okta-token-cookie', tokens.accessToken.accessToken, { maxAge: 60 * 60 * 36 });
+		//set cookie max age to 12 hours
+		setCookie('okta-token-cookie', tokens.accessToken.accessToken, { maxAge: 60 * 60 * 12 });
 		oktaAuth.handleLoginRedirect(tokens);
 	};
 
