c39e4d9fc65ebe691e960743e5ecb4f5da81d128
Christian Pich <cmpich@zfin.org>
2025-08-20 21:56:21 +0200

    Revert "Make category variable readable"
    This reverts commit 032ae091b703c4416c070993c1fdee1d24b9116d.

--- src/main/java/org/alliancegenome/curation_api/model/document/es/ESDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/ESDocument.java
@@ -2,15 +2,10 @@ package org.alliancegenome.curation_api.model.document.es;
 
 import java.io.Serializable;
 
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Setter;
 
 @Data
 public abstract class ESDocument implements Serializable {
-	// Only needed to retrieve the category value. It's not meant to change this variable!
-	// The variable should be final, but Lombok does not support final variables with @Setter.
-	@Setter(AccessLevel.NONE)
-	public String category;
+	protected String category;
 	protected Boolean searchable = false;
 }
