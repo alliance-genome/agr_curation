e109c2368730fe3abb36b66b183ca8b307e020f0
Christian Pich <cmpich@zfin.org>
2025-08-20 15:58:30 +0200

    SCRUM-4082 prepare transcript section on allele page

--- src/main/java/org/alliancegenome/curation_api/model/document/es/TransgenicAlleleDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/TransgenicAlleleDocument.java
@@ -1,11 +1,15 @@
 package org.alliancegenome.curation_api.model.document.es;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 import org.alliancegenome.curation_api.model.entities.Allele;
 import org.alliancegenome.curation_api.model.entities.Construct;
 import org.alliancegenome.curation_api.model.entities.Gene;
 import org.alliancegenome.curation_api.model.entities.SequenceTargetingReagent;
+import org.alliancegenome.curation_api.model.entities.TransgenicAlleleConstruct;
 import org.alliancegenome.curation_api.view.View;
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -22,10 +26,34 @@ public class TransgenicAlleleDocument extends ESDocument {
 
 	private Gene gene;
 	private Allele allele;
-	private List<Construct> constructList;
-	private List<Gene> expressedGenes;
-	private List<SequenceTargetingReagent> sequenceTargetingReagents;
-	private List<Gene> regulatoryGenes;
+	/**
+	 * This collection is the association between a construct and the corresponding expressedGene, regulatoryGene, or sequenceTargetingReagent.
+	 */
+	private List<TransgenicAlleleConstruct> transgenicAlleleConstructs;
+
+	@JsonView(View.TransgenicAllelesDocumentView.class)
+	public List<Construct> getConstructList() {
+		return transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getConstruct).toList();
+	}
+
+	public List<Gene> getExpressedGenes() {
+		List<Gene> genes = new ArrayList<>(transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getExpressedGenes).flatMap(Collection::stream).toList());
+		genes.addAll(transgenicAlleleConstructs.stream()
+				.map(TransgenicAlleleConstruct::getNonBgiComponents)
+				.filter(Objects::nonNull)
+				.flatMap(Collection::stream)
+				.toList());
+		return genes;
+	}
+
+	public List<SequenceTargetingReagent> getSequenceTargetingReagents() {
+		return transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getSequenceTargetingReagents).filter(Objects::nonNull).flatMap(Collection::stream).toList();
+	}
+
+	public List<Gene> getRegulatoryGenes() {
+		return transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getRegulatoryGenes).flatMap(Collection::stream).toList();
+	}
+
 	private Boolean hasDiseaseAnnotations;
 	private Boolean hasPhenotypeAnnotations;
 }
