3e9bedbac8971a273013e2a7dc8b22f4d543b71c
VarunReddy1111 <varunreddy.go@gmail.com>
2025-08-25 14:58:53 -0500

    changes to the gene expression document builder

--- src/main/java/org/alliancegenome/curation_api/model/document/es/GeneExpressionDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/GeneExpressionDocument.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.alliancegenome.curation_api.model.entities.CrossReference;
 import org.alliancegenome.curation_api.model.entities.Gene;
-import org.alliancegenome.curation_api.model.entities.InformationContentEntity;
-import org.alliancegenome.curation_api.model.entities.Organization;
 import org.alliancegenome.curation_api.model.entities.ontology.MMOTerm;
 import org.alliancegenome.curation_api.view.View;
 
@@ -22,12 +20,13 @@ public class GeneExpressionDocument extends ESDocument {
 		category = "gene_expression_annotation";
 	}
 	private Gene gene;
-	private InformationContentEntity reference;
 	private String location;
 	private String stageName;
 	private MMOTerm assay;
-	private Organization dataProvider;
-	private List<CrossReference> crossReferences;
-	private String stageUberonTerm;
-
+	private String dataProvider;
+	private List<CrossReference> crossRefs;
+	private String stageTermId; 
+	private List<String> uberonTermIds;
+	private List<String> goTermIds;
+	private List<String> termIds;
 }
