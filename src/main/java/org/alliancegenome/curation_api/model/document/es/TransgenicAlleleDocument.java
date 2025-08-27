f8f23bc4bd7b047ed86737c1b785cf3154a956bf
Christian Pich <cmpich@zfin.org>
2025-08-27 17:22:46 +0200

    refactor document to be pure DTO

--- src/main/java/org/alliancegenome/curation_api/model/document/es/TransgenicAlleleDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/TransgenicAlleleDocument.java
@@ -1,9 +1,6 @@
 package org.alliancegenome.curation_api.model.document.es;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
 import org.alliancegenome.curation_api.model.entities.Allele;
 import org.alliancegenome.curation_api.model.entities.Construct;
@@ -11,7 +8,6 @@ import org.alliancegenome.curation_api.model.entities.Gene;
 import org.alliancegenome.curation_api.model.entities.SequenceTargetingReagent;
 import org.alliancegenome.curation_api.model.entities.TransgenicAlleleConstruct;
 import org.alliancegenome.curation_api.view.View;
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.Data;
@@ -19,50 +15,26 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@JsonView(View.TransgenicAllelesDocumentView.class)
 public class TransgenicAlleleDocument extends ESDocument {
 	{
 		category = "transgenic_allele_annotation";
 	}
 
-	@JsonView(View.TransgenicAllelesDocumentView.class)
 	private Gene gene;
-	@JsonView(View.TransgenicAllelesDocumentView.class)
 	private Allele allele;
 	/**
 	 * This collection is the association between a construct and the corresponding expressedGene, regulatoryGene, or sequenceTargetingReagent.
 	 */
-	@JsonView(View.TransgenicAllelesDocumentView.class)
 	private List<TransgenicAlleleConstruct> transgenicAlleleConstructs;
 
-	@JsonGetter
-	@JsonView(View.TransgenicAllelesDocumentView.class)
-	public List<Construct> constructList() {
-		return transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getConstruct).toList();
-	}
+	public List<Construct> constructs;
 
-	@JsonGetter
-	@JsonView(View.TransgenicAllelesDocumentView.class)
-	public List<Gene> expressedGenes() {
-		List<Gene> genes = new ArrayList<>(transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getExpressedGenes).flatMap(Collection::stream).toList());
-		genes.addAll(transgenicAlleleConstructs.stream()
-				.map(TransgenicAlleleConstruct::getNonBgiComponents)
-				.filter(Objects::nonNull)
-				.flatMap(Collection::stream)
-				.toList());
-		return genes;
-	}
+	public List<Gene> expressedGenes;
 
-	@JsonGetter
-	@JsonView(View.TransgenicAllelesDocumentView.class)
-	public List<SequenceTargetingReagent> sequenceTargetingReagents() {
-		return transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getSequenceTargetingReagents).filter(Objects::nonNull).flatMap(Collection::stream).toList();
-	}
+	public List<SequenceTargetingReagent> sequenceTargetingReagents;
 
-	@JsonGetter
-	@JsonView(View.TransgenicAllelesDocumentView.class)
-	public List<Gene> regulatoryGenes() {
-		return transgenicAlleleConstructs.stream().map(TransgenicAlleleConstruct::getRegulatoryGenes).flatMap(Collection::stream).toList();
-	}
+	public List<Gene> regulatoryGenes;
 
 	@JsonView(View.TransgenicAllelesDocumentView.class)
 	private Boolean hasDiseaseAnnotations;
