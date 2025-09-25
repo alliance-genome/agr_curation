b732f016e499fe74afff6df6a5430b4ed1854eaf
Adam Gibson <agibson@mcw.edu>
2025-09-25 10:47:31 -0500

    add PrimeReactProvider for global overlay setting

--- src/main/cliapp/src/index.js
+++ src/main/cliapp/src/index.js
@@ -1,11 +1,11 @@
 import 'react-app-polyfill/ie11';
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
+import { PrimeReactProvider } from 'primereact/api';
 
 const queryClient = new QueryClient();
 
@@ -15,12 +15,18 @@ if (!container) throw new Error('container not found!');
 
 const root = createRoot(container);
 
+ const value = {
+	  hideOverlaysOnDocumentScrolling: true,
+ };
+
 root.render(
 	<HashRouter>
 		<ScrollToTop>
 			<QueryClientProvider client={queryClient}>
-				<App />
-				<ReactQueryDevtools />
+				<PrimeReactProvider value={value}>
+					<App />
+					<ReactQueryDevtools />
+				</PrimeReactProvider>
 			</QueryClientProvider>
 		</ScrollToTop>
 	</HashRouter>
