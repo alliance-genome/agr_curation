3a25e79b2813cee325b692339c8693d20773db39
Adam <agibson@mcw.edu>
2025-10-07 07:55:25 -0500

    add back AlleleDAO change (#2255)
    * add back AlleleDAO change
    
    * restore AlleleSummaryDocumentBuilder associations
    
    * update AlleleDAO to filter AGAs
    
    * add variant counts and AlleleSummaryDTO
    
    * add not empty clauses
    
    ---------
    
    Co-authored-by: Christian Pich <cmpich@zfin.org>

--- src/main/java/org/alliancegenome/curation_api/dao/AlleleDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/AlleleDAO.java
@@ -8,12 +8,16 @@ import java.util.Map;
 import org.alliancegenome.curation_api.constants.EntityFieldConstants;
 import org.alliancegenome.curation_api.dao.associations.AgmAlleleAssociationDAO;
 import org.alliancegenome.curation_api.dao.base.BaseSQLDAO;
+import org.alliancegenome.curation_api.model.document.es.AlleleSummaryDTO;
 import org.alliancegenome.curation_api.model.entities.Allele;
 import org.apache.commons.collections.CollectionUtils;
+import org.alliancegenome.curation_api.model.input.Pagination;
+import org.alliancegenome.curation_api.response.SearchResponse;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 
 @ApplicationScoped
 public class AlleleDAO extends BaseSQLDAO<Allele> {
@@ -25,7 +29,7 @@ public class AlleleDAO extends BaseSQLDAO<Allele> {
 	@Inject AGMPhenotypeAnnotationDAO agmPhenotypeAnnotationDAO;
 	@Inject AgmAlleleAssociationDAO agmAlleleAssociationDAO;
 	@Inject HTPExpressionDatasetSampleAnnotationDAO htpExpressionDatasetSampleAnnotationDAO;
-	
+
 	protected AlleleDAO() {
 		super(Allele.class);
 	}
@@ -74,7 +78,7 @@ public class AlleleDAO extends BaseSQLDAO<Allele> {
 		results = agmPhenotypeAnnotationDAO.findIdsByParams(agmPaParams);
 		return CollectionUtils.isNotEmpty(results);
 	}
-	
+
 	public Boolean hasReferencingAgmAlleleAssociations(Long alleleId) {
 		Map<String, Object> params = new HashMap<>();
 		params.put(EntityFieldConstants.AGM_ALLELE_ASSOCIATION_OBJECT + ".id", alleleId);
@@ -86,26 +90,119 @@ public class AlleleDAO extends BaseSQLDAO<Allele> {
 		Map<String, Object> params = new HashMap<>();
 		params.put(EntityFieldConstants.GENOMIC_INFORMATION_ALLELE + ".id", alleleId);
 		List<Long> results = htpExpressionDatasetSampleAnnotationDAO.findIdsByParams(params);
-		
+
 		return CollectionUtils.isNotEmpty(results);
 	}
-	
+
 	public List<String> getAllAllelePrimaryExternalIds() {
 		String sql = """
 			SELECT be.primaryexternalid
 			FROM biologicalentity be, allele as a
 			WHERE be.id = a.id and be.primaryexternalid is not NULL
 		""";
-		
+
 		Query query = entityManager.createNativeQuery(sql);
 		List<Object> objects = query.getResultList();
 		List<String> list = new ArrayList<>();
-		
+
 		objects.forEach(object -> {
 			list.add((String) object);
 		});
-		
+
 		return list;
+	}
+	public SearchResponse<AlleleSummaryDTO> findAllelesForSummary(Pagination pagination, Map<String, Object> params) {
+		SearchResponse<Allele> filteredResults = super.findByParams(pagination, params, null);
+
+		if (filteredResults.getResults().isEmpty()) {
+			SearchResponse<AlleleSummaryDTO> emptyResponse = new SearchResponse<>();
+			emptyResponse.setResults(new ArrayList<>());
+			emptyResponse.setTotalResults(filteredResults.getTotalResults());
+			return emptyResponse;
+		}
+
+		List<Long> alleleIds = filteredResults.getResults().stream()
+			.map(Allele::getId)
+			.collect(java.util.stream.Collectors.toList());
+
+		String baseQuery = """
+			SELECT DISTINCT a FROM Allele a
+			LEFT JOIN FETCH a.dataProvider
+			LEFT JOIN FETCH a.dataProviderCrossReference
+			WHERE a.id IN :alleleIds
+			ORDER BY a.id
+			""";
+
+		TypedQuery<Allele> query = entityManager.createQuery(baseQuery, Allele.class);
+		query.setParameter("alleleIds", alleleIds);
+		List<Allele> alleles = query.getResultList();
+
+		Map<Long, Long> variantCountMap = new HashMap<>();
+
+		if (!alleleIds.isEmpty()) {
+			String geneAssocQuery = """
+				SELECT DISTINCT a FROM Allele a
+				LEFT JOIN FETCH a.alleleGeneAssociations aga
+				LEFT JOIN FETCH aga.alleleGeneAssociationObject gene
+				LEFT JOIN FETCH gene.geneSymbol
+				LEFT JOIN FETCH aga.relation r
+				WHERE a.id IN :alleleIds
+				AND r.name = 'is_allele_of'
+				AND aga.internal = false
+				AND aga.obsolete = false
+				and a.alleleGeneAssociations is not empty
+				""";
+			TypedQuery<Allele> geneQuery = entityManager.createQuery(geneAssocQuery, Allele.class);
+			geneQuery.setParameter("alleleIds", alleleIds);
+			geneQuery.getResultList(); // This loads the associations into the session
+
+			String constructAssocQuery = """
+				SELECT DISTINCT a FROM Allele a
+				LEFT JOIN FETCH a.alleleConstructAssociations aca
+				LEFT JOIN FETCH aca.alleleConstructAssociationObject construct
+				LEFT JOIN FETCH construct.constructSymbol
+				WHERE a.id IN :alleleIds and a.alleleConstructAssociations is not empty
+				""";
+			TypedQuery<Allele> constructQuery = entityManager.createQuery(constructAssocQuery, Allele.class);
+			constructQuery.setParameter("alleleIds", alleleIds);
+			constructQuery.getResultList();
+
+			String variantCountQuery = """
+				SELECT a.id, COUNT(ava.id)
+				FROM Allele a
+				LEFT JOIN a.alleleVariantAssociations ava
+				WHERE a.id IN :alleleIds and a.alleleVariantAssociations is not empty
+				GROUP BY a.id
+				""";
+			Query variantCountQueryExec = entityManager.createQuery(variantCountQuery);
+			variantCountQueryExec.setParameter("alleleIds", alleleIds);
+			List<Object[]> variantCounts = variantCountQueryExec.getResultList();
+
+			for (Object[] row : variantCounts) {
+				variantCountMap.put((Long) row[0], (Long) row[1]);
+			}
+
+			String notesQuery = """
+				SELECT DISTINCT a FROM Allele a
+				LEFT JOIN FETCH a.relatedNotes
+				WHERE a.id IN :alleleIds and a.relatedNotes is not empty
+				""";
+			TypedQuery<Allele> notesQueryExec = entityManager.createQuery(notesQuery, Allele.class);
+			notesQueryExec.setParameter("alleleIds", alleleIds);
+			notesQueryExec.getResultList();
+		}
+
+		List<AlleleSummaryDTO> dtos = new ArrayList<>();
+		for (Allele allele : alleles) {
+			Long count = variantCountMap.getOrDefault(allele.getId(), 0L);
+			dtos.add(new AlleleSummaryDTO(allele, count));
+		}
+
+		SearchResponse<AlleleSummaryDTO> response = new SearchResponse<>();
+		response.setResults(dtos);
+		response.setTotalResults(filteredResults.getTotalResults());
+
+		return response;
 	}
 
 
--- src/main/java/org/alliancegenome/curation_api/model/document/builders/AlleleSummaryDocumentBuilder.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/builders/AlleleSummaryDocumentBuilder.java
@@ -1,12 +1,19 @@
 package org.alliancegenome.curation_api.model.document.builders;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.alliancegenome.curation_api.model.document.es.AlleleSummaryDTO;
 import org.alliancegenome.curation_api.model.document.es.AlleleSummaryDocument;
 import org.alliancegenome.curation_api.model.entities.Allele;
+import org.alliancegenome.curation_api.model.entities.Construct;
 import org.alliancegenome.curation_api.model.entities.CrossReference;
+import org.alliancegenome.curation_api.model.entities.Gene;
 import org.alliancegenome.curation_api.model.entities.ResourceDescriptorPage;
+import org.alliancegenome.curation_api.model.entities.associations.AlleleConstructAssociation;
+import org.alliancegenome.curation_api.model.entities.associations.AlleleGeneAssociation;
 import org.alliancegenome.curation_api.services.ResourceDescriptorPageService;
 import org.apache.commons.collections.CollectionUtils;
 
@@ -16,16 +23,34 @@ import lombok.extern.slf4j.Slf4j;
 public class AlleleSummaryDocumentBuilder {
 
 
-	public AlleleSummaryDocument buildSummaryDocument(Allele allele, ResourceDescriptorPageService resourceDescriptorPageService) {
+	public AlleleSummaryDocument buildSummaryDocument(AlleleSummaryDTO alleleDTO, ResourceDescriptorPageService resourceDescriptorPageService) {
+		Allele allele = alleleDTO.getAllele();
 		AlleleSummaryDocument doc = new AlleleSummaryDocument();
 
 		doc.setAllele(allele);
 
 		doc.setCrossReference(getCrossReference(allele, resourceDescriptorPageService));
 
+		doc.setAlterationType(determineAlterationType(alleleDTO.getVariantCount()));
+
 		doc.setDescription(buildDescription(allele));
 
+		Optional<Gene> optionalAlleleOfGene = buildAlleleOfGene(allele);
+		optionalAlleleOfGene.ifPresent(gene -> doc.setAlleleOfGene(gene));
+
+		doc.setConstructSlimList(getConstructs(allele));
+
 		return doc;
+	}
+
+	private String determineAlterationType(Long variantCount) {
+		if (variantCount == null || variantCount == 0) {
+			return "allele";
+		} else if (variantCount == 1) {
+			return "allele with one variant";
+		} else {
+			return "allele with multiple variants";
+		}
 	}
 
 	private String buildDescription(Allele allele) {
@@ -46,6 +71,37 @@ public class AlleleSummaryDocumentBuilder {
 
 		return description;
 
+	}
+
+	private Optional<Gene> buildAlleleOfGene(Allele allele) {
+		List<AlleleGeneAssociation> alleleGeneAssociations = allele.getAlleleGeneAssociations();
+
+		if (CollectionUtils.isEmpty(alleleGeneAssociations)) {
+			return Optional.empty();
+		}
+
+		List<Gene> alleleOfGeneList = alleleGeneAssociations.stream()
+			.filter(aga -> aga.getRelation().getName().equals("is_allele_of"))
+			.filter(aga -> !aga.getInternal() && !aga.getObsolete())
+			.map(aga -> aga.getAlleleGeneAssociationObject())
+			.collect(Collectors.toList());
+
+		return alleleOfGeneList.stream().findFirst();
+	}
+
+	private List<Construct> getConstructs(Allele allele) {
+		List<Construct> constructs = new ArrayList<>();
+		if (CollectionUtils.isNotEmpty(allele.getAlleleConstructAssociations())) {
+			for (AlleleConstructAssociation association : allele.getAlleleConstructAssociations()) {
+				Construct construct = association.getAlleleConstructAssociationObject();
+
+				if (construct != null) {
+					constructs.add(construct);
+				}
+			}
+
+		}
+		return constructs;
 	}
 
 	private CrossReference getCrossReference(Allele allele, ResourceDescriptorPageService resourceDescriptorPageService) {
