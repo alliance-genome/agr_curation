5b6d1703e97f49f2aa031ea93ce5f38e1c35a1e7
VarunReddy1111 <varunreddy.go@gmail.com>
2025-10-21 11:39:21 -0500

    Changes in gene expression doc

--- src/main/java/org/alliancegenome/curation_api/model/document/es/GeneExpressionDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/GeneExpressionDocument.java
@@ -1,5 +1,6 @@
 package org.alliancegenome.curation_api.model.document.es;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.alliancegenome.curation_api.model.entities.GeneExpressionAnnotation;
@@ -23,5 +24,7 @@ public class GeneExpressionDocument extends ESDocument {
 	private List<String> uberonTermIds;
 	private List<String> goTermIds;
 	private List<String> termIds;
+	private HashMap<String, Integer> speciesOrder;
+	int phylogeneticSortingIndex;
 
 }
