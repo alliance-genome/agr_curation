f53486098d9f53441a75ee2d41cdc2b05c21f2d6
Christian Pich <cmpich@zfin.org>
2025-11-18 17:18:15 +0100

    SCRUM-5363 add allele object to variantSummaryDTO (#2304)

--- src/main/java/org/alliancegenome/curation_api/model/document/builders/VariantDocumentBuilder.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/builders/VariantDocumentBuilder.java
@@ -1,8 +1,11 @@
 package org.alliancegenome.curation_api.model.document.builders;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.alliancegenome.curation_api.model.document.es.VariantSummaryDTO;
+import org.alliancegenome.curation_api.model.entities.Allele;
 import org.alliancegenome.curation_api.model.entities.Variant;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +16,21 @@ public class VariantDocumentBuilder {
 	public List<VariantSummaryDTO> buildVariantDocument(Variant variant) {
 		return variant.getCuratedVariantGenomicLocations().stream()
 				.map(curatedVariantGenomicLocationAssociation -> {
-					VariantSummaryDTO dto = new VariantSummaryDTO();
-					dto.setVariant(curatedVariantGenomicLocationAssociation);
-					return dto;
-				}).toList();
+					List<VariantSummaryDTO> dtos = new ArrayList<>();
+					variant.getAlleleVariantAssociations().forEach(alleleVariantAssociation -> {
+						VariantSummaryDTO dto = new VariantSummaryDTO();
+						dto.setVariant(curatedVariantGenomicLocationAssociation);
+						Allele alleleAssociationSubject = alleleVariantAssociation.getAlleleAssociationSubject();
+						// create a shallow version of an allele for performance / resource purposes
+						if (alleleAssociationSubject != null) {
+							Allele allele = new Allele();
+							allele.setPrimaryExternalId(alleleAssociationSubject.getPrimaryExternalId());
+							dto.setAllele(allele);
+						}
+						dtos.add(dto);
+					});
+					return dtos;
+				}).flatMap(Collection::stream)
+				.toList();
 	}
 }
