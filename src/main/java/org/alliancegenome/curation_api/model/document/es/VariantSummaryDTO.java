e555a258d9e433aec3cd8153edfa91edb8da60e3
Christian Pich <cmpich@zfin.org>
2025-11-18 16:50:20 +0100

    SCRUM-5363 add allele object to variantSummaryDTO

--- src/main/java/org/alliancegenome/curation_api/model/document/es/VariantSummaryDTO.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/VariantSummaryDTO.java
@@ -1,5 +1,6 @@
 package org.alliancegenome.curation_api.model.document.es;
 
+import org.alliancegenome.curation_api.model.entities.Allele;
 import org.alliancegenome.curation_api.model.entities.associations.CuratedVariantGenomicLocationAssociation;
 import org.alliancegenome.curation_api.view.View;
 import com.fasterxml.jackson.annotation.JsonView;
@@ -13,5 +14,7 @@ import lombok.EqualsAndHashCode;
 public class VariantSummaryDTO {
 
 	private CuratedVariantGenomicLocationAssociation variant;
+
+	private Allele allele;
 
 }
