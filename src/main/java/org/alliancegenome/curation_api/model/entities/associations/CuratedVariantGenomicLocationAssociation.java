1f9e42cf11dfc5d4f7d82c354826a74ac98dd0bb
Christian Pich <cmpich@zfin.org>
2026-01-01 09:30:59 +0100

    SCRUM-5666 add overlap info to VariantSummaryDocument object

--- src/main/java/org/alliancegenome/curation_api/model/entities/associations/CuratedVariantGenomicLocationAssociation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/associations/CuratedVariantGenomicLocationAssociation.java
@@ -1,5 +1,6 @@
 package org.alliancegenome.curation_api.model.entities.associations;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -7,12 +8,12 @@ import java.util.stream.Collectors;
 
 import org.alliancegenome.curation_api.constants.LinkMLSchemaConstants;
 import org.alliancegenome.curation_api.interfaces.AGRCurationSchemaVersion;
+import org.alliancegenome.curation_api.model.entities.Gene;
 import org.alliancegenome.curation_api.model.entities.PredictedVariantConsequence;
 import org.alliancegenome.curation_api.view.View;
 import org.alliancegenome.curation_api.view.View.VariantView;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
@@ -31,70 +32,90 @@ import lombok.ToString;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
 @ToString(exclude = "predictedVariantConsequences", callSuper = true)
-@AGRCurationSchemaVersion(min = "2.4.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = { VariantGenomicLocationAssociation.class })
+@AGRCurationSchemaVersion(min = "2.4.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = {VariantGenomicLocationAssociation.class})
 @Schema(name = "CuratedVariantGenomicLocationAssociation", description = "POJO representing an association between a variant and a curated genomic location")
 
 @Table(indexes = {
-	@Index(name = "cvgla_internal_index", columnList = "internal"),
-	@Index(name = "cvgla_obsolete_index", columnList = "obsolete"),
-	@Index(name = "cvgla_hgvs_index", columnList = "hgvs"),
-	@Index(name = "cvgla_createdby_index", columnList = "createdBy_id"),
-	@Index(name = "cvgla_updatedby_index", columnList = "updatedBy_id"),
-	@Index(name = "cvgla_relation_index", columnList = "relation_id"),
-	@Index(name = "cvgla_dnamutationtype_index", columnList = "dnaMutationType_id"),
-	@Index(name = "cvgla_genelocalizationtype_index", columnList = "geneLocalizationType_id"),
-	@Index(name = "cvgla_consequence_index", columnList = "consequence_id"),
-	@Index(name = "cvgla_curatedconsequence_index", columnList = "curatedConsequence_id"),
-	@Index(name = "cvgla_variantassociationsubject_index", columnList = "variantassociationsubject_id"),
-	@Index(name = "cvgla_vglaobject_index", columnList = "variantgenomiclocationassociationobject_id")
-	}, name = "CuratedVariantGenomicLocation"
+		@Index(name = "cvgla_internal_index", columnList = "internal"),
+		@Index(name = "cvgla_obsolete_index", columnList = "obsolete"),
+		@Index(name = "cvgla_hgvs_index", columnList = "hgvs"),
+		@Index(name = "cvgla_createdby_index", columnList = "createdBy_id"),
+		@Index(name = "cvgla_updatedby_index", columnList = "updatedBy_id"),
+		@Index(name = "cvgla_relation_index", columnList = "relation_id"),
+		@Index(name = "cvgla_dnamutationtype_index", columnList = "dnaMutationType_id"),
+		@Index(name = "cvgla_genelocalizationtype_index", columnList = "geneLocalizationType_id"),
+		@Index(name = "cvgla_consequence_index", columnList = "consequence_id"),
+		@Index(name = "cvgla_curatedconsequence_index", columnList = "curatedConsequence_id"),
+		@Index(name = "cvgla_variantassociationsubject_index", columnList = "variantassociationsubject_id"),
+		@Index(name = "cvgla_vglaobject_index", columnList = "variantgenomiclocationassociationobject_id")
+}, name = "CuratedVariantGenomicLocation"
 )
 
 public class CuratedVariantGenomicLocationAssociation extends VariantGenomicLocationAssociation {
 
 	@IndexedEmbedded(
-		includePaths = {
-			"variantTranscript.name", "variantTranscript.primaryExternalId",
-			"variantTranscript.modInternalId", "variantTranscript.curie",
-			"vepConsequence.name", "variantTranscript.name_keyword",
-			"variantTranscript.primaryExternalId_keyword", "variantTranscript.modInternalId_keyword",
-			"variantTranscript.curie_keyword", "vepConsequence.name_keyword",
-			"variantTranscript.transcriptId", "variantTranscript.transcriptId_keyword"
-		}
+			includePaths = {
+					"variantTranscript.name", "variantTranscript.primaryExternalId",
+					"variantTranscript.modInternalId", "variantTranscript.curie",
+					"vepConsequence.name", "variantTranscript.name_keyword",
+					"variantTranscript.primaryExternalId_keyword", "variantTranscript.modInternalId_keyword",
+					"variantTranscript.curie_keyword", "vepConsequence.name_keyword",
+					"variantTranscript.transcriptId", "variantTranscript.transcriptId_keyword"
+			}
 	)
 	@OneToMany(mappedBy = "variantGenomicLocation", cascade = CascadeType.ALL, orphanRemoval = true)
 	@JsonManagedReference
-	@JsonView({ View.FieldsAndLists.class, VariantView.class })
+	@JsonView({View.FieldsAndLists.class, VariantView.class})
 	private List<PredictedVariantConsequence> predictedVariantConsequences;
 
 	@Transient
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	@JsonView({ VariantView.class })
+	@JsonView({VariantView.class})
 	public List<String> getHgvsC() {
 		if (predictedVariantConsequences == null) {
 			return Collections.emptyList();
 		}
 		return predictedVariantConsequences.stream()
-			.map(PredictedVariantConsequence::getHgvsCodingNomenclature)
-			.filter(Objects::nonNull)
-			.distinct()
-			.sorted()
-			.collect(Collectors.toList());
+				.map(PredictedVariantConsequence::getHgvsCodingNomenclature)
+				.filter(Objects::nonNull)
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
 	}
 
 	@Transient
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	@JsonView({ VariantView.class })
+	@JsonView({VariantView.class})
 	public List<String> getHgvsP() {
 		if (predictedVariantConsequences == null) {
 			return Collections.emptyList();
 		}
 		return predictedVariantConsequences.stream()
-			.map(PredictedVariantConsequence::getHgvsProteinNomenclature)
-			.filter(Objects::nonNull)
-			.distinct()
-			.sorted()
-			.collect(Collectors.toList());
+				.map(PredictedVariantConsequence::getHgvsProteinNomenclature)
+				.filter(Objects::nonNull)
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
 	}
 
+	@Transient
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@JsonView({VariantView.class})
+	public List<Gene> getOverlapGenes() {
+		if (predictedVariantConsequences == null) {
+			return Collections.emptyList();
+		}
+		return predictedVariantConsequences.stream()
+				.map(predictedVariantConsequence -> {
+					return predictedVariantConsequence
+							.getVariantTranscript()
+							.getTranscriptGeneAssociations()
+							.stream().map(TranscriptGeneAssociation::getTranscriptGeneAssociationObject)
+							.toList();
+				})
+				.flatMap(Collection::stream)
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
+	}
 }
