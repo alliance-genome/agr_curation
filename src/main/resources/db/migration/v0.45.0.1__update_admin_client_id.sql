4b8a1d0d2fa6584d470a9fbebf0bffe997db75b7
VarunReddy1111 <varunreddy.go@gmail.com>
2026-01-08 12:23:44 -0600

    hotfix to beta to resolve conflicts in migration versions

--- src/main/resources/db/migration/v0.45.0.1__update_admin_client_id.sql
+++ src/main/resources/db/migration/v0.45.0.1__update_admin_client_id.sql
@@ -1,2 +1,13 @@
+ALTER TABLE person ADD COLUMN IF NOT EXISTS authid VARCHAR(255);
+ALTER TABLE person ADD COLUMN IF NOT EXISTS authemail VARCHAR(255);
+
+UPDATE person SET authemail = oktaemail WHERE oktaemail IS NOT NULL;
+
+ALTER TABLE person DROP CONSTRAINT IF EXISTS person_authid_uk;
+ALTER TABLE person ADD CONSTRAINT person_authid_uk UNIQUE (authid);
+
+ALTER TABLE person DROP CONSTRAINT IF EXISTS person_authemail_uk;
+ALTER TABLE person ADD CONSTRAINT person_authemail_uk UNIQUE (authemail);
+
 update person set authId = '6umhkcuinvjdlmtp5841ites7g' where authemail = 'admin@alliancegenome.org';
 
