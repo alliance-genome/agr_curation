44acc4b3787e9fe03a75ced8ae90ab358ee5bb45
Christian Pich <cmpich@zfin.org>
2026-01-11 13:05:31 +0100

    SCRUM-5666 add getMostSevereConsequence method

--- src/main/java/org/alliancegenome/curation_api/model/entities/associations/CuratedVariantGenomicLocationAssociation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/associations/CuratedVariantGenomicLocationAssociation.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static org.alliancegenome.curation_api.services.associations.CuratedVariantGenomicLocationAssociationService.SORTED_VARIANT_CONSEQUENCE_MAP;
+
 import org.alliancegenome.curation_api.constants.LinkMLSchemaConstants;
 import org.alliancegenome.curation_api.interfaces.AGRCurationSchemaVersion;
 import org.alliancegenome.curation_api.model.entities.Gene;
@@ -66,8 +68,27 @@ public class CuratedVariantGenomicLocationAssociation extends VariantGenomicLoca
 	)
 	@OneToMany(mappedBy = "variantGenomicLocation", cascade = CascadeType.ALL, orphanRemoval = true)
 	@JsonManagedReference
-	@JsonView({View.FieldsAndLists.class, VariantView.class})
 	private List<PredictedVariantConsequence> predictedVariantConsequences;
+
+	@Transient
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@JsonView({View.FieldsOnly.class, VariantView.class})
+	public PredictedVariantConsequence getMostSevereConsequence() {
+		if (predictedVariantConsequences == null) {
+			return null;
+		}
+		return predictedVariantConsequences.stream()
+				.sorted(Comparator.comparingInt(pvc -> {
+					if (pvc.getVepConsequences() == null || pvc.getVepConsequences().isEmpty()) {
+						return Integer.MAX_VALUE;
+					}
+					return pvc.getVepConsequences().stream()
+							.map(soTerm -> SORTED_VARIANT_CONSEQUENCE_MAP.getOrDefault(soTerm.getName(), Integer.MAX_VALUE))
+							.min(Integer::compareTo)
+							.orElse(Integer.MAX_VALUE);
+				}))
+				.collect(Collectors.toList()).getFirst();
+	}
 
 	@Transient
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
