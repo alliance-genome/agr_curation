23e78e376f23bf42324efe13c3786939f715fa7b
Adam Gibson <agibson@mcw.edu>
2026-01-30 10:05:41 -0600

    SCRUM-5496 add link to TruncatedReferencesTemplate for constructs

--- src/main/cliapp/src/containers/constructsPage/ConstructsTable.js
+++ src/main/cliapp/src/containers/constructsPage/ConstructsTable.js
@@ -245,7 +245,13 @@ export const ConstructsTable = () => {
 			{
 				field: 'references.primaryCrossReferenceCurie',
 				header: 'References',
-				body: (rowData) => <TruncatedReferencesTemplate references={rowData.references} />,
+				body: (rowData) => (
+					<TruncatedReferencesTemplate
+						references={rowData.references}
+						identifier={rowData.primaryExternalId}
+						detailPage="Construct"
+					/>
+				),
 				sortable: { isInEditMode },
 				filterConfig: FILTER_CONFIGS.referencesFilterConfig,
 			},
