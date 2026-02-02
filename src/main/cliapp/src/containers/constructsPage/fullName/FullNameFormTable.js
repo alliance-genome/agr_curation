c94e407d7f97d1c528559ac11296fe410adeed9f
Adam Gibson <agibson@mcw.edu>
2026-02-02 10:44:56 -0600

    SCRUM-5729 add more columns to constructPage full name table

--- src/main/cliapp/src/containers/constructsPage/fullName/FullNameFormTable.js
+++ src/main/cliapp/src/containers/constructsPage/fullName/FullNameFormTable.js
@@ -2,6 +2,14 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { ColumnGroup } from 'primereact/columngroup';
 import { Row } from 'primereact/row';
+import {
+	synonymScopeTemplate,
+	nameTypeTemplate,
+	synonymUrlTemplate,
+	displayTextTemplate,
+	formatTextTemplate,
+} from '../../../components/NameSlotAnnotationComponent';
+import { evidenceTemplate } from '../../../components/EvidenceComponent';
 
 export const FullNameFormTable = ({ name, tableRef }) => {
 	const internalTemplate = (rowData) => {
@@ -13,8 +21,13 @@ export const FullNameFormTable = ({ name, tableRef }) => {
 			<Row>
 				<Column header="Display Text" />
 				<Column header="Format Text" />
+				<Column header="Synonym Scope" />
 				<Column header="Name Type" />
+				<Column header="Synonym URL" />
 				<Column header="Internal" />
+				<Column header="Evidence" />
+				<Column header="Updated By" />
+				<Column header="Date Updated" />
 			</Row>
 		</ColumnGroup>
 	);
@@ -30,10 +43,45 @@ export const FullNameFormTable = ({ name, tableRef }) => {
 			columnResizeMode="expand"
 			ref={tableRef}
 		>
-			<Column field="displayText" header="Display Text" headerClassName="surface-0" />
-			<Column field="formatText" header="Format Text" headerClassName="surface-0" />
-			<Column field="nameType.name" header="Name Type" headerClassName="surface-0" />
-			<Column field="internal" header="Internal" headerClassName="surface-0" body={internalTemplate} />
+			<Column field="displayText" header="Display Text" headerClassName="surface-0" body={displayTextTemplate} />
+			<Column
+				field="formatText"
+				header="Format Text"
+				headerClassName="surface-0"
+				body={formatTextTemplate}
+			/>
+			<Column
+				field="synonymScope"
+				header="Synonym Scope"
+				headerClassName="surface-0"
+				body={synonymScopeTemplate}
+			/>
+			<Column
+				field="nameType"
+				header="Name Type"
+				headerClassName="surface-0"
+				body={nameTypeTemplate}
+			/>
+			<Column
+				field="synonymUrl"
+				header="Synonym URL"
+				headerClassName="surface-0"
+				body={synonymUrlTemplate}
+			/>
+			<Column
+				field="internal"
+				header="Internal"
+				body={internalTemplate}
+				headerClassName="surface-0"
+			/>
+			<Column
+				field="evidence.curie"
+				header="Evidence"
+				headerClassName="surface-0"
+				body={(rowData) => evidenceTemplate(rowData)}
+			/>
+			<Column field="updatedBy.uniqueId" header="Updated By" />
+			<Column field="dateUpdated" header="Date Updated" />
 		</DataTable>
 	);
 };
