632dacddf015a4cd7c9a3c00e809d15bd365c84a
Christian Pich <cmpich@zfin.org>
2026-02-05 13:30:03 +0100

    SCRUM-5666 add GenomeLocation to the gene object

--- src/main/java/org/alliancegenome/curation_api/model/entities/associations/VariantLocationAssociation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/associations/VariantLocationAssociation.java
@@ -79,27 +79,5 @@ public abstract class VariantLocationAssociation extends LocationAssociation {
 	@ManyToOne
 	@JsonView({CurationView.FieldsOnly.class})
 	private SOTerm curatedConsequence;
-	
-	@Transient
-	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	@JsonView({CurationView.FieldsOnly.class, CurationView.VariantView.class})
-	public String getNucleotideChange() {
-		if (variantAssociationSubject != null && variantAssociationSubject.getVariantType() != null) {
-			String variantTypeCurie = variantAssociationSubject.getVariantType().getCurie();
-			if ("SO:0000667".equals(variantTypeCurie)) {
-				// Insertion
-				return "c>c" + variantSequence;
-			} else if ("SO:1000008".equals(variantTypeCurie)) {
-				// Point mutation
-				return referenceSequence + ">" + variantSequence;
-			} else if ("SO:0000159".equals(variantTypeCurie)) {
-				// Deletion
-				return "t" + referenceSequence + ">t";
-			}
-			if (referenceSequence != null && variantSequence != null) {
-				return referenceSequence + ">" + variantSequence;
-			}
-		}
-		return null;
-	}
+
 }
