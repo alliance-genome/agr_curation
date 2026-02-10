1cb79fb6a0eff1980a314545faeca32ef624934e
Olin Blodgett <nilo@nilosplace.net>
2026-02-10 17:23:48 -0500

    Refactored expression to speed up the retrival of annotations

--- src/main/java/org/alliancegenome/curation_api/dao/GeneExpressionAnnotationDAO.java
+++ src/main/java/org/alliancegenome/curation_api/dao/GeneExpressionAnnotationDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.alliancegenome.curation_api.dao.base.BaseSQLDAO;
 import org.alliancegenome.curation_api.model.entities.GeneExpressionAnnotation;
+import org.apache.commons.collections.CollectionUtils;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.Query;
@@ -27,4 +28,17 @@ public class GeneExpressionAnnotationDAO extends BaseSQLDAO<GeneExpressionAnnota
 		List<Object> list = query.getResultList();
 		return new ArrayList<>(list.stream().map(o -> (String) o).toList());
 	}
+
+	public List<GeneExpressionAnnotation> getByIds(List<String> ids) {
+		if (CollectionUtils.isEmpty(ids)) {
+			return new ArrayList<>();
+		}
+
+		return entityManager.createQuery(
+				"SELECT g FROM GeneExpressionAnnotation g WHERE expressionAnnotationSubject.primaryExternalId IN :ids", GeneExpressionAnnotation.class)
+			.setParameter("ids", ids)
+			.getResultList();
+		
+	}
+
 }
