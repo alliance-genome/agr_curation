aad3e6c5cf53729dfb86c7ddb4fcfdcd3ac19f55
Christian Pich <cmpich@zfin.org>
2026-02-27 13:57:38 +0100

    SCRUM-5666 add JSON view to introns and exons (#2492)
    * SCRUM-5666 add JSON view to introns and exons
    
    * SCRUM-4101 add sequence feature name of transcripts

--- src/main/java/org/alliancegenome/curation_api/model/entities/PredictedVariantConsequence.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/PredictedVariantConsequence.java
@@ -166,16 +166,16 @@ public class PredictedVariantConsequence extends AuditedObject {
 
 	@FullTextField(analyzer = "autocompleteAnalyzer", searchAnalyzer = "autocompleteSearchAnalyzer")
 	@KeywordField(name = "introns_keyword", aggregable = Aggregable.YES, sortable = Sortable.YES, searchable = Searchable.YES, normalizer = "sortNormalizer")
-	@JsonView({CurationView.FieldsOnly.class, CurationView.SequenceSummaryDocument.class})
+	@JsonView({CurationView.FieldsOnly.class, CurationView.SequenceSummaryDocument.class, CurationView.VariantSummaryDocument.class})
 	@Transient
 	private String introns;
 
 	@FullTextField(analyzer = "autocompleteAnalyzer", searchAnalyzer = "autocompleteSearchAnalyzer")
 	@KeywordField(name = "exons_keyword", aggregable = Aggregable.YES, sortable = Sortable.YES, searchable = Searchable.YES, normalizer = "sortNormalizer")
-	@JsonView({CurationView.FieldsOnly.class, CurationView.SequenceSummaryDocument.class})
+	@JsonView({CurationView.FieldsOnly.class, CurationView.SequenceSummaryDocument.class, CurationView.VariantSummaryDocument.class})
 	@Transient
 	private String exons;
-	
+
 	@FullTextField(analyzer = "autocompleteAnalyzer", searchAnalyzer = "autocompleteSearchAnalyzer", valueBridge = @ValueBridgeRef(type = BooleanValueBridge.class))
 	@KeywordField(name = "geneLevelConsequence_keyword", aggregable = Aggregable.YES, sortable = Sortable.YES, searchable = Searchable.YES, valueBridge = @ValueBridgeRef(type = BooleanValueBridge.class))
 	@JsonView({CurationView.FieldsOnly.class, CurationView.VariantSummaryDocument.class, CurationView.SequenceSummaryDocument.class})
@@ -184,7 +184,7 @@ public class PredictedVariantConsequence extends AuditedObject {
 
 	@Transient
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	@JsonView({CurationView.FieldsOnly.class, CurationView.VariantView.class, CurationView.SequenceSummaryDocument.class})
+	@JsonView({CurationView.FieldsOnly.class, CurationView.VariantView.class, CurationView.SequenceSummaryDocument.class, CurationView.VariantSummaryDocument.class})
 	public String getIntronExonLocation() {
 		if (exons == null && introns == null) {
 			return null;
@@ -196,5 +196,19 @@ public class PredictedVariantConsequence extends AuditedObject {
 			return "Intron " + introns;
 		}
 		return "Exon " + exons + " : " + "Intron " + introns;
+	}
+
+	@Transient
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@JsonView({CurationView.FieldsOnly.class, CurationView.VariantSummaryDocument.class})
+	public String getSequenceFeatureName() {
+		if (hgvsCodingNomenclature == null) {
+			return null;
+		}
+		String[] tokens = hgvsCodingNomenclature.split(":");
+		if (tokens.length < 2) {
+			return null;
+		}
+		return tokens[1];
 	}
 }
