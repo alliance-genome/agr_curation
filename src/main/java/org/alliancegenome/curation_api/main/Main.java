d7e32486ea24756fe0f4fd95b7bd547c1bfc1ac0
Olin Blodgett <nilo@nilosplace.net>
2026-03-10 21:20:13 -0400

    Revert Log

--- src/main/java/org/alliancegenome/curation_api/main/Main.java
+++ src/main/java/org/alliancegenome/curation_api/main/Main.java
@@ -8,7 +8,7 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Running main method of quarkus");
+		Log.info("Running main method of quarkus");
 		ExceptionCatcher.initialize();
 		Quarkus.run(args);
 	}
