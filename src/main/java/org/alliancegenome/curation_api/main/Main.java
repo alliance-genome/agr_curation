81683e2a5e336069fb3586c504c66ef3303d0b41
Olin Blodgett <nilo@nilosplace.net>
2026-03-10 21:17:53 -0400

    Added Exceptional

--- src/main/java/org/alliancegenome/curation_api/main/Main.java
+++ src/main/java/org/alliancegenome/curation_api/main/Main.java
@@ -1,13 +1,15 @@
 package org.alliancegenome.curation_api.main;
 
-import io.quarkus.logging.Log;
+import org.alliancegenome.exceptional.client.ExceptionCatcher;
+
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class Main {
 	public static void main(String[] args) {
-		Log.info("Running main method of quarkus");
+		System.out.println("Running main method of quarkus");
+		ExceptionCatcher.initialize();
 		Quarkus.run(args);
 	}
 }
