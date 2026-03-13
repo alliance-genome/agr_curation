675f1065665309d7f3a6b282c5056c085d6efb2c
Olin Blodgett <nilo@nilosplace.net>
2026-03-13 09:39:51 -0400

    fix: Add @JsonView annotations to all DiseaseSearchResultDocument fields
    Fields without @JsonView were being filtered out during serialization.
    
    SCRUM-4937

--- src/main/java/org/alliancegenome/curation_api/model/document/es/DiseaseSearchResultDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/DiseaseSearchResultDocument.java
@@ -18,19 +18,29 @@ public class DiseaseSearchResultDocument extends ESDocument {
 		category = "disease_search_result";
 	}
 
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	private String curie;
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	private String definition;
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	String name;
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	Set<String> synonyms = new HashSet<>();
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	String primaryKey;
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	Set<String> crossReferences = new HashSet<>();
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	Set<String> genes = new HashSet<>();
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	Set<String> diseaseGroup = new HashSet<>();
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	Set<String> associatedSpecies = new HashSet<>();
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	@JsonProperty("name_key")
 	String nameKey;
 
+	@JsonView(CurationView.DiseaseSearchResultDocument.class)
 	Set<String> secondaryIds = new HashSet<>();
-
 
 }
