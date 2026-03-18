0cd5f086c717b62b67a91d1342376a9612799fa2
Christian Pich <cmpich@zfin.org>
2026-03-18 12:54:56 +0100

    optimize imports

--- src/main/java/org/alliancegenome/curation_api/model/document/es/VariantSummaryDocument.java
+++ src/main/java/org/alliancegenome/curation_api/model/document/es/VariantSummaryDocument.java
@@ -1,12 +1,9 @@
 package org.alliancegenome.curation_api.model.document.es;
 
-
 import java.util.List;
-import java.util.Set;
 
 import org.alliancegenome.curation_api.model.entities.Variant;
 import org.alliancegenome.curation_api.view.CurationView;
-
 import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.Data;
@@ -14,12 +11,11 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-@JsonView({ CurationView.VariantSummaryDocument.class })
+@JsonView({CurationView.VariantSummaryDocument.class})
 public class VariantSummaryDocument extends AVSParentDocument {
 
 	{
 		category = "variant_summary";
 	}
 	private List<Variant> variants;
-
 }
