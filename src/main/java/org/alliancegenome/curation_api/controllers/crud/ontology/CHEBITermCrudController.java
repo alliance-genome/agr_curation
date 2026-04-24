1fb99cc08a978bea1abef486c7bf2d65d3ad24b6
VarunReddy1111 <varunreddy.go@gmail.com>
2026-04-24 10:28:29 -0500

    turned on closure calculation

--- src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/CHEBITermCrudController.java
+++ src/main/java/org/alliancegenome/curation_api/controllers/crud/ontology/CHEBITermCrudController.java
@@ -21,7 +21,7 @@ public class CHEBITermCrudController extends BaseOntologyTermController<CHEBITer
 	public void init() {
 		GenericOntologyLoadConfig config = new GenericOntologyLoadConfig();
 		config.setLoadOnlyIRIPrefix("CHEBI");
-		config.setCalculateClosure(false);
+		config.setCalculateClosure(true);
 		setService(chebiTermService, CHEBITerm.class, config);
 	}
 
