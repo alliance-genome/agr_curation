9bb998197080f00c7f05aecd2349ae0d686ef1b0
Ketaki Kindre <kkindre@caltech.edu>
2026-06-30 13:24:27 -0500

    Revert "SCRUM-6161: Fix Relation filter panel width and dev proxy IPv6 issue"
    This reverts commit f52c3b935c72a89b10025ff117817e508f8f0e4c.

--- src/main/cliapp/vite.config.js
+++ src/main/cliapp/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
 		open: true,
 		proxy: {
 			'/api': {
-				target: process.env.API_URL || 'http://127.0.0.1:8080',
+				target: process.env.API_URL || 'http://localhost:8080',
 				changeOrigin: true,
 			},
 		},
