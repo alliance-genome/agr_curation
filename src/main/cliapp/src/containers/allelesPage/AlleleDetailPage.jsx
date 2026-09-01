590e074eff6cab35a119ac53ec89e8b58fbddbad
Adam Gibson <agibson@mcw.edu>
2026-09-01 09:46:12 -0500

    SCRUM-6076 extract AlleleForm from the allele detail page
    Moves the form body into a shared AlleleForm taking state, dispatch and
    isVisible, along with the five EDIT dispatch wrappers and the column size
    constants that only the form needs. ALLELE_TOGGLEABLE_FIELDS is exported from
    there so both pages can pass the same list to useFormFieldVisibility.
    
    The detail page keeps the query, PUT and sticky header. No behaviour change.

--- src/main/cliapp/src/containers/allelesPage/AlleleDetailPage.jsx
+++ src/main/cliapp/src/containers/allelesPage/AlleleDetailPage.jsx
@@ -7,61 +7,12 @@ import { useParams } from 'react-router-dom';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { AlleleService } from '../../service/AlleleService';
 import ErrorBoundary from '../../components/Error/ErrorBoundary';
-import { TaxonDetailPageEditor } from '../../components/Editors/autocomplete/taxon/TaxonDetailPageEditor';
 import { useAlleleReducer } from './useAlleleReducer';
-import { InCollectionDetailPageEditor } from '../../components/Editors/autocomplete/inCollection/InCollectionDetailPageEditor';
-import { BooleanDetailPageEditor } from '../../components/Editors/dropdown/boolean/BooleanDetailPageEditor';
-import { IdentifierDetailPageTemplate } from '../../components/Templates/IdentifierDetailPageTemplate';
-import { DataProviderDetailPageTemplate } from '../../components/Templates/DataProviderDetailPageTemplate';
-import { DateDetailPageTemplate } from '../../components/Templates/DateDetailPageTemplate';
-import { UserDetailPageTemplate } from '../../components/Templates/UserDetailPageTemplate';
-import { SynonymsForm } from './synonyms/SynonymsForm';
-import { FullNameForm } from './fullName/FullNameForm';
-import { MutationTypesForm } from './mutationTypes/MutationTypesForm';
-import { InheritanceModesForm } from './inheritanceModes/InheritanceModesForm';
-import { SecondaryIdsForm } from './secondaryIds/SecondaryIdsForm';
-import { FunctionalImpactsForm } from './functionalImpacts/FunctionalImpactsForm';
-import { DatabaseStatusForm } from './databaseStatus/DatabaseStatusForm';
-import { RelatedNotesForm } from './relatedNotes/RelatedNotesForm';
-import { SymbolForm } from './symbol/SymbolForm';
-import { GermilineTransmissionStatusForm } from './germlineTransmissionStatus/GermlineTransmissionStatusForm';
-import { ReferencesForm } from './referencesTable/ReferencesForm';
-import { NomenclatureEventsForm } from './nomenclatureEvents/NomenclatureEventsForm';
 import { StickyHeader } from '../../components/StickyHeader';
 import { LoadingOverlay } from '../../components/LoadingOverlay';
-import { AlleleGeneAssociationsForm } from './alleleGeneAssociations/AlleleGeneAssociationsForm';
 import { validateRequiredAutosuggestField, processErrors } from './utils';
-import { FormFieldVisibilityMenu, FormSection, useFormFieldVisibility } from '../../components/FormFieldVisibility';
-
-// Every section on the page, in display order.
-const ALLELE_TOGGLEABLE_FIELDS = [
-	'Curie',
-	'Primary External ID',
-	'MOD Internal ID',
-	'Name',
-	'Symbol',
-	'Synonyms',
-	'Secondary IDs',
-	'Nomenclature Events',
-	'Taxon',
-	'Mutation Types',
-	'Functional Impacts',
-	'Germline Transmission Status',
-	'Database Status',
-	'Inheritance Modes',
-	'References',
-	'In Collection',
-	'Is Extinct',
-	'Related Notes',
-	'Allele Gene Associations',
-	'Data Provider',
-	'Updated By',
-	'Date Updated',
-	'Created By',
-	'Date Created',
-	'Internal',
-	'Obsolete',
-];
+import { FormFieldVisibilityMenu, useFormFieldVisibility } from '../../components/FormFieldVisibility';
+import { AlleleForm, ALLELE_TOGGLEABLE_FIELDS } from './AlleleForm';
 
 export default function AlleleDetailPage() {
 	const { identifier } = useParams();
@@ -73,10 +24,6 @@ export default function AlleleDetailPage() {
 	const alleleService = new AlleleService();
 	const toastSuccess = useRef(null);
 	const toastError = useRef(null);
-
-	const labelColumnSize = 'col-3';
-	const widgetColumnSize = 'col-4';
-	const fieldDetailsColumnSize = 'col-5';
 
 	const { isPending: getRequestIsLoading, data: alleleQueryData } = useQuery({
 		queryKey: [identifier],
@@ -142,60 +89,6 @@ export default function AlleleDetailPage() {
 		});
 	};
 
-	const onTaxonValueChange = (event) => {
-		let value = {};
-		if (typeof event.value === 'object') {
-			value = event.value;
-		} else {
-			value.curie = event.value;
-		}
-		alleleDispatch({
-			type: 'EDIT',
-			field: 'taxon',
-			value,
-		});
-	};
-
-	const onInCollectionValueChange = (event) => {
-		let value = {};
-		if (typeof event.value === 'object') {
-			value = event.value;
-		} else if (event.value === '') {
-			value = undefined;
-		} else {
-			value.name = event.value;
-		}
-		alleleDispatch({
-			type: 'EDIT',
-			field: 'inCollection',
-			value,
-		});
-	};
-
-	const onIsExtinctValueChange = (event) => {
-		alleleDispatch({
-			type: 'EDIT',
-			field: 'isExtinct',
-			value: event.value,
-		});
-	};
-
-	const onInternalValueChange = (event) => {
-		alleleDispatch({
-			type: 'EDIT',
-			field: 'internal',
-			value: event.value,
-		});
-	};
-
-	const onObsoleteValueChange = (event) => {
-		alleleDispatch({
-			type: 'EDIT',
-			field: 'obsolete',
-			value: event.value,
-		});
-	};
-
 	if (getRequestIsLoading)
 		return (
 			<div className="flex align-items-center justify-content-center h-screen">
@@ -238,200 +131,7 @@ export default function AlleleDetailPage() {
 						</SplitterPanel>
 					</Splitter>
 				</StickyHeader>
-				<form className="mt-8">
-					<FormSection isVisible={isVisible('Curie')}>
-						<IdentifierDetailPageTemplate
-							identifier={alleleState.allele?.curie}
-							label="Curie"
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Primary External ID')}>
-						<IdentifierDetailPageTemplate
-							identifier={alleleState.allele?.primaryExternalId}
-							label="Primary External ID"
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('MOD Internal ID')}>
-						<IdentifierDetailPageTemplate
-							identifier={alleleState.allele?.modInternalId}
-							label="MOD Internal ID"
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Name')}>
-						<FullNameForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Symbol')}>
-						<SymbolForm state={alleleState} dispatch={alleleDispatch} labelColumnSize={labelColumnSize} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Synonyms')}>
-						<SynonymsForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Secondary IDs')}>
-						<SecondaryIdsForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Nomenclature Events')}>
-						<NomenclatureEventsForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Taxon')}>
-						<TaxonDetailPageEditor
-							taxon={alleleState.allele?.taxon}
-							onTaxonValueChange={onTaxonValueChange}
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-							errorMessages={alleleState.errorMessages}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Mutation Types')}>
-						<MutationTypesForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Functional Impacts')}>
-						<FunctionalImpactsForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Germline Transmission Status')}>
-						<GermilineTransmissionStatusForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Database Status')}>
-						<DatabaseStatusForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Inheritance Modes')}>
-						<InheritanceModesForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('References')}>
-						<ReferencesForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('In Collection')}>
-						<InCollectionDetailPageEditor
-							inCollection={alleleState.allele?.inCollection}
-							onInCollectionValueChange={onInCollectionValueChange}
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-							errorMessages={alleleState.errorMessages}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Is Extinct')}>
-						<BooleanDetailPageEditor
-							value={alleleState.allele?.isExtinct}
-							name={'isExtinct'}
-							label={'Is Extinct'}
-							onValueChange={onIsExtinctValueChange}
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-							errorMessages={alleleState.errorMessages}
-							showClear={true}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Related Notes')}>
-						<RelatedNotesForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Allele Gene Associations')}>
-						<AlleleGeneAssociationsForm state={alleleState} dispatch={alleleDispatch} />
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Data Provider')}>
-						<DataProviderDetailPageTemplate
-							dataProvider={alleleState.allele?.dataProvider?.abbreviation}
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Updated By')}>
-						<UserDetailPageTemplate
-							user={alleleState.allele?.updatedBy?.uniqueId}
-							fieldName="Updated By"
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Date Updated')}>
-						<DateDetailPageTemplate
-							date={alleleState.allele?.dateUpdated}
-							fieldName="Date Updated"
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Created By')}>
-						<UserDetailPageTemplate
-							user={alleleState.allele?.createdBy?.uniqueId}
-							fieldName="Created By"
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Date Created')}>
-						<DateDetailPageTemplate
-							date={alleleState.allele?.dateCreated}
-							fieldName="Date Created"
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Internal')}>
-						<BooleanDetailPageEditor
-							value={alleleState.allele?.internal}
-							name={'internal'}
-							label={'Internal'}
-							onValueChange={onInternalValueChange}
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-							errorMessages={alleleState.errorMessages}
-						/>
-					</FormSection>
-
-					<FormSection isVisible={isVisible('Obsolete')}>
-						<BooleanDetailPageEditor
-							value={alleleState.allele?.obsolete}
-							name={'obsolete'}
-							label={'Obsolete'}
-							onValueChange={onObsoleteValueChange}
-							widgetColumnSize={widgetColumnSize}
-							labelColumnSize={labelColumnSize}
-							fieldDetailsColumnSize={fieldDetailsColumnSize}
-							errorMessages={alleleState.errorMessages}
-						/>
-					</FormSection>
-				</form>
+				<AlleleForm state={alleleState} dispatch={alleleDispatch} isVisible={isVisible} />
 			</ErrorBoundary>
 		</>
 	);
